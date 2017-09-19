9e9ffbd49eb6490bb1168ce2ff32b10a81498ba9
yanneta <yannet@gmail.com>
2017-09-18 17:05:59 -0700

    mv plot_confussion_matrix to plots.py

--- fastai/plots.py
+++ fastai/plots.py
@@ -34,3 +34,30 @@ def plots_from_files(imspaths, figsize=(10,5), rows=1, titles=None, maintitle=No
         if titles is not None: sp.set_title(titles[i], fontsize=16)
         img = plt.imread(imspaths[i])
         plt.imshow(img)
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix',
+        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    (This function is copied from the scikit docs.)
+    """
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
