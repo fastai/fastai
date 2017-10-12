486c5a55713c410a632e4ca554491bf804554747
Jeremy Howard <j@howard.fm>
2017-10-12 11:11:02 -0700

    finish rossman model

--- fastai/column_data.py
+++ fastai/column_data.py
@@ -39,8 +39,9 @@ class ColumnarModelData(ModelData):
         ((val_df, trn_df), (val_y, trn_y)) = split_by_idx(val_idxs, df, y)
         return self.from_data_frames(path, trn_df, val_df, trn_y, val_y, cat_flds, bs)
 
-    def get_learner(self, emb_szs, n_cont, emb_drop, szs, drops, y_range=None):
-        model = MixedInputModel(emb_szs, n_cont, emb_drop, szs, drops, y_range)
+    def get_learner(self, emb_szs, n_cont, emb_drop, out_sz, szs, drops,
+                    y_range=None, use_bn=False):
+        model = MixedInputModel(emb_szs, n_cont, emb_drop, out_sz, szs, drops, y_range, use_bn)
         return StructuredLearner(self, StructuredModel(model.cuda()), opt_fn=optim.Adam)
 
 
@@ -51,7 +52,8 @@ def emb_init(x):
 
 
 class MixedInputModel(nn.Module):
-    def __init__(self, emb_szs, n_cont, emb_drop, szs, drops, y_range=None):
+    def __init__(self, emb_szs, n_cont, emb_drop, out_sz, szs, drops,
+                 y_range=None, use_bn=False):
         super().__init__()
         self.embs = nn.ModuleList([nn.Embedding(c, s) for c,s in emb_szs])
         for emb in self.embs: emb_init(emb)
@@ -60,22 +62,28 @@ class MixedInputModel(nn.Module):
         szs = [n_emb+n_cont] + szs
         self.lins = nn.ModuleList([
             nn.Linear(szs[i], szs[i+1]) for i in range(len(szs)-1)])
+        self.bns = nn.ModuleList([
+            nn.BatchNorm1d(sz) for sz in szs[1:]])
         for o in self.lins: kaiming_normal(o.weight.data)
+        self.outp = nn.Linear(szs[-1], out_sz)
+        kaiming_normal(self.outp.weight.data)
 
         self.emb_drop = nn.Dropout(emb_drop)
-        self.drops = [nn.Dropout(drop) for drop in drops]
-        self.drops = nn.ModuleList(self.drops + [None])
-        self.cont_lin = nn.Linear(n_cont,n_cont)
-        self.y_range = y_range
+        self.drops = nn.ModuleList([nn.Dropout(drop) for drop in drops])
+        self.bn = nn.BatchNorm1d(n_cont)
+        self.use_bn,self.y_range = use_bn,y_range
 
     def forward(self, x_cat, x_cont):
         x = [e(x_cat[:,i]) for i,e in enumerate(self.embs)]
         x = torch.cat(x, 1)
-        x2 = F.relu(self.cont_lin(x_cont))
-        x = self.emb_drop(torch.cat([x, x2], 1))
-        for l,d in zip(self.lins, self.drops):
+        x2 = self.bn(x_cont)
+        x = self.emb_drop(x)
+        x = torch.cat([x, x2], 1)
+        for l,d,b in zip(self.lins, self.drops, self.bns):
             x = F.relu(l(x))
-            if d: x = d(x)
+            if self.use_bn: x = b(x)
+            x = d(x)
+        x = self.outp(x)
         if self.y_range:
             x = F.sigmoid(x)
             x = x*(self.y_range[1] - self.y_range[0])
@@ -89,7 +97,9 @@ class StructuredLearner(Learner):
         self.crit = F.mse_loss
 
 class StructuredModel(BasicModel):
-    def get_layer_groups(self): return [self.model.embs, self.model.lins]
+    def get_layer_groups(self):
+        m=self.model
+        return [m.embs, children(m.lins)+children(m.bns), m.outp]
 
 
 class CollabFilterDataset(Dataset):
