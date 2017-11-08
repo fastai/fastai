730e599449b61247b93de4a476a635b638999a43
Wayde Gilliam <wgilliam@ohmeow.com>
2017-11-08 10:01:39 -0800

    add documentation for lr_find()

--- fastai/learner.py
+++ fastai/learner.py
@@ -93,6 +93,34 @@ class Learner():
         self.fit_gen(self.model, self.data, layer_opt, n_cycle, **kwargs)
 
     def lr_find(self, start_lr=1e-5, end_lr=10, wds=None):
+        """Helps you find an optimal learning rate for a model.
+
+         It uses the technique developed in the 2015 paper 
+         `Cyclical Learning Rates for Training Neural Networks`, where 
+         we simply keep increasing the learning rate from a very small value, 
+         until the loss starts decreasing.
+
+        Args:
+            start_lr (float): The starting learning rate
+            end_lr (float): The maximum learning rate to try.
+            wds (iterable or float): TO DO!
+
+        Examples:
+            As training moves us closer to the optimal weights for a model,
+            the optimal learning rate will be smaller. We can take advantage of
+            that knowledge and provide lr_find() with a starting learning rate
+            1000x smaller than the model's current learning rate as such:
+
+            >> learn.lr_find(lr/1000)
+
+        Notes:
+            lr_find() may finish before going through each batch of examples if
+            the loss decreases enough.
+
+        .. _Cyclical Learning Rates for Training Neural Networks:
+            http://arxiv.org/abs/1506.01186
+
+        """
         self.save('tmp')
         layer_opt = self.get_layer_opt(start_lr, wds)
         self.sched = LR_Finder(layer_opt, len(self.data.trn_dl), end_lr)
