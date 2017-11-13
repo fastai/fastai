defd28ff336b62315095e99b87539054ae4a6011
Hiromi Suenaga <hiromis@gmail.com>
2017-11-13 15:33:25 +0900

    Added docstring to TTA function

--- fastai/learner.py
+++ fastai/learner.py
@@ -145,6 +145,23 @@ class Learner():
     def predict_array(self, arr): return to_np(self.model(V(T(arr).cuda())))
 
     def TTA(self, n_aug=4, is_test=False):
+        """ Predict with Test Time Augmentation (TTA)
+
+        Additional to the original test/validation images, apply image augmentation to them
+        (just like for training images) and calculate the mean of predictions. The intent
+        is to increase the accuracy of predictions by examining the images using multiple
+        perspectives.
+
+        Args:
+            n_aug: a number of augmentation images to use per original image
+            is_test: indicate to use test images; otherwise use validation images
+
+        Returns:
+            (tuple): a tuple containing:
+
+                log predictions (numpy.ndarray): log predictions (i.e. `np.exp(log_preds)` will return probabilities)
+                targs (numpy.ndarray): target values when `is_test==False`; zeros otherwise.
+        """
         dl1 = self.data.test_dl     if is_test else self.data.val_dl
         dl2 = self.data.test_aug_dl if is_test else self.data.aug_dl
         preds1,targs = predict_with_targs(self.model, dl1)
