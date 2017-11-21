cff9cc71557a382c4406965e99efb3fc22d8776b
Ramesh Sampath <ramesh@sampathweb.com>
2017-11-21 11:50:26 -0800

    Bug: get_cv_idxs error when val_pct > 0.3

--- fastai/dataset.py
+++ fastai/dataset.py
@@ -5,12 +5,15 @@ from .transforms import *
 from .layer_optimizer import *
 from .dataloader import DataLoader
 
-def get_cv_idxs(n, cv_idx=4, val_pct=0.2, seed=42):
+def get_cv_idxs(n, cv_idx=None, val_pct=0.2, seed=42):
     np.random.seed(seed)
+    if cv_idx is None: cv_idx=int((1-val_pct)/val_pct)
     n_val = int(val_pct*n)
     idx_start = cv_idx*n_val
     idxs = np.random.permutation(n)
-    return idxs[idx_start:idx_start+n_val]
+    val_idxs = idxs[idx_start:idx_start+n_val]
+    if len(val_idxs) == 0: print("Warning: No Validation Indexes Selected.")
+    return val_idxs
 
 def resize_img(fname, targ, path, new_path):
     dest = os.path.join(path,new_path,str(targ),fname)
