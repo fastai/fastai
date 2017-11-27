3f4134073fac9eb9eb09c34760b53db6159b0718
Jeremy Howard <j@fast.ai>
2017-11-27 20:43:35 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -25,31 +25,32 @@ setup(
     version = 1.0 ,
 
     description = "The fast.ai deep learning and machine learning library. Git pull fastai, for all fast.ai sessions and tutorials also",
-    
-    author = "Jeremy Howard, Rachel Thomas, Yannet Interian and many others",
-    
-    author_email = "j@fast.ai",
+
+    author = "Jeremy Howard and contributors"
+
+    author_email = "info@fast.ai",
 
     license = "GNU General Public License",
-    
+
     url = "https://github.com/fastai/fastai",
-    
+
     download_url =  'https://github.com/fastai/fastai/archive/1.0.tar.gz',
 
-    install_requires = 
+    install_requires =
      ['awscli',
-     'bcolz',     'bleach',     'certifi',     'cycler',     'decorator',
-    # 'docrepr' ,     'entrypoints',
+     'bcolz',     'bleach',     'certifi',     'cycler',     'decorator',
+    # 'docrepr' ,
+     'entrypoints',
      'feather-format',
-     'graphviz',     'html5lib',     'ipykernel',     'ipython',     'ipython-genutils',     'ipywidgets',     'isoweek',     'jedi',     'Jinja2',     'jsonschema',     'jupyter',     'jupyter-client',     'jupyter-console',
-     'jupyter_contrib_nbextensions',     'jupyter-core',
-     'kaggle-cli',     'MarkupSafe',     'matplotlib',     'mistune',     'nbconvert',     'nbformat',     'notebook',     'numpy',     'olefile',     'opencv-python',     'pandas',     'pandas_summary',     'pandocfilters',     'pexpect',     'pickleshare',     'Pillow',
-     'plotnine',     'prompt-toolkit',     'ptyprocess',     'Pygments',     'pyparsing',     'python-dateutil',     'pytz',     'PyYAML',     'pyzmq',     'qtconsole',     'scipy',     'seaborn',     'simplegeneric',     'six',
-     'sklearn_pandas',     'terminado',     'testpath',
-     'torchtext',     'tornado',     'tqdm',     'traitlets',     'wcwidth',     'webencodings',     'widgetsnbextension'],
+     'graphviz',     'html5lib',     'ipykernel',     'ipython',     'ipython-genutils',     'ipywidgets',     'isoweek',     'jedi',     'Jinja2',     'jsonschema',     'jupyter',     'jupyter-client',     'jupyter-console',
+     'jupyter_contrib_nbextensions',     'jupyter-core',
+     'kaggle-cli',     'MarkupSafe',     'matplotlib',     'mistune',     'nbconvert',     'nbformat',     'notebook',     'numpy',     'olefile',     'opencv-python',     'pandas',     'pandas_summary',     'pandocfilters',     'pexpect',     'pickleshare',     'Pillow',
+     'plotnine',     'prompt-toolkit',     'ptyprocess',     'Pygments',     'pyparsing',     'python-dateutil',     'pytz',     'PyYAML',     'pyzmq',     'qtconsole',     'scipy',     'seaborn',     'simplegeneric',     'six',
+     'sklearn_pandas',     'terminado',     'testpath',
+     'torchtext',     'tornado',     'tqdm',     'traitlets',     'wcwidth',     'webencodings',     'widgetsnbextension'],
 
 
-   
+
     keywords = ['deeplearning', 'pytorch', 'machinelearning'],
 
         classifiers = ['Development Status :: 3 - Alpha',
