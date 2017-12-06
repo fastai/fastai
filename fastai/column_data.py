48c514f84d10cdbbd5d5800d599545c7c0edda79
Ramesh Sampath <ramesh@sampathweb.com>
2017-12-06 08:19:26 -0800

    Add test_dl to from_arrays API of ColumnarModelData

--- fastai/column_data.py
+++ fastai/column_data.py
@@ -48,7 +48,7 @@ class ColumnarModelData(ModelData):
             DataLoader(val_ds, bs*2, shuffle=False, num_workers=1), test_dl)
 
     @classmethod
-    def from_arrays(cls, path, val_idxs, xs, y, bs=64, shuffle=True, test_xs=None):
+    def from_arrays(cls, path, val_idxs, xs, y, bs=64, test_xs=None, shuffle=True):
         ((val_xs, trn_xs), (val_y, trn_y)) = split_by_idx(val_idxs, xs, y)
         test_ds = PassthruDataset(*(test_xs.T), [0] * len(test_xs)) if test_xs is not None else None
         return cls(path, PassthruDataset(*(trn_xs.T), trn_y), PassthruDataset(*(val_xs.T), val_y),
