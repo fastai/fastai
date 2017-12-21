4dd6ed2b2180da6ea83481521a19fed78fded613
Jeremy Howard <j@fast.ai>
2017-12-21 01:19:02 +0000

    cuda 9

--- fastai/nlp.py
+++ fastai/nlp.py
@@ -11,11 +11,14 @@ from sklearn.model_selection import train_test_split
 from torchtext.datasets import language_modeling
 
 import spacy
+from spacy.symbols import ORTH
 
-re_br = re.compile('<br />')
-spacy_en = spacy.load('en')
+re_br = re.compile(r'<\s*br\s*/?>', re.IGNORECASE)
 def sub_br(x): return re_br.sub("\n", x)
-def spacy_tok(x): return [tok.text for tok in spacy_en.tokenizer(sub_br(x))]
+
+my_tok = spacy.load('en')
+my_tok.tokenizer.add_special_case('<eos>', [{ORTH: '<eos>'}])
+def spacy_tok(x): return [tok.text for tok in my_tok.tokenizer(sub_br(x))]
 
 re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
 def tokenize(s): return re_tok.sub(r' \1 ', s).split()
@@ -199,7 +202,7 @@ class ConcatTextDatasetFromDataFrames(torchtext.data.Dataset):
         fields = [('text', text_field)]
         text = []
 
-        text += text_field.preprocess(df[col].str.cat(sep=' '))
+        text += text_field.preprocess(df[col].str.cat(sep=' <eos> '))
         if (newline_eos): text.append('<eos>')
 
         examples = [torchtext.data.Example.fromlist([text], fields)]
@@ -353,7 +356,8 @@ class TextData(ModelData):
     def from_splits(cls, path, splits, bs, text_name='text', label_name='label'):
         text_fld = splits[0].fields[text_name]
         label_fld = splits[0].fields[label_name]
-        label_fld.build_vocab(splits[0])
+        if hasattr(label_fld, 'build_vocab'):
+            label_fld.build_vocab(splits[0])
         iters = torchtext.data.BucketIterator.splits(splits, batch_size=bs)
         trn_iter,val_iter,test_iter = iters[0],iters[1],None
         test_dl = None
@@ -366,12 +370,13 @@ class TextData(ModelData):
         obj.bs = bs
         obj.pad_idx = text_fld.vocab.stoi[text_fld.pad_token]
         obj.nt = len(text_fld.vocab)
-        obj.c = len(label_fld.vocab)
+        obj.c = (len(label_fld.vocab) if hasattr(label_fld, 'vocab')
+                 else len(getattr(splits[0][0], label_name)))
         return obj
 
     def get_model(self, opt_fn, max_sl, bptt, emb_sz, n_hid, n_layers, **kwargs):
-        m = get_rnn_classifer(max_sl, bptt, self.bs, self.c, self.nt, emb_sz=emb_sz, n_hid=n_hid, n_layers=n_layers,
-                              pad_token=self.pad_idx, **kwargs)
+        m = get_rnn_classifer(max_sl, bptt, self.bs, self.c, self.nt,
+              emb_sz=emb_sz, n_hid=n_hid, n_layers=n_layers, pad_token=self.pad_idx, **kwargs)
         model = TextModel(to_gpu(m))
         return RNN_Learner(self, model, opt_fn=opt_fn)
 
