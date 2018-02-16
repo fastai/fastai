866597823d89ebd4d2c04bde94eaf63b97122f0f
nok <channoklam@yahoo.com.hk>
2018-02-17 00:55:06 +0800

    Change accuracy_multi function to match datatype

--- fastai/metrics.py
+++ fastai/metrics.py
@@ -13,5 +13,6 @@ def accuracy_thresh(thresh):
     return lambda preds,targs: accuracy_multi(preds, targs, thresh)
 
 def accuracy_multi(preds, targs, thresh):
-    return ((preds>thresh)==targs).float().mean()
+    return ((preds>thresh).float()==targs).float().mean()
+
 
