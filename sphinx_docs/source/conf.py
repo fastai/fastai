db968f44fcb855b950a69899ef5256b8463b62d7
paperspace <cuddlecuddleai@gmail.com>
2018-03-28 15:36:52 -0400

    move sphinx documentation location

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -40,11 +40,9 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- sphinx_docs/source/index.rst
+++ sphinx_docs/source/index.rst
@@ -1,5 +1,5 @@
 .. fastai documentation master file, created by
-   sphinx-quickstart on Wed Mar 28 15:16:21 2018.
+   sphinx-quickstart on Wed Mar 28 15:35:19 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
