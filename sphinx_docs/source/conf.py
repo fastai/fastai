a2602ea17048ef558144ecc804e16dfee9eccac8
paperspace <cuddlecuddleai@gmail.com>
2018-03-28 15:38:33 -0400

    adding path in sphinx config

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -11,10 +11,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
