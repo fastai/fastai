c112c3f4fa7fa4f867e989c11fa5b7cd491294bb
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-03-28 17:34:59 -0700

    Style changes to threadpool executor. Renamed lazy_threadpool_executor to executors

--- fastai/executors.py
+++ fastai/executors.py
@@ -7,36 +7,23 @@ class LazyThreadPoolExecutor(ThreadPoolExecutor):
     def map(self, fn, *iterables, timeout=None, chunksize=1, prefetch=None):
         """
         Collects iterables lazily, rather than immediately.
-        Docstring same as parent class: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Executor
+        Docstring same as parent: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Executor
         Implmentation taken from this PR: https://github.com/python/cpython/pull/707
         """
-        if timeout is not None:
-            end_time = timeout + time.time()
-        if prefetch is None:
-            prefetch = self._max_workers
-        if prefetch < 0:
-            raise ValueError("prefetch count may not be negative")
-
+        if timeout is not None: end_time = timeout + time.time()
+        if prefetch is None: prefetch = self._max_workers
+        if prefetch < 0: raise ValueError("prefetch count may not be negative")
         argsiter = zip(*iterables)
-
-        fs = collections.deque(self.submit(fn, *args) for args in itertools.islice(argsiter, self._max_workers + prefetch))
-
-        # Yield must be hidden in closure so that the futures are submitted
-        # before the first iterator value is required.
+        fs = collections.deque(self.submit(fn, *args) for args in itertools.islice(argsiter, self._max_workers+prefetch))
+        # Yield must be hidden in closure so that the futures are submitted before the first iterator value is required.
         def result_iterator():
             nonlocal argsiter
             try:
                 while fs:
-                    if timeout is None:
-                        res = fs[0].result()
-                    else:
-                        res = fs[0].result(end_time - time.time())
-
+                    res = fs[0].result() if timeout is None else fs[0].result(end_time-time.time())
                     # Got a result, future needn't be cancelled
                     del fs[0]
-
-                    # Dispatch next task before yielding to keep
-                    # pipeline full
+                    # Dispatch next task before yielding to keep pipeline full
                     if argsiter:
                         try:
                             args = next(argsiter)
@@ -44,9 +31,7 @@ class LazyThreadPoolExecutor(ThreadPoolExecutor):
                             argsiter = None
                         else:
                             fs.append(self.submit(fn, *args))
-
                     yield res
             finally:
-                for future in fs:
-                    future.cancel()
+                for future in fs: future.cancel()
         return result_iterator()
