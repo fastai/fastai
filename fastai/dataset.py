28affb2205fa102d508d728e60255bc811ad07a2
Vibhutha Kumarage <vkartz@gmail.com>
2018-04-06 10:24:16 +0530

    Added docstring to get_cv_idxs()

--- fastai/dataset.py
+++ fastai/dataset.py
@@ -8,6 +8,17 @@ from .layer_optimizer import *
 from .dataloader import DataLoader
 
 def get_cv_idxs(n, cv_idx=0, val_pct=0.2, seed=42):
+    """ Get a list of index values for Validation set from a dataset
+    
+    Arguments:
+        n : int, Total number of elements in the data set.
+        cv_idx : int, starting index [idx_start = cv_idx*int(val_pct*n)] 
+        val_pct : (int, float), validation set percentage 
+        seed : seed value for RandomState
+        
+    Returns:
+        list of indexes 
+    """
     np.random.seed(seed)
     n_val = int(val_pct*n)
     idx_start = cv_idx*n_val
