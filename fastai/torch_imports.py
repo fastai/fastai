924bfa33e11937d34673250133d7df6a11217229
mcskinner <git@mcskinner.com>
2018-04-06 16:57:37 -0700

    initial testing setup for LayerOptimizer

--- fastai/torch_imports.py
+++ fastai/torch_imports.py
@@ -21,7 +21,7 @@ from .models.nasnet import nasnetalarge
 import warnings
 warnings.filterwarnings('ignore', message='Implicit dimension choice', category=UserWarning)
 
-def children(m): return m if isinstance(m, (list, tuple)) else list(m.children())
+def children(m): return m if is_listy(m) else list(m.children())
 def save_model(m, p): torch.save(m.state_dict(), p)
 def load_model(m, p): m.load_state_dict(torch.load(p, map_location=lambda storage, loc: storage))
 
