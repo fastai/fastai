b6fa1bd50edd0d2269dabcfd637ec5df1ee5e07b
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-04-09 08:19:23 -0400

    Clean-up code
    Added a bit of doc, hid the all_val argument of fit in the kwargs and changed the IterBatch class to an iterator (or tried to?)

--- fastai/model.py
+++ fastai/model.py
@@ -87,7 +87,7 @@ def set_train_mode(m):
     else: m.train()
 
 
-def fit(model, data, epochs, opt, crit, metrics=None, callbacks=None, stepper=Stepper, all_val=False, **kwargs):
+def fit(model, data, epochs, opt, crit, metrics=None, callbacks=None, stepper=Stepper, **kwargs):
     """ Fits a model
 
     Arguments:
@@ -106,7 +106,8 @@ def fit(model, data, epochs, opt, crit, metrics=None, callbacks=None, stepper=St
     for cb in callbacks: cb.on_train_begin()
     names = ["epoch", "trn_loss", "val_loss"] + [f.__name__ for f in metrics]
     layout = "{!s:10} " * len(names)
-
+    all_val = kwargs['all_val'] if 'all_val' in kwargs else False
+    
     num_batch = len(data.trn_dl)
     if epochs<1:
         num_batch = int(num_batch*epochs)
@@ -116,7 +117,7 @@ def fit(model, data, epochs, opt, crit, metrics=None, callbacks=None, stepper=St
         stepper.reset(True)
         t = tqdm(iter(data.trn_dl), leave=False, total=num_batch)
         i = 0
-        if all_val: val_iter = iter_batch(data.val_dl)
+        if all_val: val_iter = IterBatch(data.val_dl)
         for (*x,y) in t:
             batch_num += 1
             for cb in callbacks: cb.on_batch_begin()
@@ -148,13 +149,16 @@ def print_stats(epoch, values, decimals=6):
     values = [epoch] + list(np.round(values, decimals))
     print(layout.format(*values))
 
-class iter_batch():
+class IterBatch():
     def __init__(self, dl):
         self.idx = 0
         self.dl = dl
         self.iter = iter(dl)
     
-    def get_next(self):
+    def __iter__(self):
+        return self
+
+    def next(self):
         res = next(self.iter)
         self.idx += 1
         if self.idx == len(self.dl):
@@ -163,8 +167,9 @@ class iter_batch():
         return res 
 
 def validate_next(stepper, metrics, val_iter):
+    """Computes the loss on the next minibatch of the validation set."""
     stepper.reset(False)
-    (*x,y) = val_iter.get_next()
+    (*x,y) = val_iter.next()
     preds,l = stepper.evaluate(VV(x), VV(y))
     res = [to_np(l)[0]]
     res += [f(preds.data,y) for f in metrics]
