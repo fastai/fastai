8563a97214fba4990cbf1c22d9f9c14871b71a7d
mcskinner <git@mcskinner.com>
2018-04-10 12:08:48 -0700

    proposed fix for #319 re language model train/val/test handling

--- fastai/core.py
+++ fastai/core.py
@@ -6,6 +6,7 @@ def sum_geom(a,r,n): return a*n if r==1 else math.ceil(a*(1-r**n)/(1-r))
 def is_listy(x): return isinstance(x, (list,tuple))
 def is_iter(x): return isinstance(x, collections.Iterable)
 def map_over(x, f): return [f(o) for o in x] if is_listy(x) else f(x)
+def map_none(x, f): return None if x is None else f(x)
 
 conv_dict = {np.dtype('int8'): torch.LongTensor, np.dtype('int16'): torch.LongTensor,
     np.dtype('int32'): torch.LongTensor, np.dtype('int64'): torch.LongTensor,
