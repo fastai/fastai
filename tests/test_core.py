bbeb5446373065d6328d2f15f572a454b4bc3968
Nisreen Al-Basha <albasha.nisreen@gmail.com>
2018-05-06 11:20:17 -0400

    Cleans up comments

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,77 +13,30 @@ def test_partition_functionality():
   sz = 2
   ex = [[1,2],[3,4],[5]]
   test_partition(a, sz, ex)
-  # result = partition(a, sz)
-  # assert len(result) == len(ex)
-  # assert all([a == b for a, b in zip(result, ex)])
 
   sz = 3
   ex = [[1,2,3],[4,5]]
   test_partition(a, sz, ex)
-  # result = partition(a, sz)
-  # assert len(result) == len(ex)
-  # assert all([a == b for a,b in zip(result, ex)])
 
   sz = 1
   ex = [[1],[2],[3],[4],[5]]
-  # result = partition(a, sz)
   test_partition(a, sz, ex)
-  # assert len(result) == len(ex)
-  # assert all([a == b for a,b in zip(result, ex)])
 
   sz = 6
   ex = [[1,2,3,4,5]]
   test_partition(a, sz, ex)
-  # result = partition(a, sz)
-  # assert len(result) == len(ex)
-  # assert all([a == b for a,b in zip(result, ex)])
 
   sz = 3
   a = []
   result = partition(a, sz)
   assert len(result) == 0
 
+
 def test_partition_error_handling():
   sz = 0
   a = [1,2,3,4,5]
   with pytest.raises(ValueError):
     partition(a, sz)
-
-# a generator is a fn that returns an iterable
-# to create a generator, define a normal fn with a yield statement instead of a return statement
-# while a return statement terminates a fn entirely, a yield statement pauses the fn, saving all its states
-# a generator contains one or more yield statements
-# when called, it returns an iterator but does not start execution immediately
-
-  # seq = [1,2,3,4,5,6]
-  # idx = [2,4]
-# --> [1,2], [3,4], [5,6]
-# --> split before the second element (3). next, split from the second element up until the 4th (5)
-
-# idx = [2,3]
-# --> [1,2], [3], [4,5,6]
-
-# examples with 3 indices:
-  # seq = [1,2,3,4,5,6]
-  # idx = [2,4,5]
-# --> [1,2], [3,4], [5], [6]
-
-# what are the most useful tests for this function?
-  # 1) 1 index
-  # 2) 2 indices
-  # 3) 3 indices
-  # 4) no indices
-  # 5) all indices
-
-  # for item in enumerate(split_by_idxs(seq, idxs)):
-  #   print('item ---', item)
-  #   result.append(item)
-  # print('result ---', result)
-  # assert len(result) == len(ex)
-  # assert all([a == b for a,b in zip(result, ex)])
-
-  # assert len(result) == len(ex)
-  # assert all([a == b for a,b in zip(result, ex)])
 
 
 def test_split_by_idxs_functionality():
@@ -114,6 +67,7 @@ def test_split_by_idxs_functionality():
   idxs = []
   ex = [[1,2,3,4,5,6]]
   test_split_by_idxs(seq, idxs, ex)
+
 
 def test_split_by_idxs_error_handling():
   seq = [1,2,3,4]
