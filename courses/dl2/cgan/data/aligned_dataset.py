054a40ba7d87ce4c498dff4c13d3de26b7e87be7
Alex <tensoralex@gmail.com>
2018-05-09 21:04:14 -0400

    import fixes for pix2pix part of cgan

--- courses/dl2/cgan/data/aligned_dataset.py
+++ courses/dl2/cgan/data/aligned_dataset.py
@@ -2,8 +2,8 @@ import os.path
 import random
 import torchvision.transforms as transforms
 import torch
-from data.base_dataset import BaseDataset
-from data.image_folder import make_dataset
+from .base_dataset import BaseDataset
+from .image_folder import make_dataset
 from PIL import Image
 
 
