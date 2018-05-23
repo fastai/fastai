fba27d7d63f814cf5a3cda39c4086c65ace6093a
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-05-22 21:41:04 -0400

    Clean-up

--- fastai/layer_optimizer.py
+++ fastai/layer_optimizer.py
@@ -8,11 +8,9 @@ def opt_params(parm, lr, wd):
 class LayerOptimizer():
     def __init__(self, opt_fn, layer_groups, lrs, wds=None):
         if not isinstance(layer_groups, (list,tuple)): layer_groups=[layer_groups]
-        if not isinstance(lrs, Iterable): lrs=[lrs]
-        if len(lrs)==1: lrs=lrs*len(layer_groups)
+        lrs = listify(lrs, layer_groups)
         if wds is None: wds=0.
-        if not isinstance(wds, Iterable): wds=[wds]
-        if len(wds)==1: wds=wds*len(layer_groups)
+        wds = listify(wds, layer_groups)
         self.layer_groups,self.lrs,self.wds = layer_groups,lrs,wds
         self.opt = opt_fn(self.opt_params())
 
@@ -33,21 +31,18 @@ class LayerOptimizer():
             return self.opt.param_groups[0]['momentum']
 
     def set_lrs(self, lrs):
-        if not isinstance(lrs, Iterable): lrs=[lrs]
-        if len(lrs)==1: lrs=lrs*len(self.layer_groups)
+        lrs = listify(lrs, self.layer_groups)
         set_lrs(self.opt, lrs)
         self.lrs=lrs
 
     def set_wds_out(self, wds):
-        if not isinstance(wds, Iterable): wds=[wds]
-        if len(wds)==1: wds=wds*len(self.layer_groups)
+        wds = listify(wds, self.layer_groups)
         set_wds_out(self.opt, wds)
         set_wds(self.opt, [0] * len(self.layer_groups))
         self.wds=wds
 
     def set_wds(self, wds):
-        if not isinstance(wds, Iterable): wds=[wds]
-        if len(wds)==1: wds=wds*len(self.layer_groups)
+        wds = listify(wds, self.layer_groups)
         set_wds(self.opt, wds)
         set_wds_out(self.opt, [0] * len(self.layer_groups))
         self.wds=wds
@@ -73,19 +68,16 @@ def zip_strict_(l, r):
     return zip(l, r)
 
 def set_lrs(opt, lrs):
-    if not isinstance(lrs, Iterable): lrs=[lrs]
-    if len(lrs)==1: lrs=lrs*len(opt.param_groups)
+    lrs = listify(lrs, opt.param_groups)
     for pg,lr in zip_strict_(opt.param_groups,lrs): pg['lr'] = lr
 
 def set_wds_out(opt, wds):
-    if not isinstance(wds, Iterable): wds=[wds]
-    if len(wds)==1: wds=wds*len(opt.param_groups)
+    wds = listify(wds, opt.param_groups)
     assert(len(opt.param_groups) == len(wds))
     for pg,wd in zip_strict_(opt.param_groups,wds): pg['wd'] = wd
 
 def set_wds(opt, wds):
-    if not isinstance(wds, Iterable): wds=[wds]
-    if len(wds)==1: wds=wds*len(opt.param_groups)
+    wds = listify(wds, opt.param_groups)
     assert(len(opt.param_groups) == len(wds))
     for pg,wd in zip_strict_(opt.param_groups,wds): pg['weight_decay'] = wd
 
