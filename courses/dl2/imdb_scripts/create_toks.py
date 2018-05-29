d726e816b1d78bcfe9cb7268bfbdf4e2907108c1
Sebastian <ruder.sebastian@gmail.com>
2018-05-29 15:35:02 +0100

    Replaced prefix with dir_path, used Path, replaced test.csv with val.csv

--- courses/dl2/imdb_scripts/create_toks.py
+++ courses/dl2/imdb_scripts/create_toks.py
@@ -36,30 +36,29 @@ def get_all(df, n_lbls):
     for i, r in enumerate(df):
         print(i)
         tok_, labels_ = get_texts(r, n_lbls)
-        tok += tok_;
+        tok += tok_
         labels += labels_
     return tok, labels
 
 
-def create_toks(prefix, pr_abbr, chunksize=24000, n_lbls=1):
-    PATH = f'data/nlp_clas/{prefix}/'
+def create_toks(dir_path, chunksize=24000, n_lbls=1):
+    dir_path = Path(dir_path)
+    assert dir_path.exists(), f'Error: {dir_path} does not exist.'
+    df_trn = pd.read_csv(dir_path.joinpath('train.csv'), header=None, chunksize=chunksize)
+    df_val = pd.read_csv(dir_path.joinpath('val.csv'), header=None, chunksize=chunksize)
 
-    df_trn = pd.read_csv(f'{PATH}train.csv', header=None, chunksize=chunksize)
-    df_val = pd.read_csv(f'{PATH}test.csv', header=None, chunksize=chunksize)
-    print(prefix)
-
-    os.makedirs(f'{PATH}tmp', exist_ok=True)
+    tmp_path = dir_path.joinpath('tmp')
+    tmp_path.mkdir(exist_ok=True)
     tok_trn, trn_labels = get_all(df_trn, n_lbls)
     tok_val, val_labels = get_all(df_val, n_lbls)
 
-    np.save(f'{PATH}tmp/tok_trn.npy', tok_trn)
-    np.save(f'{PATH}tmp/tok_val.npy', tok_val)
-    np.save(f'{PATH}tmp/lbl_trn.npy', trn_labels)
-    np.save(f'{PATH}tmp/lbl_val.npy', val_labels)
+    np.save(tmp_path.joinpath('tok_trn.npy'), tok_trn)
+    np.save(tmp_path.joinpath('tok_val.npy'), tok_val)
+    np.save(tmp_path.joinpath('lbl_trn.npy'), trn_labels)
+    np.save(tmp_path.joinpath('lbl_val.npy'), val_labels)
 
     trn_joined = [' '.join(o) for o in tok_trn]
-    mdl_fn = f'{PATH}tmp/{pr_abbr}_joined.txt'
-    open(mdl_fn, 'w', encoding='utf-8').writelines(trn_joined)
+    open(tmp_path.joinpath('joined.txt'), 'w', encoding='utf-8').writelines(trn_joined)
 
 
 if __name__ == '__main__': fire.Fire(create_toks)
