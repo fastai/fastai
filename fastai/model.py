fix bug in weighted averaging of validation metrics

fastai/model.py
@@ -99,8 +99,9 @@ def fit(model, data, n_epochs, opt, crit, metrics=None, callbacks=None, stepper=
        crit: loss function to optimize. Example: F.cross_entropy
     """
 
-    all_val = kwargs.pop('all_val') if 'all_val' in kwargs else False
-    get_ep_vals = kwargs.pop('get_ep_vals') if 'get_ep_vals' in kwargs else False
+    seq_first = kwargs.pop('seq_first', False)
+    all_val = kwargs.pop('all_val', False)
+    get_ep_vals = kwargs.pop('get_ep_vals', False)
     metrics = metrics or []
     callbacks = callbacks or []
     avg_mom=0.98
@@ -157,7 +158,7 @@ def fit(model, data, n_epochs, opt, crit, metrics=None, callbacks=None, stepper=
                     break
 
         if not all_val:
-            vals = validate(model_stepper, cur_data.val_dl, metrics)
+            vals = validate(model_stepper, cur_data.val_dl, metrics, seq_first=seq_first)
             stop=False
             for cb in callbacks: stop = stop or cb.on_epoch_end(vals)
             if swa_model is not None:
@@ -210,14 +211,17 @@ def validate_next(stepper, metrics, val_iter):
     stepper.reset(True)
     return res
 
-def validate(stepper, dl, metrics):
+def batch_sz(x, seq_first=False):
+    if is_listy(x): x = x[0]
+    return x.shape[1 if seq_first else 0]
+
+def validate(stepper, dl, metrics, seq_first=False):
     batch_cnts,loss,res = [],[],[]
     stepper.reset(False)
     with no_grad_context():
         for (*x,y) in iter(dl):
             preds, l = stepper.evaluate(VV(x), VV(y))
-            if isinstance(x,list): batch_cnts.append(len(x[0]))
-            else: batch_cnts.append(len(x))
+            batch_cnts.append(batch_sz(x, seq_first=seq_first))
             loss.append(to_np(l))
             res.append([f(preds.data, y) for f in metrics])
     return [np.average(loss, 0, weights=batch_cnts)] + list(np.average(np.stack(res), 0, weights=batch_cnts))