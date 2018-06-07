fe4ab9ee0100f1ea390787beb0b7a1dd82412e61
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-06-06 22:41:30 -0400

    Bug in load_model with RNNs

--- fastai/torch_imports.py
+++ fastai/torch_imports.py
@@ -34,8 +34,8 @@ def load_model(m, p):
     sd = torch.load(p, map_location=lambda storage, loc: storage)
     names = set(m.state_dict().keys())
     for n in list(sd.keys()): # list "detatches" the iterator
-        if n not in names and n+'_raw' in names and n+'_raw' not in sd:
-            sd[n+'_raw'] = sd[n]
+        if n not in names and n+'_raw' in names:
+            if n+'_raw' not in sd: sd[n+'_raw'] = sd[n]
             del sd[n]
     m.load_state_dict(sd)
 
