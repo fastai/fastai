f1432a778601c1b1a95e645dc12f9802802dee06
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-07-19 07:30:47 -0400

    Refactors

--- fastai/transforms.py
+++ fastai/transforms.py
@@ -365,8 +365,7 @@ class Scale(CoordTransform):
 
     def do_transform(self, x, is_y):
         h,w,*_ = x.shape
-        intpr = cv2.INTER_AREA
-        if(self.sz_y < min(h, w)) : intpr = cv2.INTER_LINEAR 
+        intpr = cv2.INTER_LINEAR if(self.sz_y < min(h, w)) else cv2.INTER_AREA
         if is_y: return scale_min(x, self.sz_y, intpr if self.tfm_y == TfmType.PIXEL else cv2.INTER_NEAREST)
         else   : return scale_min(x, self.sz, intpr)
 
