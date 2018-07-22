4f5991d4f69704eeff901508e04ca6e633bce95a
Stas <stas@stason.org>
2018-07-22 15:26:06 -0700

    set identical defaults for is_reg, is_multi, bs in ColumnarModelData from_* methods

--- fastai/column_data.py
+++ fastai/column_data.py
@@ -62,14 +62,14 @@ class ColumnarModelData(ModelData):
                    bs=bs, shuffle=shuffle, test_ds=test_ds)
 
     @classmethod
-    def from_data_frames(cls, path, trn_df, val_df, trn_y, val_y, cat_flds, bs, is_reg, is_multi, test_df=None, shuffle=True):
+    def from_data_frames(cls, path, trn_df, val_df, trn_y, val_y, cat_flds, bs=64, is_reg=True, is_multi=False, test_df=None, shuffle=True):
         trn_ds  = ColumnarDataset.from_data_frame(trn_df,  cat_flds, trn_y, is_reg, is_multi)
         val_ds  = ColumnarDataset.from_data_frame(val_df,  cat_flds, val_y, is_reg, is_multi)
         test_ds = ColumnarDataset.from_data_frame(test_df, cat_flds, None,  is_reg, is_multi) if test_df is not None else None
         return cls(path, trn_ds, val_ds, bs, test_ds=test_ds, shuffle=shuffle)
 
     @classmethod
-    def from_data_frame(cls, path, val_idxs, df, y, cat_flds, bs, is_reg=True, is_multi=False, test_df=None, shuffle=True):
+    def from_data_frame(cls, path, val_idxs, df, y, cat_flds, bs=64, is_reg=True, is_multi=False, test_df=None, shuffle=True):
         ((val_df, trn_df), (val_y, trn_y)) = split_by_idx(val_idxs, df, y)
         return cls.from_data_frames(path, trn_df, val_df, trn_y, val_y, cat_flds, bs, is_reg, is_multi, test_df=test_df, shuffle=shuffle)
 
