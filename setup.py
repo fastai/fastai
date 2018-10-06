401353b29846dfac79b6962686fe0ce8e6a3779a
Stas Bekman <stas@stason.org>
2018-10-06 10:37:02 -0700

    start new dev cycle

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def create_version_file(version):
         f.write("__version__ = '{}'\n".format(version))
 
 # version
-version = '1.0.5'
+version = '1.0.6.dev0'
 create_version_file(version)
 
 with open('README.md') as readme_file:   readme = readme_file.read()
