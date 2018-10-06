1d9e4c426db920cdd98c55486afab5c2d8833858
Jeremy Howard <j@howard.fm>
2018-10-05 22:30:45 -0700

    add rand_pad

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -8,7 +8,7 @@ from fastai.docs import *
 class TestVision():
     def test_mnist_end_to_end(self):
         untar_data(MNIST_PATH)
-        data = image_data_from_folder(MNIST_PATH)
+        data = image_data_from_folder(MNIST_PATH, ds_tfms=(rand_pad(2, 28), []))
         learn = ConvLearner(data, tvm.resnet18, metrics=accuracy)
         learn.fit(1)
         assert accuracy(*learn.get_preds()) > 0.98
