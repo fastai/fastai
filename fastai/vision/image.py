1d371dc5361ec02f41fad05eacfedc3819d21205
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-10-09 13:58:46 -0400

    Change __all__ content

--- fastai/vision/image.py
+++ fastai/vision/image.py
@@ -5,9 +5,8 @@ from io import BytesIO
 import PIL
 
 __all__ = ['Image', 'ImageBBox', 'ImageBase', 'ImageMask', 'RandTransform', 'TfmAffine', 'TfmCoord', 'TfmCrop', 'TfmLighting',
-           'TfmPixel', 'Transform', 'affine_grid', 'affine_mult', 'apply_tfms', 'bb2hw', 'get_crop_target', 'get_default_args',
-           'get_resize_target', 'grid_sample', 'image2np', 'log_uniform', 'logit', 'logit_', 'pil2tensor', 'rand_bool', 'rand_crop',
-           'resolve_tfms', 'round_multiple', 'show_image', 'uniform', 'uniform_int']
+           'TfmPixel', 'Tfms', 'Transform', 'apply_tfms', 'bb2hw', 'image2np', 'log_uniform', 'logit', 'logit_', 'pil2tensor', 
+           'rand_bool', 'uniform', 'uniform_int']
 
 def logit(x:Tensor)->Tensor:  return -(1/x-1).log()
 def logit_(x:Tensor)->Tensor: return (x.reciprocal_().sub_(1)).log_().neg_()
