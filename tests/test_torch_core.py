e51a01514291820be2a666ee07b2ef902c57bdd4
Jeremy Howard <j@howard.fm>
2018-10-11 07:30:08 -0700

    skip slow tests by default; replace tensor() with something that avoids clone() where possible

--- tests/test_torch_core.py
+++ tests/test_torch_core.py
@@ -9,3 +9,24 @@ def test_show_install(capsys):
     assert match
     match = re.findall(rf'torch\s+: {torch.__version__}',   captured.out)
     assert match
+
+
+a=[1,2,3]
+exp=torch.tensor(a)
+
+def test_tensor_with_list():
+    r = tensor(a)
+    assert torch.all(r==exp)
+
+def test_tensor_with_ndarray():
+    b=np.array(a)
+    r = tensor(b)
+    assert np_address(r.numpy()) == np_address(b)
+    assert torch.all(r==exp)
+
+def test_tensor_with_tensor():
+    c=torch.tensor(a)
+    r = tensor(c)
+    assert r.data_ptr()==c.data_ptr()
+    assert torch.all(r==exp)
+
