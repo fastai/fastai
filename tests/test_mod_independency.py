936cb50546e467a7980a5058c9f8f53a1c26eed9
Stas Bekman <stas@stason.org>
2018-10-13 16:44:51 -0700

    cleanup

--- tests/test_mod_independency.py
+++ tests/test_mod_independency.py
@@ -3,24 +3,24 @@
 import sys
 
 # packages that shouldn't be required to be installed for fastai to work:
-unwated_deps = ['jupyter_contrib_nbextensions']
+unwanted_deps = ['jupyter_contrib_nbextensions']
 
 class CheckDependencyImporter(object):
 
     def find_spec(self, fullname, path, target=None):
         #print("spec: ", fullname, path, target)
-        # catch if any of the undesirable dependencies gets triggered
-        for mod in unwated_deps:
+        # catch if any of the unwanted dependencies gets triggered
+        for mod in unwanted_deps:
             if mod == fullname:
-                print(f"detected undesirable dependency on {mod}")
+                print(f"detected unwanted dependency on {mod}")
                 sys.exit(1)
         return None
 
 sys.meta_path.insert(0, CheckDependencyImporter())
 
-def test_undesirable_dependencies():
+def test_unwanted_mod_dependencies():
     # unload any candidates we want to test, including fastai, so we can test import
-    for mod in unwated_deps + ['fastai']:
+    for mod in unwanted_deps + ['fastai']:
         if mod in sys.modules: del sys.modules[mod]
     # test
     import fastai
