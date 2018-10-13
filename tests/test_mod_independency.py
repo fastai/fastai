866922ae08fc4878a111a2552112cfd92f7eb3b4
Stas Bekman <stas@stason.org>
2018-10-13 16:42:33 -0700

    test to check that the core fastai library doesn't depend on unnecessary modules

---
+++ tests/test_mod_independency.py
@@ -0,0 +1,26 @@
+# this test checks that the core fastai library doesn't depend on unnecessary modules.
+
+import sys
+
+# packages that shouldn't be required to be installed for fastai to work:
+unwated_deps = ['jupyter_contrib_nbextensions']
+
+class CheckDependencyImporter(object):
+
+    def find_spec(self, fullname, path, target=None):
+        #print("spec: ", fullname, path, target)
+        # catch if any of the undesirable dependencies gets triggered
+        for mod in unwated_deps:
+            if mod == fullname:
+                print(f"detected undesirable dependency on {mod}")
+                sys.exit(1)
+        return None
+
+sys.meta_path.insert(0, CheckDependencyImporter())
+
+def test_undesirable_dependencies():
+    # unload any candidates we want to test, including fastai, so we can test import
+    for mod in unwated_deps + ['fastai']:
+        if mod in sys.modules: del sys.modules[mod]
+    # test
+    import fastai
