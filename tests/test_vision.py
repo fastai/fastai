bd7c15a4ab1762ddfe277bf68dedd93f6a12b8e2
Jeremy Howard <j@howard.fm>
2018-10-14 13:15:36 -0700

    fix tests

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -2,33 +2,32 @@ import pytest
 from fastai import *
 from fastai.vision import *
 
-path = Paths.MNIST_TINY
-
 @pytest.fixture(scope="module")
-def data(request):
-    untar_data(path)
+def path(request):
+    path = untar_data(URLs.MNIST_TINY)
     d = defaults.device
     defaults.device = torch.device('cpu')
     def _final(): defaults.device = d
     request.addfinalizer(_final)
+    return path
 
-def test_multi_iter_broken():
+def test_multi_iter_broken(path):
     data = image_data_from_folder(path, ds_tfms=(rand_pad(2, 28), []))
     for i in range(5): x,y = next(iter(data.train_dl))
 
-def test_multi_iter():
+def test_multi_iter(path):
     data = image_data_from_folder(path, ds_tfms=(rand_pad(2, 28), []))
     data.normalize()
     for i in range(5): x,y = data.train_dl.one_batch()
 
-def test_clean_tear_down():
+def test_clean_tear_down(path):
     docstr = "test DataLoader iter doesn't get stuck"
     data = image_data_from_folder(path, ds_tfms=(rand_pad(2, 28), []))
     data.normalize()
     data = image_data_from_folder(path, ds_tfms=(rand_pad(2, 28), []))
     data.normalize()
 
-def test_normalize():
+def test_normalize(path):
     data = image_data_from_folder(path, ds_tfms=(rand_pad(2, 28), []))
     x,y = data.train_dl.one_batch()
     m,s = x.mean(),x.std()
