c30cf413afd875fb5b5470b972b03409561ed0b1
Stas Bekman <stas@stason.org>
2018-10-15 20:57:54 -0700

    reorganize the outputs and add info on python location and sys.path

--- fastai/utils/collect_env.py
+++ fastai/utils/collect_env.py
@@ -4,29 +4,22 @@ from ..core import *
 
 __all__ = ['show_install']
 
+def get_env(name):
+    "Return env var value if it's defined and not an empty string, or return Unknown"
+    if name in os.environ and len(os.environ[name]):
+        return os.environ[name]
+    else:
+        return "Unknown"
+
 def show_install(show_nvidia_smi:bool=False):
     "Print user's setup information: python -c 'import fastai; fastai.show_install()'"
 
     import platform, fastai.version, subprocess
 
     rep = []
-    rep.append(["platform", platform.platform()])
-
     opt_mods = []
 
-    if platform.system() == 'Linux':
-        try:
-            import distro
-        except ImportError:
-            opt_mods.append('distro');
-            # partial distro info
-            rep.append(["distro", platform.uname().version])
-        else:
-            # full distro info
-            rep.append(["distro", ' '.join(distro.linux_distribution())])
-
-    cenv = 'CONDA_DEFAULT_ENV'
-    rep.append(["conda env", (os.environ[cenv] if (cenv in os.environ and len(os.environ[cenv])) else "Unknown")])
+    rep.append(["=== Software ===", None])
 
     rep.append(["python version", platform.python_version()])
     rep.append(["fastai version", fastai.__version__])
@@ -53,16 +46,14 @@ def show_install(show_nvidia_smi:bool=False):
         match = re.findall(r'Driver Version: +(\d+\.\d+)', smi)
         if match: rep.append(["nvidia driver", match[0]])
 
+    rep.append(["torch cuda ver", torch.version.cuda])
     rep.append(["torch cuda is",
                 f"{'' if torch.cuda.is_available() else 'Not '}available"])
-    rep.append(["torch cuda ver", torch.version.cuda])
 
     # disable this info for now, seems to be available even on cpu-only systems
     #rep.append(["cudnn", torch.backends.cudnn.version()])
     #rep.append(["cudnn avail", torch.backends.cudnn.enabled])
 
-    gpu_cnt = torch.cuda.device_count()
-    rep.append(["torch gpus", gpu_cnt])
 
     # it's possible that torch might not see what nvidia-smi sees?
     gpu_total_mem = []
@@ -77,15 +68,39 @@ def show_install(show_nvidia_smi:bool=False):
                 output = result.stdout.decode('utf-8')
                 gpu_total_mem = [int(x) for x in output.strip().split('\n')]
 
-    # information for each gpu
-    for i in range(gpu_cnt):
-        rep.append([f"  [gpu{i}]", None])
-        rep.append(["  name", torch.cuda.get_device_name(i)])
-        if gpu_total_mem: rep.append(["  total mem", f"{gpu_total_mem[i]}MB"])
+    gpu_cnt = torch.cuda.device_count()
+    rep.append(["\n=== Hardware ===", None])
+    if gpu_cnt:
+        rep.append(["torch gpus", gpu_cnt])
+        # information for each gpu
+        for i in range(gpu_cnt):
+            rep.append([f"  gpu{i}", (f"{gpu_total_mem[i]}MB | " if gpu_total_mem else "") + torch.cuda.get_device_name(i)])
+    else:
+        rep.append([f"No GPUs", None])
+
+
+    rep.append(["\n=== Environment ===", None])
+
+    rep.append(["platform", platform.platform()])
+
+    if platform.system() == 'Linux':
+        try:
+            import distro
+        except ImportError:
+            opt_mods.append('distro');
+            # partial distro info
+            rep.append(["distro", platform.uname().version])
+        else:
+            # full distro info
+            rep.append(["distro", ' '.join(distro.linux_distribution())])
+
+    rep.append(["conda env", get_env('CONDA_DEFAULT_ENV')])
+    rep.append(["python", sys.executable])
+    rep.append(["sys.path", "\n".join(sys.path)])
 
     print("\n\n```")
 
-    keylen = max([len(e[0]) for e in rep])
+    keylen = max([len(e[0]) for e in rep if e[1] is not None])
     for e in rep:
         print(f"{e[0]:{keylen}}", (f": {e[1]}" if e[1] is not None else ""))
 
