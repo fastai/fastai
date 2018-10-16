cbdead10ebd35499e50f827dba3a08516258bbbe
Stas Bekman <stas@stason.org>
2018-10-15 23:04:38 -0700

    sync the test with the output format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,8 @@ from fastai.utils.collect_env import *
 def test_show_install(capsys):
     show_install()
     captured = capsys.readouterr()
-    #print(captured.out)
-    match = re.findall(rf'fastai\s+: {fastai.__version__}', captured.out)
+    print(captured.out)
+    match = re.findall(rf'fastai version\s+: {fastai.__version__}', captured.out)
     assert match
-    match = re.findall(rf'torch\s+: {torch.__version__}',   captured.out)
+    match = re.findall(rf'torch version\s+: {torch.__version__}',   captured.out)
     assert match
