4d415339065e193695488d8b641d8021f9b043dd
Stas Bekman <stas@stason.org>
2018-10-15 23:05:34 -0700

    cleanup

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from fastai.utils.collect_env import *
 def test_show_install(capsys):
     show_install()
     captured = capsys.readouterr()
-    print(captured.out)
+    #print(captured.out)
     match = re.findall(rf'fastai version\s+: {fastai.__version__}', captured.out)
     assert match
     match = re.findall(rf'torch version\s+: {torch.__version__}',   captured.out)
