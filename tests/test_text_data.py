19fd3ed31cd9cacfc0b72b1877cb76a584cf8d73
Martin Boyanov <mboyanov@sigm.ai>
2018-10-18 12:11:01 +0300

    Avoid dependency on spacy models for the test by adding a DummyTokenizer

--- tests/test_text_data.py
+++ tests/test_text_data.py
@@ -1,12 +1,17 @@
-from fastai.text.data import LanguageModelLoader, TextDataset
+from fastai.text.data import LanguageModelLoader, TextDataset, Tokenizer
 import pandas as pd
 import numpy as np
 
 
+class DummyTokenizer(Tokenizer):
+
+    def process_all(self, texts):
+        return [t.split() for t in texts]
+
 def test_should_load_backwards_lm():
     # GIVEN
-    df = pd.DataFrame([{0: 0, "text": " fast ai is a cool project"}, {0: 0, 'text': "hello world"}])
-    text_ds = TextDataset.from_df('/tmp/', df, label_cols=[0], txt_cols=["text"], min_freq=0)
+    df = pd.DataFrame([{0: 0, "text": "fast ai is a cool project"}, {0: 0, 'text': "hello world"}])
+    text_ds = TextDataset.from_df('/tmp/', df, label_cols=[0], txt_cols=["text"], min_freq=0, tokenizer=DummyTokenizer())
     # WHEN
     lml = LanguageModelLoader(text_ds, bs=1, backwards=True)
     # THEN
