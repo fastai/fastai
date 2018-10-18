95f44e82b589c6985dcbe1178f144a0e36d36f0d
Jeremy Howard <j@howard.fm>
2018-10-18 14:15:34 -0700

    update URLs

--- fastai/__init__.py
+++ fastai/__init__.py
@@ -9,6 +9,5 @@ from .torch_core import *
 from .train import *
 from .gen_doc.nbdoc import doc
 from .datasets import *
-from . import datasets
 from .utils.collect_env import *
 from .version import __version__
