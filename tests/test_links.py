b8310cdeb2db31cd26717780fa7fd723f860b12d
Jeremy Howard <j@howard.fm>
2018-10-23 12:05:23 -0700

    fix test_links

--- tests/test_links.py
+++ tests/test_links.py
@@ -18,7 +18,7 @@ def test_torchvision():
 
 def test_fastai_prefix():
     docstr   = "functions for your application (`fastai.vision`)"
-    expected = "functions for your application ([`fastai.vision`](/vision#vision))"
+    expected = "functions for your application ([`fastai.vision`](/vision.html#vision))"
     assert_link(docstr, expected, msg='Should match keywords prefixed with fastai. See `index.ipynb`')
 
 def test_link_typedef():
@@ -33,39 +33,39 @@ def test_link_typedef_double_bt():
 
 def test_link_inner_class_functions():
     docstr   = "To train your model in mixed precision you just have to call `Learner.to_fp16`, which converts the model and modifies the existing `Learner` to add `MixedPrecision`."
-    expected = "To train your model in mixed precision you just have to call [`Learner.to_fp16`](/train#to_fp16), which converts the model and modifies the existing [`Learner`](/basic_train#Learner) to add [`MixedPrecision`](/callbacks.fp16#MixedPrecision)."
+    expected = "To train your model in mixed precision you just have to call [`Learner.to_fp16`](/train.html#to_fp16), which converts the model and modifies the existing [`Learner`](/basic_train.html#Learner) to add [`MixedPrecision`](/callbacks.fp16.html#MixedPrecision)."
     imports = 'from fastai.callbacks.fp16 import *'
     assert_link(docstr, expected, nb_cells=[gen_notebooks.get_code_cell(imports)])
 
 def test_class_anchor():
     docstr   = "`DataBunch.create`, `DeviceDataLoader.proc_batch`"
-    expected = "[`DataBunch.create`](/basic_data#DataBunch.create), [`DeviceDataLoader.proc_batch`](/basic_data#DeviceDataLoader.proc_batch)"
+    expected = "[`DataBunch.create`](/basic_data.html#DataBunch.create), [`DeviceDataLoader.proc_batch`](/basic_data.html#DeviceDataLoader.proc_batch)"
     imports = 'from fastai.basic_train import *'
     assert_link(docstr, expected, nb_cells=[gen_notebooks.get_code_cell(imports)])
 
 def test_link_class_methods():
     docstr   = "`ImageDataBunch.from_csv`"
-    expected = "[`ImageDataBunch.from_csv`](/vision.data#ImageDataBunch.from_csv)"
+    expected = "[`ImageDataBunch.from_csv`](/vision.data.html#ImageDataBunch.from_csv)"
     imports = 'from fastai.vision.data import *'
     assert_link(docstr, expected, nb_cells=[gen_notebooks.get_code_cell(imports)])
 
 def test_respects_import_order():
     docstr   = "`learner`"
-    expected = "[`learner`](/vision.learner#vision.learner)"
+    expected = "[`learner`](/vision.learner.html#vision.learner)"
     assert_link(docstr, expected, build_nb_cells(['fastai.text', 'fastai.vision']))
 
-    expected_text = "[`learner`](/text.learner#text.learner)"
+    expected_text = "[`learner`](/text.learner.html#text.learner)"
     assert_link(docstr, expected_text, build_nb_cells(['fastai.vision', 'fastai.text']))
 
 def test_nb_module_name_has_highest_priority():
     # get_imported_modules.nb_module_name should have highest priority. This is the associated notebook module.
     # Ex: vision.transforms.ipynb is associated with fastai.vision.transforms
     docstr   = "`transform`"
-    expected = "[`transform`](/text.transform#text.transform)"
+    expected = "[`transform`](/text.transform.html#text.transform)"
     modules = gen_notebooks.get_imported_modules([], nb_module_name='fastai.text.transform')
     assert_link(docstr, expected, modules=modules)
 
-    expected = "[`transform`](/tabular.transform#tabular.transform)"
+    expected = "[`transform`](/tabular.transform.html#tabular.transform)"
     modules = gen_notebooks.get_imported_modules([], nb_module_name='fastai.tabular.transform')
     assert_link(docstr, expected, modules=modules)
 
@@ -78,11 +78,11 @@ In each case (except for `collab`), the module is organized this way:
 ### `models`
 ### `learner`"""
     expected = """## Module structure
-In each case (except for [`collab`](/collab#collab)), the module is organized this way:
-### [`transform`](/text.transform#text.transform)
-### [`data`](/text.data#text.data)
-### [`models`](/text.models#text.models)
-### [`learner`](/text.learner#text.learner)"""
+In each case (except for [`collab`](/collab.html#collab)), the module is organized this way:
+### [`transform`](/text.transform.html#text.transform)
+### [`data`](/text.data.html#text.data)
+### [`models`](/text.models.html#text.models)
+### [`learner`](/text.learner.html#text.learner)"""
     assert_link(docstr, expected, msg='data, models should link to highest module. transform and learner links to first match')
 
 def test_link_vision_learner_priority():
@@ -93,7 +93,7 @@ def test_link_vision_learner_priority():
     """
 
     docstr   = "Pass in your `data`, calculated `preds`, actual `y`,"
-    expected = "Pass in your [`data`](/vision.data#vision.data), calculated `preds`, actual `y`,"
+    expected = "Pass in your [`data`](/vision.data.html#vision.data), calculated `preds`, actual `y`,"
     err_msg = "`data` should link to vision.data instead of text.data."
     modules = gen_notebooks.get_imported_modules([gen_notebooks.get_code_cell(imports)], nb_module_name='fastai.vision.learner')
     assert_link(docstr, expected, modules=modules, msg=err_msg)
