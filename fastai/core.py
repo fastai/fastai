754da7a3d39d56cceaa0bb5414a76e7c08a79124
Wayde Gilliam <wgilliam@ohmeow.com>
2018-10-23 14:06:40 -0700

    added np.darray type as an "is_listy"

--- fastai/core.py
+++ fastai/core.py
@@ -43,7 +43,7 @@ def num_cpus()->int:
     try:                   return len(os.sched_getaffinity(0))
     except AttributeError: return os.cpu_count()
 
-def is_listy(x:Any)->bool: return isinstance(x, (tuple,list))
+def is_listy(x:Any)->bool: return isinstance(x, (tuple,list,np.ndarray))
 def is_tuple(x:Any)->bool: return isinstance(x, tuple)
 def noop(x): return x
 
