e2e4fa52f6a039c8699ef3493f7dfac5100ed0e7
marii <marii@marii.moe>
2018-10-27 19:29:03 -0700

    Added test for half precision

--- tests/test_torch_core.py
+++ tests/test_torch_core.py
@@ -1,8 +1,20 @@
 import pytest, torch, fastai
 from fastai.torch_core import *
+from math import isclose
 
 a=[1,2,3]
 exp=torch.tensor(a)
+a3b3b3 =torch.ones([1,3,3,3])
+
+class ConvBNNet(torch.nn.Module):
+    def __init__(self, D_in):
+        super(ConvBNNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(D_in, D_in*2, 2)
+        self.bn = torch.nn.BatchNorm2d(D_in*2)
+
+    def forward(self, x):
+        h_relu = self.conv1(x).clamp(min=0)
+        return self.bn(h_relu)
 
 def test_tensor_with_list():
     r = tensor(a)
@@ -19,3 +31,11 @@ def test_tensor_with_tensor():
     r = tensor(c)
     assert r.data_ptr()==c.data_ptr()
     assert torch.all(r==exp)
+
+def test_model2half():
+    m = ConvBNNet(3)
+    half_m = model2half(m)
+    assert isinstance(half_m.conv1.weight, torch.HalfTensor)
+    assert isinstance(half_m.bn.weight, torch.FloatTensor)
+    result = half_m.cuda().forward(a3b3b3.cuda().half()).sum()
+    assert isclose(result,0,abs_tol=1e-3)
