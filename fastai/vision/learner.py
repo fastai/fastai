01aec14004bd0d056ad276130d3137f6f0c68066
Jeremy Howard <j@howard.fm>
2018-10-28 07:50:48 -0700

    Learner.create_cnn

--- fastai/vision/learner.py
+++ fastai/vision/learner.py
@@ -7,7 +7,7 @@ from . import models
 from ..callback import *
 from ..layers import *
 
-__all__ = ['ConvLearner', 'create_body', 'create_head', 'num_features', 'ClassificationInterpretation']
+__all__ = ['create_body', 'create_head', 'num_features', 'ClassificationInterpretation']
 
 def create_body(model:Model, cut:Optional[int]=None, body_fn:Callable[[Model],Model]=None):
     "Cut off the body of a typically pretrained `model` at `cut` or as specified by `body_fn`."
@@ -44,21 +44,24 @@ model_meta = {
     models.resnet50 :{**_resnet_meta}, models.resnet101:{**_resnet_meta},
     models.resnet152:{**_resnet_meta}}
 
-class ConvLearner(Learner):
+@classmethod
+def _create_cnn(cls, data:DataBunch, arch:Callable, cut:Union[int,Callable]=None, pretrained:bool=True,
+                lin_ftrs:Optional[Collection[int]]=None, ps:Floats=0.5,
+                custom_head:Optional[nn.Module]=None, split_on:Optional[SplitFuncOrIdxList]=None, **kwargs:Any)->None:
     "Build convnet style learners."
-    def __init__(self, data:DataBunch, arch:Callable, cut:Union[int,Callable]=None, pretrained:bool=True,
-                 lin_ftrs:Optional[Collection[int]]=None, ps:Floats=0.5,
-                 custom_head:Optional[nn.Module]=None, split_on:Optional[SplitFuncOrIdxList]=None, **kwargs:Any)->None:
-        meta = model_meta.get(arch, _default_meta)
-        torch.backends.cudnn.benchmark = True
-        body = create_body(arch(pretrained), ifnone(cut,meta['cut']))
-        nf = num_features(body) * 2
-        head = custom_head or create_head(nf, data.c, lin_ftrs, ps)
-        model = nn.Sequential(body, head)
-        super().__init__(data, model, **kwargs)
-        self.split(ifnone(split_on,meta['split']))
-        if pretrained: self.freeze()
-        apply_init(model[1], nn.init.kaiming_normal_)
+    meta = model_meta.get(arch, _default_meta)
+    torch.backends.cudnn.benchmark = True
+    body = create_body(arch(pretrained), ifnone(cut,meta['cut']))
+    nf = num_features(body) * 2
+    head = custom_head or create_head(nf, data.c, lin_ftrs, ps)
+    model = nn.Sequential(body, head)
+    learn = Learner(data, model, **kwargs)
+    learn.split(ifnone(split_on,meta['split']))
+    if pretrained: learn.freeze()
+    apply_init(model[1], nn.init.kaiming_normal_)
+    return learn
+
+Learner.create_cnn = _create_cnn
 
 class ClassificationInterpretation():
     "Interpretation methods for classification models."
