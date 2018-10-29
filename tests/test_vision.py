87aeeeaa648db544ab028ce965a9cfaa81f04896
Navjot Singh <navjotts@gmail.com>
2018-10-29 10:00:21 -0700

    remove device dependency from tests

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -3,12 +3,8 @@ from fastai import *
 from fastai.vision import *
 
 @pytest.fixture(scope="module")
-def path(request):
+def path():
     path = untar_data(URLs.MNIST_TINY)
-    d = defaults.device
-    defaults.device = torch.device('cpu')
-    def _final(): defaults.device = d
-    request.addfinalizer(_final)
     return path
 
 def test_path_can_be_str_type(path):
