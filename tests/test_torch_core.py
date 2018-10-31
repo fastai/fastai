efca6f63669d0419f1425608ef17295a988f92a6
Marii <marii@marii.moe>
2018-10-31 06:27:07 -0700

    added tests to test in channels, and exception case (#1023)

--- tests/test_torch_core.py
+++ tests/test_torch_core.py
@@ -1,8 +1,10 @@
 import pytest, torch, fastai
 from fastai.torch_core import *
+from fastai.layers import *
 
 a=[1,2,3]
 exp=torch.tensor(a)
+b=[3,6,6]
 
 def test_tensor_with_list():
     r = tensor(a)
@@ -19,3 +21,12 @@ def test_tensor_with_tensor():
     r = tensor(c)
     assert r.data_ptr()==c.data_ptr()
     assert torch.all(r==exp)
+
+def test_in_channels():
+    m = simple_cnn(b)
+    assert in_channels(m) == 3
+
+def test_in_channels_no_weights():
+    with pytest.raises(Exception) as e_info:
+        in_channels(nn.Sequential())
+    assert e_info.value.args[0] == 'No weight layer'
