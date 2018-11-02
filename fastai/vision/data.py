e9f7b5a87c2a32fd1d0c65ab5da7068e89f690c5
zc <zachcaceres@gmail.com>
2018-11-01 19:42:05 -0700

    fix: dont use ds for a dataloader variable

--- fastai/vision/data.py
+++ fastai/vision/data.py
@@ -348,10 +348,10 @@ class ImageDataBunch(DataBunch):
         return self
 
     def show_batch(self:DataBunch, rows:int=None, figsize:Tuple[int,int]=(9,10), ds_type:DatasetType=DatasetType.Valid)->None:
-        ds = self.valid_dl
-        if (ds_type == DatasetType.Train): ds = self.train_dl
-        elif (ds_type == DatasetType.Test) ds = self.test_dl
-        show_image_batch(ds, self.classes, figsize=figsize, rows=rows)
+        dl = self.valid_dl
+        if (ds_type == DatasetType.Train): dl = self.train_dl
+        elif (ds_type == DatasetType.Test) dl = self.test_dl
+        show_image_batch(dl, self.classes, figsize=figsize, rows=rows)
 
     def labels_to_csv(self, dest:str)->None:
         "Save file names and labels in `data` as CSV to file name `dest`."
