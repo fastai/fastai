1a15ea108e6e027ac5d09f37998bb299609cf1ce
Sylvain Gugger <sylvain.gugger@gmail.com>
2018-11-06 16:02:36 +0000

    Simplify batchnorm test

---
+++ tests/test_batchnom_issue_minimal.py
@@ -0,0 +1,20 @@
+import torch, torch.nn as nn
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataloader import DataLoader
+import pytest
+
+def _run_batch_size_test(bs):
+    dataset = TensorDataset(torch.randn(9, 128), torch.randint(0,3,(9,)))
+    dataloader = DataLoader(dataset, batch_size=bs)
+    simple_model = nn.Sequential(nn.BatchNorm1d(128), nn.Linear(128,4))
+    for (x,y) in iter(dataloader):
+        z = simple_model(x)
+
+# This test will fail as the last batch will have a size of 1
+@pytest.mark.skip
+def test_batch_size_4():
+    _run_batch_size_test(4)
+
+# This test succeeds
+def test_batch_size_3():
+    _run_batch_size_test(3)
