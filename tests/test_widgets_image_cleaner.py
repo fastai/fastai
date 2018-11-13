2cf10fdcacaf2965f3777a7378b637ab47f6c32b
noklam <channoklam@yahoo.com.hk>
2018-11-13 16:06:54 +0800

    Update test_widgets_image_cleaner.py
    fix typo

--- tests/test_widgets_image_cleaner.py
+++ tests/test_widgets_image_cleaner.py
@@ -11,8 +11,8 @@ def test_image_cleaner_length_long():
         data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), batch_size=16, num_workers=2)
         n = len(data.valid_ds)
     assert  ImageCleaner(data.valid_ds, np.arange(n+1))
-@pytest.mark.xfail(reason = "Expected Fail
-                   
+    
+@pytest.mark.xfail(reason = "Expected Fail")                   
 def test_image_cleaner_length_short():
     path = untar_data(URLs.MNIST_TINY)
     data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), batch_size=16, num_workers=2)
