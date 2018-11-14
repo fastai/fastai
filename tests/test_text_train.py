725337a1de52f95cbea91abc4cca9183b0dcd55a
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-11-14 09:50:30 -0500

    Skip tests for now (should be marked as slow)

--- tests/test_text_train.py
+++ tests/test_text_train.py
@@ -31,7 +31,7 @@ def learn():
     learn.fit_one_cycle(2, 5e-3)
     return learn
 
-
+@pytest.mark.skip(reason="only one integration test for text train")
 def manual_seed(seed=42):
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -43,6 +43,7 @@ def manual_seed(seed=42):
 def test_val_loss(learn):
     assert learn.validate()[1] > 0.3
 
+@pytest.mark.skip(reason="only one integration test for text train")
 def test_qrnn_works_with_no_split():
     gc.collect()
     manual_seed()
@@ -53,6 +54,7 @@ def test_qrnn_works_with_no_split():
     learn.fit_one_cycle(2, 5e-3)
     assert learn.validate()[1] > 0.3
 
+@pytest.mark.skip(reason="only one integration test for text train")
 def test_qrnn_works_if_split_fn_provided():
     gc.collect()
     manual_seed()
