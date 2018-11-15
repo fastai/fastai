8da9b1e5b68d59519601e536d97826a8ab24ea95
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2018-11-16 01:17:08 +0530

    Update __init__.py

--- fastai/vision/models/__init__.py
+++ fastai/vision/models/__init__.py
@@ -1,6 +1,6 @@
 from torchvision.models import ResNet,resnet18,resnet34,resnet50,resnet101,resnet152
 from torchvision.models import densenet121,densenet161,densenet169,densenet201
-from torchvision.models import inception,inception_v3
+from torchvision.models import inception_v3
 from torchvision.models import squeezenet1_0,squeezenet1_1
 from .darknet import *
 from .unet import *
