026c0d42603359dae0981225da978eb269737c79
Mukesh Jangir <mukeshjangir29@gmail.com>
2018-11-20 00:52:10 +0530

    Fix an issue in ImageItemList.from_csv method (#1208)
    * Fix an issue in ImageItemList.from_csv method
    
    There is a bug in ImageItemList.from_csv method. When we pass a string instead of a Path object to the path parameter, this causes an error.
    
    Fix for this bug is to convert string into Path object.
    
    * Refactor

--- fastai/vision/data.py
+++ fastai/vision/data.py
@@ -309,8 +309,8 @@ class ImageItemList(ItemList):
     @classmethod
     def from_csv(cls, path:PathOrStr, csv_name:str, cols:IntsOrStrs=0, header:str='infer',
                  folder:PathOrStr='.', suffix:str='')->'ItemList':
-        df = pd.read_csv(path/csv_name, header=header)
-        return cls.from_df(df, path=path, cols=cols, folder=folder, suffix=suffix)
+        df = pd.read_csv(Path(path)/csv_name, header=header)
+        return cls.from_df(df, path=Path(path), create_func=create_func, cols=cols, folder=folder, suffix=suffix)
 
 class ObjectCategoryProcessor(MultiCategoryProcessor):
     def process_one(self,item): return [item[0], [self.c2i.get(o,None) for o in item[1]]]
