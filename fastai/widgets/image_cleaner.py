2b553be9ad2f53edcda87bea51ce447afbac7ef2
lesscomfortable <pancho_ingham@hotmail.com>
2018-11-23 12:07:22 -0800

    Reference to source in largest_indices

--- fastai/widgets/image_cleaner.py
+++ fastai/widgets/image_cleaner.py
@@ -71,7 +71,8 @@ class DatasetFormatter():
         return np.array(sims)
 
     def largest_indices(arr, n):
-        """Returns the n largest indices from a numpy array."""
+        'https://stackoverflow.com/questions/6910641/how-do-i-get-indices-of-n-maximum-values-in-a-numpy-array'
+        "Returns the n largest indices from a numpy array."
         flat = arr.flatten()
         indices = np.argpartition(flat, -n)[-n:]
         indices = indices[np.argsort(-flat[indices])]
