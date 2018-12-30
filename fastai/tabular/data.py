20304c16dd1be3b58332db677c4b4078943100bd
Sylvain <sylvain.gugger@gmail.com>
2018-12-30 18:24:27 +0000

    Fix #1417

--- fastai/tabular/data.py
+++ fastai/tabular/data.py
@@ -93,8 +93,8 @@ class TabularDataBunch(DataBunch):
         cont_names = ifnone(cont_names, list(set(df)-set(cat_names)-{dep_var}))
         procs = listify(procs)
         src = (TabularList.from_df(df, path=path, cat_names=cat_names, cont_names=cont_names, procs=procs)
-                           .split_by_idx(valid_idx)
-                           .label_from_df(cols=dep_var, classes=classes))
+                           .split_by_idx(valid_idx))
+        src = src.label_from_df(cols=dep_var) if classes is None else src.label_from_df(cols=dep_var, classes=classes)
         if test_df is not None: src.add_test(TabularList.from_df(test_df, cat_names=cat_names, cont_names=cont_names,
                                                                  processor = src.train.x.processor))
         return src.databunch(**kwargs)
