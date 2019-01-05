1be4305ce285cba0319fbc2f58f9bafc75126645
Benudek <benedikt.herudek@gmail.com>
2019-01-05 13:35:09 +0100

    decommnted print and clarified TO DOs

--- tests/test_basic_data.py
+++ tests/test_basic_data.py
@@ -6,22 +6,23 @@ import sys
 
 ## run: pytest tests/test_basic_data.py -s
 
-
 # filename: test_basic_data.py
+# code of file tested: basic_data.py
 
 ## Test Cases
-
     ## TO DO: Function intercept_args
-
     ## TO DO: Class DeviceDataLoader
 
 ## Class DataBunch
 
 def test_DataBunch_Create():
-    x_train,y_train =  fake_basedata(n_in=3, batch_size=6),fake_basedata(n_in=3, batch_size=6)
-    x_valid,y_valid =  fake_basedata(n_in=3, batch_size=3),fake_basedata(n_in=3, batch_size=3)
+    x_train =  fake_basedata(n_in=3, batch_size=6) 
+    y_train =  fake_basedata(n_in=3, batch_size=6) 
+    x_valid =  fake_basedata(n_in=3, batch_size=3) 
+    y_valid =  fake_basedata(n_in=3, batch_size=3) 
     bs=5
-    train_ds,valid_ds = TensorDataset(x_train, y_train),TensorDataset(x_valid, y_valid)
+    train_ds = TensorDataset(x_train, y_train)
+    valid_ds = TensorDataset(x_valid, y_valid)
     data = DataBunch.create(train_ds, valid_ds, bs=bs)
    
     assert 3 == len(data.train_dl)
@@ -29,21 +30,21 @@ def test_DataBunch_Create():
     assert 2 == len(data.valid_dl)
     assert 9 == len(data.valid_ds)
 
-## TO DO (?)ideally, call one_batch with type dataloader    
+## TO DO: 
+    # call one_batch with type dataloader    
+    # make tests to use detach, denorm flags
 def test_DataBunch_onebatch():
     data = fake_data(n_in=4, n_out=5, batch_size=6)
     x,y = data.one_batch()
-    assert 4 == x[0].shape[0] 
-    assert 6 == x.shape[0] 
-    assert 6 == y.shape[0] 
+    assert 4 == x[0].shape[0] ## print(x[1].shape[0]) ## 4 /  print(len(x[0])) ## 4 /  print(len(x[1])) ## 4
+    assert 6 == x.shape[0] ## 6 print(len(x)) ## 6
+    assert 6 == y.shape[0] ## 6 print(len(y)) ## 6
 
-    
 def test_DataBunch_oneitem():
     data = fake_data()
     x,y = data.one_item(item=1)
     assert 1 == x.shape[0]
     assert 1 == y.shape[0]
-    
     
 def test_DataBunch_show_batch(capsys):
      data = fake_data()
@@ -52,9 +53,10 @@ def test_DataBunch_show_batch(capsys):
      match = re.findall(r'tensor', captured.out)
      assert match
 
-## TO DO: check over file created ?    
+## TO DO: check over file created  
 ##def test_DataBunch_export():     
 ##     data = fake_data()
 ##     data.export()
+
 
 
