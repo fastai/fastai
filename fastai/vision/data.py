1ea20c632e26684a55b2a46d297c7e9ed44c3167
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-01-17 16:59:05 +0000

    Fix #1477

--- fastai/vision/data.py
+++ fastai/vision/data.py
@@ -39,6 +39,7 @@ def get_annotations(fname, prefix=None):
 
 def bb_pad_collate(samples:BatchSamples, pad_idx:int=0) -> Tuple[FloatTensor, Tuple[LongTensor, LongTensor]]:
     "Function that collect `samples` of labelled bboxes and adds padding with `pad_idx`."
+    if isinstance(samples[0][1], int): return data_collate(samples)
     max_len = max([len(s[1].data[1]) for s in samples])
     bboxes = torch.zeros(len(samples), max_len, 4)
     labels = torch.zeros(len(samples), max_len).long() + pad_idx
