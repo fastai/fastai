c558e1915875fd2d830519e5e7568b858cd8c979
Andrea de Luca <35294634+terribilissimo@users.noreply.github.com>
2019-01-21 20:00:08 +0100

    Added plot_multi_top_losses() for Interpretation upon multi-labeled datasets (#1476)
    * Add files via upload
    
    * Add files via upload
    
    * Update learner.py
    
    * Update learner.py
    
    * Update learner.py
    
    * Update learner.py
    
    * Add files via upload
    
    Briefly documented my `plot_multi_top_losses()`.
    
    I did put it in "New Methods - Please document or move to the undocumented section" section.
    
    * Change 1liner
    
    * Update learner.py
    
    * Add files via upload
    
    * Remove dupe init
    
    * Strip
    
    * Remove typo

--- fastai/vision/learner.py
+++ fastai/vision/learner.py
@@ -112,6 +112,43 @@ class ClassificationInterpretation():
             cl = int(cl)
             im.show(ax=axes.flat[i], title=
                 f'{classes[self.pred_class[idx]]}/{classes[cl]} / {self.losses[idx]:.2f} / {self.probs[idx][cl]:.2f}')
+            
+    def plot_multi_top_losses(self, samples:int=3, figsz:Tuple[int,int]=(8,8), save_misclassified:bool=False):
+        "Show images in `top_losses` along with their prediction, actual, loss, and probability of predicted class in a multilabeled dataset."
+        if samples >20:
+            print("Max 20 samples")
+            return
+        losses, idxs = self.top_losses(self.data.c)
+        infolist, ordlosses_idxs, mismatches_idxs, mismatches, losses_mismatches, mismatchescontainer = [],[],[],[],[],[]                                                      
+        truthlabels=np.asarray(self.y_true, dtype=int) 
+        classes_ids=[k for k in enumerate(self.data.classes)]
+        predclass=np.asarray(self.pred_class)
+        for i, pred in enumerate(predclass):
+            where_truth=np.nonzero((truthlabels[i]>0))[0]
+            mismatch=np.all(pred!=where_truth)
+            if mismatch: 
+                mismatches_idxs.append(i)
+                losses_mismatches.append((losses[i][pred],i))
+            infotup=(i, pred, where_truth, losses[i][pred], np.round(self.probs[i], decimals=3)[pred], mismatch)
+            infolist.append(infotup)
+        mismatches = self.data.valid_ds[mismatches_idxs]
+        ordlosses=sorted(losses_mismatches, key = lambda x: x[0], reverse=True)
+        for w in ordlosses: ordlosses_idxs.append(w[1])
+        mismatches_ordered_byloss=self.data.valid_ds[ordlosses_idxs]
+        print(mismatches)
+        for ima in range(len(mismatches_ordered_byloss)):
+            mismatchescontainer.append(mismatches_ordered_byloss[ima][0]) 
+        for sampleN in range(samples):
+            actualclasses=''
+            for clas in infolist[ordlosses_idxs[sampleN]][2]:
+                actualclasses=actualclasses+' -- '+str(classes_ids[clas][1])
+            imag=mismatches_ordered_byloss[sampleN][0]
+            imag=show_image(imag, figsize=figsz)
+            imag.set_title(f"""Predicted: {classes_ids[infolist[ordlosses_idxs[sampleN]][1]][1]}, 
+                               Actual: {actualclasses}, Loss: {infolist[ordlosses_idxs[sampleN]][3]}, 
+                               Probability: {infolist[ordlosses_idxs[sampleN]][4]}""")
+            plt.show()
+            if save_misclassified: return mismatchescontainer
 
     def confusion_matrix(self, slice_size:int=None):
         "Confusion matrix as an `np.ndarray`."
