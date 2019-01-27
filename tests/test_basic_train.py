cda9217a3a0b15e2d7040aba837fbe0073f6453e
Benudek <benedikt.herudek@gmail.com>
2019-01-27 12:15:19 +0100

    really simple assert
    Signed-off-by: Benudek <benedikt.herudek@gmail.com>

--- tests/test_basic_train.py
+++ tests/test_basic_train.py
@@ -1,3 +1,14 @@
 import pytest,fastai
+from utils.fakes import *
 
-#I'm all empty, please fill me with tests about the functions in basic_train.py
+## filename: test_basic_train.py
+## tests functions in basic_train.py. For test of fit, see test_train
+
+## Class Learner, see documentation:  https://docs.fast.ai/basic_train.html
+ 
+## run: pytest tests/test_basic_train.py (add -s for screenoutput )
+
+def test_get_preds():
+    learn = fake_learner()
+    a = learn.get_preds()
+    assert learn.data.batch_size == len(a[1])
