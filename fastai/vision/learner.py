8c0ef5ee99754d274a09430c2ee080a5467b52b2
PPPW <pei.wang.fudan@gmail.com>
2019-01-26 20:41:39 -0500

    include more torchvision models

--- fastai/vision/learner.py
+++ fastai/vision/learner.py
@@ -15,10 +15,16 @@ def _default_split(m:nn.Module): return (m[1],)
 def _resnet_split(m:nn.Module): return (m[0][6],m[1])
 # Split squeezenet model on maxpool layers
 def _squeezenet_split(m:nn.Module): return (m[0][0][5], m[0][0][8], m[1])
+def _densenet_split(m:nn.Module): return (m[0][0][7],m[1])
+def _vgg_split(m:nn.Module): return (m[0][0][22],m[1])
+def _alexnet_split(m:nn.Module): return (m[0][0][6],m[1])
 
 _default_meta = {'cut':-1, 'split':_default_split}
 _resnet_meta  = {'cut':-2, 'split':_resnet_split }
 _squeezenet_meta = {'cut':-1, 'split': _squeezenet_split}
+_densenet_meta = {'cut':-1, 'split':_densenet_split}
+_vgg_meta = {'cut':-1, 'split':_vgg_split}
+_alexnet_meta = {'cut':-1, 'split':_alexnet_split}
 
 model_meta = {
     models.resnet18 :{**_resnet_meta}, models.resnet34: {**_resnet_meta},
@@ -26,7 +32,12 @@ model_meta = {
     models.resnet152:{**_resnet_meta},
 
     models.squeezenet1_0:{**_squeezenet_meta},
-    models.squeezenet1_1:{**_squeezenet_meta}}
+    models.squeezenet1_1:{**_squeezenet_meta},
+
+    models.densenet121:{**_densenet_meta}, models.densenet169:{**_densenet_meta},
+    models.densenet201:{**_densenet_meta}, models.densenet161:{**_densenet_meta},
+    models.vgg16_bn:{**_vgg_meta}, models.vgg19_bn:{**_vgg_meta},
+    models.alexnet:{**_alexnet_meta}}
 
 def cnn_config(arch):
     "Get the metadata associated with `arch`."
