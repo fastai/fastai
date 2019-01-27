82f88896ebee6b37e8e2cceb3c1b35abaa8938e0
Benudek <benedikt.herudek@gmail.com>
2019-01-27 12:35:52 +0100

    screen assert

--- tests/utils/text.py
+++ tests/utils/text.py
@@ -15,6 +15,11 @@ from io import StringIO
 def apply_print_resets(buf):
     return re.sub(r'^.*\r', '', buf, 0, re.M)
 
+def assert_screenout(out, what):
+    out_pr = apply_print_resets(out).lower()
+    match_str = out_pr.find(what.lower())
+    assert match_str != -1, f"expecting to find {what} in output: f{out_pr}"
+
 class CaptureStdout():
     """ Context manager to capture stdout, clean it up and make it available via obj.out or str(obj).
 
