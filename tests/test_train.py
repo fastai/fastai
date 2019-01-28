51c13f5cdd72037442f6e08d89396d7df6b3517c
Benudek <benedikt.herudek@gmail.com>
2019-01-28 17:45:00 +0100

    no empty lines, check over complete list, shortened assert stable lr and momentum
    Signed-off-by: Benudek <benedikt.herudek@gmail.com>

--- tests/test_train.py
+++ tests/test_train.py
@@ -4,80 +4,45 @@ from utils.text import *
 
 ## filename: test_train.py
 ## tests functions in train.py
-
 ## Class Learner, see documentation:  https://docs.fast.ai/train.html
- 
 ## Model fitting methods
-
 ## run: pytest tests/test_train.py (add -s for screenoutput )
-
 
 @pytest.fixture(scope="module")
 def learn():
-    
     learn = fake_learner(50,50)
-   
     return learn
 
 def test_fit(learn):
-
     ## Test confirms learning rate and momentum are stable, see difference to test_fit_one_cycle
-    
     learning_rate = 3e-3
     weight_decay = 1e-2
     eps = 4
-    
     with CaptureStdout() as cs:  learn.fit(epochs=eps, lr=learning_rate, wd=weight_decay)
-   
-    lrs = list(learn.recorder.lrs)
-    prevlr = learning_rate
-    for lr in lrs:
-        if prevlr is not None: assert prevlr == lr
-        prevlr = lr
-    moms = list(learn.recorder.moms)
-    prevmom = None
-    for mom in moms:
-        if prevmom is not None: assert prevmom == mom
-        prevmom = mom
+    assert set(learn.recorder.lrs) == {learning_rate}
+    assert set(learn.recorder.moms) == {learn.recorder.moms[0]}
    
 def test_fit_one_cycle(learn):
-
-    ## Test confirms expected behavior leaning rate and momentum
+    ## Test confirms expected behavior change of learning rate and momentum
     ## see graphical representation here: output cell 17 of, learn.sched.plot_lr() in 
     ## https://github.com/sgugger/Deep-Learning/blob/master/Cyclical%20LR%20and%20momentums.ipynb
-
-    cycle_length = 4
     lr = 3e-3
-
+    cycle_length = 4
     with CaptureStdout() as cs: learn.fit_one_cycle(cycle_length, lr)
-  
     listlrs = list(learn.recorder.lrs)
     listmoms = list(learn.recorder.moms) 
-    
-    ## eliminate the final low learning rates with corresponding momentum
-    for (idx,lr) in enumerate(listlrs):
-        if lr < listlrs[0]:
-            del listlrs[idx]
-            del listmoms[idx]
-
     ## we confirm learning rate is at its max when momentum is at its min
     val_lr, idx_lr = max((val, idx) for (idx, val) in enumerate(listlrs))
     val_mom, idx_mom = min((val, idx) for (idx, val) in enumerate(listmoms))
     assert idx_lr == idx_mom
-
-    ## we separate the graph 1st half (left) and 2nd part (right) with previously identified saddle point
     maxlr_minmom = idx_lr # = idx_mom
-    
     ## confirm 1st half (left): learning rate is at its minimum when momentum is at its maximum
     val_lr, idx_lr = min((val, idx) for (idx, val) in enumerate(listlrs[0:maxlr_minmom+1]))
     val_mom, idx_mom = max((val, idx) for (idx, val) in enumerate(listmoms[0:maxlr_minmom+1]))
     assert idx_lr == idx_mom
-
     ## confirm 2nd half (right): learning rate is at its minimum when momentum is at its maximum
-    ## Note, for simplicity we cut off the final learning rates < start learning rate and ignore the final stable momentum
-    val_lr, idx_lr = min((val, idx) for (idx, val) in enumerate(listlrs[maxlr_minmom:])) ## remove previous min.
-    val_mom, idx_mom = max((val, idx) for (idx, val) in enumerate(listmoms[maxlr_minmom:])) ## remove previous max.
-   
+    val_lr, idx_lr = min((val, idx) for (idx, val) in enumerate(listlrs[maxlr_minmom:])) 
+    val_mom, idx_mom = max((val, idx) for (idx, val) in enumerate(listmoms[maxlr_minmom:])) 
     assert idx_lr == idx_mom
 
     
