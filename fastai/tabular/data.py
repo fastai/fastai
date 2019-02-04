4815444b192b5d6e3bc3ee35f91b92dba9451d43
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-02-04 16:25:01 -0500

    Pull bs out of the kwargs

--- fastai/tabular/data.py
+++ fastai/tabular/data.py
@@ -87,8 +87,8 @@ class TabularDataBunch(DataBunch):
     @classmethod
     def from_df(cls, path, df:DataFrame, dep_var:str, valid_idx:Collection[int], procs:OptTabTfms=None,
                 cat_names:OptStrList=None, cont_names:OptStrList=None, classes:Collection=None, 
-                test_df=None, **kwargs)->DataBunch:
-        "Create a `DataBunch` from `df` and `valid_idx` with `dep_var`."
+                test_df=None, bs:int=64, **kwargs)->DataBunch:
+        "Create a `DataBunch` from `df` and `valid_idx` with `dep_var`. `kwargs` are passed to `DataBunch.create`."
         cat_names = ifnone(cat_names, []).copy()
         cont_names = ifnone(cont_names, list(set(df)-set(cat_names)-{dep_var}))
         procs = listify(procs)
@@ -97,7 +97,7 @@ class TabularDataBunch(DataBunch):
         src = src.label_from_df(cols=dep_var) if classes is None else src.label_from_df(cols=dep_var, classes=classes)
         if test_df is not None: src.add_test(TabularList.from_df(test_df, cat_names=cat_names, cont_names=cont_names,
                                                                  processor = src.train.x.processor))
-        return src.databunch(**kwargs)
+        return src.databunch(bs=bs, **kwargs)
 
 class TabularList(ItemList):
     "Basic `ItemList` for tabular data."
@@ -161,10 +161,10 @@ class TabularList(ItemList):
         display(HTML(text2html_table(items, [10] * len(items[0]))))
 
 def tabular_learner(data:DataBunch, layers:Collection[int], emb_szs:Dict[str,int]=None, metrics=None,
-        ps:Collection[float]=None, emb_drop:float=0., y_range:OptRange=None, use_bn:bool=True, **kwargs):
+        ps:Collection[float]=None, emb_drop:float=0., y_range:OptRange=None, use_bn:bool=True, **learn_kwargs):
     "Get a `Learner` using `data`, with `metrics`, including a `TabularModel` created using the remaining params."
     emb_szs = data.get_emb_szs(ifnone(emb_szs, {}))
     model = TabularModel(emb_szs, len(data.cont_names), out_sz=data.c, layers=layers, ps=ps, emb_drop=emb_drop,
                          y_range=y_range, use_bn=use_bn)
-    return Learner(data, model, metrics=metrics, **kwargs)
+    return Learner(data, model, metrics=metrics, **learn_kwargs)
 
