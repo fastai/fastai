80a0702bfce4ad310bb709b775dec99f6a31507a
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-04 19:46:59 +0000

    Remove check tfm args for now, needs fixing

--- fastai/data_block.py
+++ fastai/data_block.py
@@ -643,17 +643,17 @@ class LabelList(Dataset):
 
     def transform(self, tfms:TfmList, tfm_y:bool=None, **kwargs):
         "Set the `tfms` and `tfm_y` value to be applied to the inputs and targets."
-        _check_kwargs(self.x, tfms, **kwargs)
+        #_check_kwargs(self.x, tfms, **kwargs)
         if tfm_y is None: 
             tfm_y = self.tfm_y
-            _check_kwargs(self.y, tfms, **kwargs)
+            #_check_kwargs(self.y, tfms, **kwargs)
         self.tfms,self.tfmargs = tfms,kwargs
         self.tfm_y,self.tfms_y,self.tfmargs_y = tfm_y,tfms,kwargs
         return self
 
     def transform_y(self, tfms:TfmList=None, **kwargs):
         "Set `tfms` to be applied to the targets only."
-        _check_kwargs(self.y, tfms, **kwargs)
+        #_check_kwargs(self.y, tfms, **kwargs)
         self.tfm_y=True
         if tfms is None: self.tfms_y,self.tfmargs_y = self.tfms,{**self.tfmargs, **kwargs}
         else:            self.tfms_y,self.tfmargs_y = tfms,kwargs
