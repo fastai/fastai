50a1b588823b5a903f58f770d11c63c9b4dae64f
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-05 21:07:18 +0000

    Remove arg unused

--- tests/test_tabular_train.py
+++ tests/test_tabular_train.py
@@ -74,7 +74,7 @@ def test_empty_cont():
     learn.fit_one_cycle(1, 1e-1)
     assert learn.validate()[1] > 0.5
 
-def test_confusion_tabular(learn, out=True):
+def test_confusion_tabular(learn):
     interp = ClassificationInterpretation.from_learner(learn)
     assert isinstance(interp.confusion_matrix(), (np.ndarray))
     assert interp.confusion_matrix().sum() == len(learn.data.valid_ds)
