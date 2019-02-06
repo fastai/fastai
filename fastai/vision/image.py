bebbfe50db7cb3d8a99806f26d9b01eaf9f4e124
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-06 14:54:44 +0000

    Fix #1567

--- fastai/vision/image.py
+++ fastai/vision/image.py
@@ -371,7 +371,7 @@ class ImageBBox(ImagePoints):
     def show(self, y:Image=None, ax:plt.Axes=None, figsize:tuple=(3,3), title:Optional[str]=None, hide_axis:bool=True,
         color:str='white', **kwargs):
         "Show the `ImageBBox` on `ax`."
-        if ax is None: _,ax = plt.subplot(figsize=figsize)
+        if ax is None: _,ax = plt.subplots(figsize=figsize)
         bboxes, lbls = self._compute_boxes()
         h,w = self.flow.size
         bboxes.add_(1).mul_(torch.tensor([h/2, w/2, h/2, w/2])).long()
