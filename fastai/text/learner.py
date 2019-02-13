20b4a8221407441e5876eefd9342ae13c625e141
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-13 16:29:08 +0000

    Fix #1622

--- fastai/text/learner.py
+++ fastai/text/learner.py
@@ -26,18 +26,19 @@ _model_meta = {AWD_LSTM: {'hid_name':'emb_sz', 'url':URLs.WT103_1,
 
 def convert_weights(wgts:Weights, stoi_wgts:Dict[str,int], itos_new:Collection[str]) -> Weights:
     "Convert the model `wgts` to go with a new vocabulary."
-    dec_bias, enc_wgts = wgts['1.decoder.bias'], wgts['0.encoder.weight']
-    bias_m, wgts_m = dec_bias.mean(0), enc_wgts.mean(0)
+    dec_bias, enc_wgts = wgts.get('1.decoder.bias', None), wgts['0.encoder.weight']
+    wgts_m = enc_wgts.mean(0)
+    if dec_bias is not None: bias_m = dec_bias.mean(0)
     new_w = enc_wgts.new_zeros((len(itos_new),enc_wgts.size(1))).zero_()
-    new_b = dec_bias.new_zeros((len(itos_new),)).zero_()
+    if dec_bias is not None: new_b = dec_bias.new_zeros((len(itos_new),)).zero_()
     for i,w in enumerate(itos_new):
         r = stoi_wgts[w] if w in stoi_wgts else -1
         new_w[i] = enc_wgts[r] if r>=0 else wgts_m
-        new_b[i] = dec_bias[r] if r>=0 else bias_m
+        if dec_bias is not None: new_b[i] = dec_bias[r] if r>=0 else bias_m
     wgts['0.encoder.weight'] = new_w
-    wgts['0.encoder_dp.emb.weight'] = new_w.clone()
+    if '0.encoder_dp.emb.weight' in wgts: wgts['0.encoder_dp.emb.weight'] = new_w.clone()
     wgts['1.decoder.weight'] = new_w.clone()
-    wgts['1.decoder.bias'] = new_b
+    if dec_bias is not None: wgts['1.decoder.bias'] = new_b
     return wgts
 
 class RNNLearner(Learner):
