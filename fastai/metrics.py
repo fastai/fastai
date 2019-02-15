92ed6206c567545591b4f4f59de8a6b6d660e312
Nick Switanek <nswitanek@gmail.com>
2019-02-15 08:38:24 -0600

    Add linear and quadratic weight options to KappaScore (#1627)
    * enable user to specify linear or quadratic weights to KappaScore metric
    
    * moved weights kwarg out of subclass
    
    * strip vertical whitespace
    
    * shortened KappaScore class docstring, moved detail to metrics.ipynb. Replaced multiline if-else with one-line conditional expression. Changed ValueError message to be more informative about expected values for weights attribute.

--- fastai/metrics.py
+++ fastai/metrics.py
@@ -237,23 +237,28 @@ class FBeta(CMScores):
 class KappaScore(ConfusionMatrix):
     """
     Compute the rate of agreement (Cohens Kappa).
-    Ref.: https://github.com/scikit-learn/scikit-learn/blob/bac89c2/sklearn/metrics/classification.py
     """
+    weights:Optional[str]=None      # None, `linear`, or `quadratic`
     
     def on_epoch_end(self, **kwargs):
-        w = torch.ones((self.n_classes, self.n_classes))
-        w[self.x, self.x] = 0
         sum0 = self.cm.sum(dim=0)
         sum1 = self.cm.sum(dim=1)
         expected = torch.einsum('i,j->ij', (sum0, sum1)) / sum0.sum()
+        if self.weights is None:
+            w = torch.ones((self.n_classes, self.n_classes))
+            w[self.x, self.x] = 0
+        elif self.weights == "linear" or self.weights == "quadratic":
+            w = torch.zeros((self.n_classes, self.n_classes))
+            w += torch.arange(self.n_classes, dtype=torch.float)
+            w = torch.abs(w - torch.t(w)) if self.weights == "linear" else (w - torch.t(w)) ** 2
+        else:
+            raise ValueError('Unknown weights attribute given. Expected None, "linear", or "quadratic".')
         k = torch.sum(w * self.cm) / torch.sum(w * expected)
         self.metric = 1 - k
-        
 
 class MatthewsCorreff(ConfusionMatrix):
     """    
     Compute the Matthews correlation coefficient.
-    Ref.: https://github.com/scikit-learn/scikit-learn/blob/bac89c2/sklearn/metrics/classification.py
     """
 
     def on_epoch_end(self, **kwargs):
