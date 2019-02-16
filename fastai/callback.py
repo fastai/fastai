66f13df6632278092ae5892a2a9b25f39688cb06
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-16 14:15:56 +0000

    Fix new

--- fastai/callback.py
+++ fastai/callback.py
@@ -27,8 +27,6 @@ class OptimWrapper():
     def new(self, layer_groups:Collection[nn.Module]):
         "Create a new `OptimWrapper` from `self` with another `layer_groups` but the same hyper-parameters."
         opt_func = getattr(self, 'opt_func', self.opt.__class__)
-        split_groups = split_no_wd_params(layer_groups)
-        opt = opt_func([{'params': trainable_params(l), 'lr':0} for l in split_groups])
         res = self.create(opt_func, self.lr, layer_groups, wd=self.wd, true_wd=self.true_wd, bn_wd=self.bn_wd)
         res.mom,res.beta = self.mom,self.beta
         return res
