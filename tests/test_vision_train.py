0abf71a5b6cd1873a9c181fc3d8c41e5b9c0d203
Stas Bekman <stas@stason.org>
2019-02-15 19:38:16 -0800

    these tests aren't slow at all, and one broken

--- tests/test_vision_train.py
+++ tests/test_vision_train.py
@@ -120,7 +120,6 @@ def test_model_load_mem_leak(learn_large_unfit):
     # XXX: not sure where 6MB get lost still but for now it's a small leak - need to test with a bigger model
     assert isclose(used_before, used_after_reclaimed, abs_tol=6),f"load() and used GPU RAM: before load(): {used_before}, after: {used_after}, after gc.collect() {used_after_reclaimed} used"
 
-@pytest.mark.slow
 @pytest.mark.parametrize('arch', [models.resnet18, models.squeezenet1_1])
 def test_models_meta(mnist_tiny, arch, zero_image):
     learn = create_cnn(mnist_tiny, arch, metrics=[accuracy, error_rate])
@@ -133,4 +132,3 @@ def test_ClassificationInterpretation(learn):
     assert interp.confusion_matrix().sum() == len(learn.data.valid_ds)
     conf = interp.most_confused()
     assert len(conf) == 0 or (set(conf[0][:2]) == set(conf[1][:2]) == {'3', '7'})
-    
