9927657403090a18db33108d4edb9988259fc424
Stas Bekman <stas@stason.org>
2019-02-15 20:48:04 -0800

    load/save tests w/ memory leak tracking

--- tests/test_basic_train.py
+++ tests/test_basic_train.py
@@ -7,56 +7,90 @@ import pytest, fastai
 from fastai.vision import *
 from utils.fakes import *
 from utils.text import *
+from utils.mem import *
 from fastai.utils.mem import *
 from math import isclose
 
-use_gpu = torch.cuda.is_available()
+torch_preload_mem()
 
-@pytest.fixture(scope="module")
-def learn_large_fit():
-    preload_pytorch()
+# this is not a fixture on purpose - the memory measurement tests are very
+# fickle, so they need a fresh object and not one modified by other tests
+def learn_large_unfit():
     path = untar_data(URLs.MNIST_TINY)
     data = ImageDataBunch.from_folder(path, ds_tfms=([], []), bs=2)
     learn = create_cnn(data, models.resnet18, metrics=accuracy)
-    learn.fit_one_cycle(1)
     return learn
+
+@pytest.fixture(scope="module")
+def learn(): return learn_large_unfit()
 
 def test_get_preds():
     learn = fake_learner()
     a = learn.get_preds()
     assert learn.data.batch_size == len(a[1])
 
-# WIP
-def check_mem(type, expected, received, abs_tol=2):
-    assert isclose(expected, received, abs_tol=abs_tol), f"{type} mem: expected={expected} received={received}"
-
-def check_mem_expected(used_expected, peaked_expected, used_received, peaked_received, abs_tol=2):
-    assert isclose(used_expected,   used_received,   abs_tol=abs_tol), f"used mem: expected={used_expected} received={used_received}"
-    assert isclose(peaked_expected, peaked_received, abs_tol=abs_tol), f"peaked mem: expected={peaked_expected} received={peaked_received}"
-
-@pytest.mark.skip(reason="WIP")
-@pytest.mark.skipif(not use_gpu, reason="requires cuda")
-def test_save_load(learn_large_fit):
-    learn = learn_large_fit
+def test_save_load(learn):
     name = 'mnist-tiny-test-save-load'
 
-    with GPUMemTrace() as mem_trace:
+    # testing that all these various sequences don't break each other
+    model_path = learn.save(name, return_path=True)
+    _ = learn.load(name, purge=True)
+    learn.data.sanity_check()
+    assert 709 == len(learn.data.train_ds)
+    _ = learn.purge()
+    _ = learn.load(name)
+    _ = learn.load(name)
+    model_path = learn.save(name, return_path=True)
+    _ = learn.load(name, purge=True)
+    # basic checks
+    #assert learn.recorder
+    assert learn.opt
+    assert 709 == len(learn.data.train_ds)
+    # XXX: could use more sanity checks
+
+    if os.path.exists(model_path): os.remove(model_path)
+
+def check_mem_expected(used_expected, peaked_expected, mtrace, abs_tol=2):
+    used_received, peaked_received = mtrace.data()
+    assert isclose(used_expected,   used_received,   abs_tol=abs_tol), f"used mem: expected={used_expected} received={used_received}"
+    assert isclose(peaked_expected, peaked_received, abs_tol=abs_tol), f"peaked mem: expected={peaked_expected} received={peaked_received}"
+    gc.collect()
+
+#@pytest.mark.skip(reason="WIP")
+@pytest.mark.cuda
+def test_save_load_mem_leak():
+    learn = learn_large_unfit()
+    name = 'mnist-tiny-test-save-load'
+    #learn.fit_one_cycle(1)
+
+    # save should consume no extra used or peaked memory
+    with GPUMemTrace() as mtrace:
         model_path = learn.save(name, return_path=True)
-    used_real, peak_real = mem_trace.data()
-    check_mem("used", 0, used_real, 5)
-    check_mem("peak", 0, peak_real, 5)
+    check_mem_expected(used_expected=0, peaked_expected=0, mtrace=mtrace, abs_tol=10)
 
-    with GPUMemTrace() as mem_trace:
-        _ = learn.load(name, purge=True)
-    # XXX: very different numbers if done w/o fit first
-    check_mem_expected(18, 12, *mem_trace.data(), 5)
+    # load w/ purge still leaks some the first time it's run
+    with GPUMemTrace() as mtrace:
+        learn.load(name, purge=True)
+    # XXX: very different numbers if done w/o fit first 42 8, w/ fit 24 16
+    check_mem_expected(used_expected=42, peaked_expected=8, mtrace=mtrace, abs_tol=10)
 
-    if False:
-        # XXX: deep recursion when called after save+load
-        with GPUMemTrace() as mem_trace:
-            _ = learn.purge()
-            _ = learn.load(name)
-        check_mem_expected(10, 12, *mem_trace.data(), 5)
+    # subsequent multiple load w/o purge should consume no extra used memory
+    with GPUMemTrace() as mtrace:
+        learn.load(name, purge=False)
+        learn.load(name, purge=False)
+    check_mem_expected(used_expected=0, peaked_expected=20, mtrace=mtrace, abs_tol=10)
+
+    # subsequent multiple load w/ purge should consume no extra used memory
+    with GPUMemTrace() as mtrace:
+        learn.load(name, purge=True)
+        learn.load(name, purge=True)
+    check_mem_expected(used_expected=0, peaked_expected=20, mtrace=mtrace, abs_tol=10)
+
+    # purge + load w/ default purge should consume no extra used memory
+    with GPUMemTrace() as mtrace:
+        learn.purge()
+        learn.load(name)
+    check_mem_expected(used_expected=0, peaked_expected=20, mtrace=mtrace, abs_tol=10)
 
     if os.path.exists(model_path): os.remove(model_path)
 
