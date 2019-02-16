9d884a6863639b3fc9bde9b37995518c90955edd
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-02-15 16:41:16 -0800

    Generating tests for nbtest

--- fastai/gen_doc/nbtest.py
+++ fastai/gen_doc/nbtest.py
@@ -6,8 +6,8 @@ from os.path import abspath, dirname, join
 from collections import namedtuple
 
 from fastai.gen_doc import nbdoc
-from .core import *
-from ..torch_core import *
+from ..imports.core import *
+from .core import ifnone
 from .doctest import get_parent_func, relative_test_path, full_name_with_qualname, DB_NAME
 
 from nbconvert import HTMLExporter
@@ -23,7 +23,7 @@ TestFunctionMatch = namedtuple('TestFunctionMatch', ['line_number', 'line'])
 def show_test(elt, search=True, markdown:bool=True)->str:
     "Show associated tests for a fastai function/class"
     fn_name = nbdoc.fn_name(elt)
-    md = f'Tests found for `{fn_name}`:'
+    md = ''
 
     db_matches = [get_links(t) for t in lookup_db(elt)]
     md += tests2markdown(ifnone(db_matches, []), 'This tests')
@@ -39,7 +39,10 @@ def show_test(elt, search=True, markdown:bool=True)->str:
             md += tests2markdown(related, 'Related tests')
         except OSError as e: 
             print('Could not find fastai/tests folder. If you installed from conda, please install developer build instead.')
-            
+
+    if len(md)==0: md = f'No tests found for `{fn_name}`'
+    else: md = f'Tests found for `{fn_name}`:' + md
+
     if markdown: display(Markdown(md))
     else: return md
 
@@ -104,6 +107,7 @@ def _is_file_match(elt, file_name:str, exact_match:bool=False):
 
 def _submodule_name(elt)->str:
     "Returns submodule - utils, text, vision, imports, etc."
+    if inspect.ismodule(elt): return None
     modules = elt.__module__.split('.')
     if len(modules) > 2:
         return modules[1]
@@ -113,30 +117,47 @@ def find_test_matches(elt, test_file:Path)->Tuple[List[Dict],List[Dict]]:
     "Find all functions in `test_file` related to `elt`"
     lines = get_lines(test_file)
     rel_path = relative_test_path(test_file)
-    direct_matches = direct_test_match(elt, lines, rel_path)
-    fuzzy_matches = fuzzy_test_match(elt, lines, rel_path)
+    fn_name = get_qualname(elt) if not inspect.ismodule(elt) else ''
+
+    direct_matches = direct_test_match(fn_name, lines, rel_path)
+    fuzzy_matches = fuzzy_test_match(fn_name, lines, rel_path)
     return direct_matches, fuzzy_matches
 
-def direct_test_match(elt, lines:List[Dict], rel_path:str)->List[TestFunctionMatch]:
+def direct_test_match(fn_name:str, lines:List[Dict], rel_path:str)->List[TestFunctionMatch]:
     "Any `def test_function_name():` where test name contains function/class name"
-    fn_name = nbdoc.fn_name(elt)
     result = []
+    fn_class,fn_name = separate_comp(fn_name)
+    fn_class = '_'.join(fn_class)
     for idx,line in enumerate(lines):
-        if re.match(f'^def test_\w*{fn_name}\w*\(.*', line):
+        if re.match(f'\s*def test_\w*({fn_class}_)?{fn_name}\w*\(.*', line):
             result.append((idx,line))
     return [map_test(rel_path, lno, l) for lno,l in result]
 
-def fuzzy_test_match(elt, lines:List[Dict], rel_path:str)->List[TestFunctionMatch]:
-    "Find any lines where `elt` is called and return the parent test function"
-    fuzzy_line_matches = _fuzzy_line_match(elt, lines)
+def get_qualname(elt):
+    return elt.__qualname__ if hasattr(elt, '__qualname__') else fn_name(elt)
+
+def separate_comp(qualname:str):
+    if not isinstance(qualname, str): qualname = get_qualname(qualname)
+    parts = qualname.split('.')
+    parts[-1] = remove_underscore(parts[-1])
+    if len(parts) == 1: return [], parts[0]
+    return parts[:-1], parts[-1]
+
+def remove_underscore(fn_name):
+    if fn_name and fn_name[0] == '_': return fn_name[1:] # remove private method underscore prefix
+    return fn_name
+
+def fuzzy_test_match(fn_name:str, lines:List[Dict], rel_path:str)->List[TestFunctionMatch]:
+    "Find any lines where `fn_name` is called and return the parent test function"
+    fuzzy_line_matches = _fuzzy_line_match(fn_name, lines)
     fuzzy_matches = [get_parent_func(lno, lines, ignore_missing=True) for lno,_ in fuzzy_line_matches]
     fuzzy_matches = list(filter(None.__ne__, fuzzy_matches))
     return [map_test(rel_path, lno, l) for lno,l in fuzzy_matches]
 
-def _fuzzy_line_match(elt, lines)->List[TestFunctionMatch]:
-    "Find any lines where `elt` is called"
-    fn_name = nbdoc.fn_name(elt)
+def _fuzzy_line_match(fn_name:str, lines)->List[TestFunctionMatch]:
+    "Find any lines where `fn_name` is called"
     result = []
+    _,fn_name = separate_comp(fn_name)
     for idx,line in enumerate(lines):
         if re.match(f'.*[\s\.\(]{fn_name}[\.\(]', line):
             result.append((idx,line))
@@ -147,7 +168,7 @@ def get_lines(file:Path)->List[str]:
 
 def map_test(test_file, line, line_text):
     "Creates dictionary test format to match doctest api"
-    test_name = re.match(f'^def (test_\w*)', line_text).groups(0)[0]
+    test_name = re.match(f'\s*def (test_\w*)', line_text).groups(0)[0]
     return { 'file': test_file, 'line': line, 'test': test_name }
 
 def get_links(metadata)->Tuple[str,str]:
