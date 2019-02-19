8cfcc183d3e4b8a3967a286078c75224fb81ea60
Ubuntu <ubuntu@ip-172-31-5-88.us-west-2.compute.internal>
2019-02-19 21:37:14 +0000

    fix for style guides, and minor improvements

--- fastai/train.py
+++ fastai/train.py
@@ -97,22 +97,21 @@ def clip_grad(learn:Learner, clip:float=0.1)->Learner:
 Learner.clip_grad = clip_grad
      
 class AccumulateStepper(LearnerCallback):
-    """
-    Does accumlated step every nth step by accumulating gradients
-    """
-    def __init__(self, learn:Learner, n_step:int = 1):
+    "Does accumlated step every nth step by accumulating gradients"
+
+    def __init__(self, learn:Learner, n_step:int = 1, drop_last:bool = False):
         super().__init__(learn)
         self.n_step = n_step
+        self.drop_last = drop_last
  
     def on_train_begin(self, **kwargs):
         "check if loss is reduction"
         if hasattr(self.loss_func, "reduction") and (self.loss_func.reduction == "mean"):
-             print("For better gradients consider 'reduction=sum'")
+             warn("For better gradients consider 'reduction=sum'")
         
     def on_epoch_begin(self, **kwargs):
         "init samples and batches, change optimizer"
-        self.acc_samples = 0
-        self.acc_batches = 0
+        self.acc_samples, self.acc_batches = 0., 0. 
         
     def on_batch_begin(self, last_input, last_target, **kwargs):
         "accumulate samples and batches"
@@ -120,24 +119,22 @@ class AccumulateStepper(LearnerCallback):
         self.acc_batches += 1
         
     def on_backward_end(self, **kwargs):
-        "step if number of desired batches accumulated, reset samples"
+        "accumulated step and reset samples, True will result in no stepping"
         if (self.acc_batches % self.n_step) == 0:
             for p in (self.learn.model.parameters()):
                 if p.requires_grad: p.grad.div_(self.acc_samples)
             self.acc_samples = 0
-            return False
         else: return True
     
     def on_step_end(self, **kwargs):
-        "zero gradients after stepping"
-        if (self.acc_batches % self.n_step) == 0: return False
-        else: return True
+        "zero gradients after stepping, True will result in no zeroing"
+        if not (self.acc_batches % self.n_step) == 0: return True
     
     def on_epoch_end(self, **kwargs):
         "step the rest of the accumulated grads if not perfectly divisible"
         for p in (self.learn.model.parameters()):
                 if p.requires_grad: p.grad.div_(self.acc_samples)
-        self.learn.opt.step()
+        if not self.drop_last: self.learn.opt.step()
         self.learn.opt.zero_grad()
 
 
