72c8e632b25fc103632c931bcbb3709fe50baa50
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-19 15:03:37 +0000

    Fix training in FP16 for text

--- fastai/callbacks/rnn.py
+++ fastai/callbacks/rnn.py
@@ -24,8 +24,8 @@ class RNNTrainer(LearnerCallback):
     def on_backward_begin(self, last_loss:Rank0Tensor, last_input:Tensor, **kwargs):
         "Apply AR and TAR to `last_loss`."
         #AR and TAR
-        if self.alpha != 0.:  last_loss += self.alpha * self.out[-1].pow(2).mean().float()
+        if self.alpha != 0.:  last_loss += self.alpha * self.out[-1].float().pow(2).mean()
         if self.beta != 0.:
             h = self.raw_out[-1]
-            if len(h)>1: last_loss += self.beta * (h[:,1:] - h[:,:-1]).pow(2).mean().float()
+            if len(h)>1: last_loss += self.beta * (h[:,1:] - h[:,:-1]).float().pow(2).mean()
         return last_loss
