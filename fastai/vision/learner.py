a9614ae421e96ce0719c8ef69bf2b2be5ce54f84
Ashish Tamhane <ashish.tamhane@gmail.com>
2019-02-24 11:02:40 -0600

    Added threshold value for heatmap to be computed and displayed(overla… (#1712)
    * Added threshold value for heatmap to be computed and displayed(overlaid) to prevent it when using very small images.
    
    * Type annotation

--- fastai/vision/learner.py
+++ fastai/vision/learner.py
@@ -102,7 +102,7 @@ def _cl_int_from_learner(cls, learn:Learner, ds_type:DatasetType=DatasetType.Val
     preds = learn.TTA(ds_type=ds_type, with_loss=True) if tta else learn.get_preds(ds_type=ds_type, with_loss=True)
     return cls(learn, *preds, ds_type=ds_type)
 
-def _cl_int_plot_top_losses(self, k, largest=True, figsize=(12,12), heatmap:bool=True):
+def _cl_int_plot_top_losses(self, k, largest=True, figsize=(12,12), heatmap:bool=True, heatmap_thresh:int=16):
     "Show images in `top_losses` along with their prediction, actual, loss, and probability of predicted class."
     tl_val,tl_idx = self.top_losses(k, largest)
     classes = self.data.classes
@@ -123,11 +123,12 @@ def _cl_int_plot_top_losses(self, k, largest=True, figsize=(12,12), heatmap:bool
                      preds = m(xb)
                      preds[0,cl].backward()
             acts = hook_a.stored[0].cpu()
-            grad = hook_g.stored[0][0].cpu()
-            grad_chan = grad.mean(1).mean(1)
-            mult = F.relu(((acts*grad_chan[...,None,None])).sum(0))
-            sz = im.shape[-1]
-            axes.flat[i].imshow(mult, alpha=0.6, extent=(0,sz,sz,0), interpolation='bilinear', cmap='magma')
+            if (acts.shape[-1]*acts.shape[-2]) >= heatmap_thresh:
+                grad = hook_g.stored[0][0].cpu()
+                grad_chan = grad.mean(1).mean(1)
+                mult = F.relu(((acts*grad_chan[...,None,None])).sum(0))
+                sz = im.shape[-1]
+                axes.flat[i].imshow(mult, alpha=0.6, extent=(0,sz,sz,0), interpolation='bilinear', cmap='magma')
 
 def _cl_int_plot_multi_top_losses(self, samples:int=3, figsize:Tuple[int,int]=(8,8), save_misclassified:bool=False):
     "Show images in `top_losses` along with their prediction, actual, loss, and probability of predicted class in a multilabeled dataset."
