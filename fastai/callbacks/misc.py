8a0399621a22f2d0af15f548aa6fda722e383ebb
Stas Bekman <stas@stason.org>
2019-03-05 22:35:31 -0800

    fix to accommodate that iteration starts from 0, add test

--- fastai/callbacks/misc.py
+++ fastai/callbacks/misc.py
@@ -4,8 +4,8 @@ from fastai.callback import Callback
 
 class StopAfterNBatches(Callback):
     "Stop training after n batches of the first epoch."
-    def __init__(self, n_batch:int=2):
-        self.stop,self.n_batch = False,n_batch
+    def __init__(self, n_batches:int=2):
+        self.stop,self.n_batches = False,n_batches-1 # iteration starts from 0
 
     def on_batch_end(self, iteration, **kwargs):
-        if iteration >= self.n_batch: return {'stop_epoch': True, 'stop_training': True}
+        if iteration == self.n_batches: return {'stop_epoch': True, 'stop_training': True}
