b0630073b90dc564aa94ac2bf5ddce1d1a55f36d
Konstantin Dorichev <kdorichev@gmail.com>
2019-03-07 18:48:53 +0300

    Typo corrected (#1775)
    * typo
    
    * typo
    
    * Revert "typo"
    
    This reverts commit 16d1a87facb1ef6a9ced1da713079b47c273ea74.

--- fastai/tabular/data.py
+++ fastai/tabular/data.py
@@ -15,7 +15,7 @@ OptTabTfms = Optional[Collection[TabularProc]]
 def emb_sz_rule(n_cat:int)->int: return min(600, round(1.6 * n_cat**0.56))
 
 def def_emb_sz(classes, n, sz_dict=None):
-    "Pick an embedding size for `n` dependinf on `classes` if not given in `sz_dict`."
+    "Pick an embedding size for `n` depending on `classes` if not given in `sz_dict`."
     sz_dict = ifnone(sz_dict, {})
     n_cat = len(classes[n])
     sz = sz_dict.get(n, int(emb_sz_rule(n_cat)))  # rule of thumb
