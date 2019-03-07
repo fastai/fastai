ccc4788c2a5fddaeeb722b052ced246adfa85061
illcat <40688514+illcat@users.noreply.github.com>
2019-03-07 15:03:27 +0800

    Update basics.py

--- fastai/basics.py
+++ fastai/basics.py
@@ -9,17 +9,8 @@ from .torch_core import *
 from .train import *
 from .datasets import *
 from .version import *
-from . import callbacks
+from .callbacks import *
 
-"""
-from . import core,torch_core,basic_data,basic_train,callback,data_block,layers,metrics,train,datasets,callbacks
-
-__all__  = [o for o in dir(core) if not o.startswith('_')]
-__all__ += [o for o in dir(torch_core) if not o.startswith('_')]
-__all__ += [*basic_train.__all__, *callback.__all__, 'core', 'torch_core', 'callbacks',
-           *basic_data.__all__, *data_block.__all__, *layers.__all__, *metrics.__all__,
-           *train.__all__, *datasets.__all__, '__version__']
-"""
 
 try: from .gen_doc.nbdoc import doc
 except: pass  # Optional if jupyter is present
