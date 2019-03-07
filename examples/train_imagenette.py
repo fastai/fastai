4f1602fbd61a208d74b623bac09ee2af269f4403
Jeremy Howard <info@fast.ai>
2019-03-07 11:33:53 +0000

    imagenette updates

--- examples/train_imagenette.py
+++ examples/train_imagenette.py
@@ -13,31 +13,26 @@ def get_data(path, size, bs, workers):
         contrast(scale=(0.7,1.3))
     ], [])
     return (ImageList.from_folder(path).split_by_folder(valid='val')
-            .label_from_folder().transform(tfms, size=size).presize(size)
+            .label_from_folder().transform(tfms, size=size).presize(size, scale=(0.35,1.0))
             .databunch(bs=bs, num_workers=workers).normalize(imagenet_stats))
 
 @call_parse
 def main( gpu:Param("GPU to run on", str)=None ):
     """Distributed training of Imagenette.
     Fastest multi-gpu speed is if you run with: python -m fastai.launch"""
-    path = untar_data(URLs.IMAGENETTE_160)
-    tot_epochs = 40
-    size = 128
-
-    bs,lr = 256,0.3
     gpu = setup_distrib(gpu)
     n_gpus = rank_distrib() or 1
+
+    path = untar_data(URLs.IMAGENETTE_160)
+    tot_epochs,size,lr = 40,128,0.6
+    bs = 256//n_gpus
+
     workers = min(12, num_cpus()//n_gpus)
     data = get_data(path, size, bs, workers)
-    opt_func = partial(optim.SGD, momentum=0.9)
-    learn = Learner(data, models.xresnet50(), metrics=[accuracy,top_k_accuracy], wd=1e-5,
-        opt_func=opt_func, bn_wd=False, true_wd=False,
+    opt_func = partial(optim.Adam, betas=(0.9,0.99), eps=0.3)
+    learn = Learner(data, models.xresnet50(), metrics=[accuracy,top_k_accuracy], wd=1e-3,
+        opt_func=opt_func, bn_wd=False, true_wd=True,
         loss_func = LabelSmoothingCrossEntropy()).mixup(alpha=0.2)
-    learn.callback_fns += [
-        partial(TrackEpochCallback),
-        partial(SaveModelCallback, every='epoch', name='model')
-    ]
-    learn.split(lambda m: (children(m)[-2],))
     if gpu is None: learn.model = nn.DataParallel(learn.model)
     else:           learn.distributed(gpu)
     learn.to_fp16(dynamic=True)
@@ -46,5 +41,6 @@ def main( gpu:Param("GPU to run on", str)=None ):
     tot_bs = bs*n_gpus
     bs_rat = tot_bs/256
     lr *= bs_rat
-    learn.fit_one_cycle(tot_epochs, slice(lr,lr*5), div_factor=5, moms=(0.9,0.9))
+    learn.fit_one_cycle(tot_epochs, lr, div_factor=20, pct_start=0.7, moms=(0.9,0.9))
+    learn.save('nette')
 
