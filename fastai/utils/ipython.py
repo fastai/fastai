24023c3a7d1d0c5b03ff6706b7a53955904f8650
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-03-21 13:20:50 +0000

    Add get_ipython import

--- fastai/utils/ipython.py
+++ fastai/utils/ipython.py
@@ -1,6 +1,7 @@
 "ipython utils"
 
 import os, functools, traceback, gc
+from IPython import get_ipython
 
 def is_in_ipython():
     "Is the code running in the ipython environment (jupyter including)"
