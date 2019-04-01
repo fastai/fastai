f6e9205f2fb109a1e6d5455472923185e7bd94f3
Bobak Farzin <bfarzin@gmail.com>
2019-04-01 16:28:15 -0500

    updates with ctx manger fixed

--- fastai/utils/mod_display.py
+++ fastai/utils/mod_display.py
@@ -2,18 +2,27 @@
 import fastai
 import fastprogress
 
-class progress_diabled():
+#from fastai.basic_train import Learner,Recorder
+#from fastai.core import copy,partial
+from fastai import *
+from ..basic_train import *
+from ..core import *
+
+__all__ = ['progress_disabled']
+
+class progress_disabled():
     ''' Context manager to disable the progress update bar and Recorder print'''
     def __init__(self,learner:Learner):
         self.learn = learner
         self.orig_callback_fns = copy(learner.callback_fns)
-        def __enter__(self):
-            #silence progress bar
-            fastprogress.fastprogress.NO_BAR = True
-            fastai.basic_train.master_bar, fastai.basic_train.progress_bar = fastprogress.force_console_behavior()
-            self.learn.callback_fns[0] = partial(Recorder,add_time=True,silent=True) #silence recorder
-            return self.learn
+    def __enter__(self):
+        #silence progress bar
+        fastprogress.fastprogress.NO_BAR = True
+        fastai.basic_train.master_bar, fastai.basic_train.progress_bar = fastprogress.force_console_behavior()
+        self.learn.callback_fns[0] = partial(Recorder,add_time=True,silent=True) #silence recorder
 
-        def __exit__(self,type,value,traceback):
-            fastai.basic_train.master_bar, fastai.basic_train.progress_bar = master_bar,progress_bar
-            self.learn.callback_fns = self.orig_callback_fns
+        return self.learn
+
+    def __exit__(self,type,value,traceback):
+        fastai.basic_train.master_bar, fastai.basic_train.progress_bar = master_bar,progress_bar
+        self.learn.callback_fns = self.orig_callback_fns
