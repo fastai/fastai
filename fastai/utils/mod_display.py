65367f4fd4196183b4f352a650e29bcdf94a7934
Bobak Farzin <bfarzin@gmail.com>
2019-04-01 20:11:56 -0500

    update code per comments.  remove excess imports, select recorder from list.

--- fastai/utils/mod_display.py
+++ fastai/utils/mod_display.py
@@ -1,10 +1,7 @@
-" Utils for modifying what is displayed in notbooks and command line"
+" Utils for modifying what is displayed in notebooks and command line"
 import fastai
 import fastprogress
 
-#from fastai.basic_train import Learner,Recorder
-#from fastai.core import copy,partial
-from fastai import *
 from ..basic_train import *
 from ..core import *
 
@@ -12,14 +9,17 @@ __all__ = ['progress_disabled']
 
 class progress_disabled():
     ''' Context manager to disable the progress update bar and Recorder print'''
-    def __init__(self,learner:Learner):
-        self.learn = learner
-        self.orig_callback_fns = copy(learner.callback_fns)
+    def __init__(self,learn:Learner):
+        self.learn = learn
+
     def __enter__(self):
         #silence progress bar
         fastprogress.fastprogress.NO_BAR = True
         fastai.basic_train.master_bar, fastai.basic_train.progress_bar = fastprogress.force_console_behavior()
-        self.learn.callback_fns[0] = partial(Recorder,add_time=True,silent=True) #silence recorder
+        self.orig_callback_fns = copy(self.learn.callback_fns)
+        rec_name = [x for x in self.learn.callback_fns if x.func == Recorder][0]
+        rec_idx = self.learn.callback_fns.index(rec_name)
+        self.learn.callback_fns[rec_idx] = partial(Recorder,add_time=True,silent=True) #silence recorder
 
         return self.learn
 
