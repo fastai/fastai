Fix off-by-one error in LanguageLearner show_results
As pointed out on the forums here, `LanguageLearner.show_results` is off by one word when displaying the `target` and `pred`. This happens because x and y are one word offset from each other coming from `one_batch` (which is ultimately pulling from `LanguageModelPreLoader.__getitem__`).

fastai/text/learner.py
@@ -176,8 +176,8 @@ class LanguageLearner(RNNLearner):
         items,names = [],['text', 'target', 'pred']
         for i, (x,y,z) in enumerate(zip(xs,ys,zs)):
             txt_x = ' '.join(x.text.split(' ')[:max_len])
-            txt_y = ' '.join(y.text.split(' ')[max_len:2*max_len])
-            txt_z = ' '.join(z.text.split(' ')[max_len:2*max_len])
+            txt_y = ' '.join(y.text.split(' ')[max_len-1:2*max_len-1])
+            txt_z = ' '.join(z.text.split(' ')[max_len-1:2*max_len-1])
             items.append([txt_x, txt_y, txt_z])
         items = np.array(items)
         df = pd.DataFrame({n:items[:,i] for i,n in enumerate(names)}, columns=names)