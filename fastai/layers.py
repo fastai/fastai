5c51f9eabf76853a89a9bc5741804d2ed4407e49
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-04-10 18:26:22 +0000

    More refactor

--- fastai/layers.py
+++ fastai/layers.py
@@ -329,7 +329,8 @@ class LabelSmoothingCrossEntropy(nn.Module):
     def forward(self, output, target):
         c = output.size()[-1]
         log_preds = F.log_softmax(output, dim=-1)
-        if self.reduction == 'mean':  loss = -log_preds.sum(dim=-1).mean()
-        elif self.reduction == 'sum': loss = -log_preds.sum()
-        else:                         loss = -log_preds.sum(dim=-1)
-        return loss * self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, reduction=self.reduction)
+        if self.reduction=='sum': loss = -log_preds.sum()
+        else:
+            loss = -log_preds.sum(dim=-1)
+            if self.reduction=='mean':  loss = loss.mean()
+        return loss*self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, reduction=self.reduction)
