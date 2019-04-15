b68f9b883caa1ee64da95bae35cfcf1aefc99c59
James Dietle <jdietle@gmail.com>
2019-04-15 08:06:55 -0500

    Reduce mem usage function.  (#1973)
    * Including reduce memory make dataset smaller by changing types. Should be done before a large train.
    
    * Made changes to prevent typing to float64 and printing error

--- fastai/utils/mem.py
+++ fastai/utils/mem.py
@@ -173,3 +173,46 @@ def gpu_mem_trace(func):
         with GPUMemTrace(ctx=func.__qualname__, on_exit_report=True):
             return func(*args, **kwargs)
     return wrapper
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    #Removed from debugging
+    columns = df.columns
+    #.drop('index')
+
+    for col in columns:
+        col_type = df[col].dtype
+        if str(col_type) != 'category' and col_type != 'datetime64[ns]' and col_type != bool:
+            if col_type != object:
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                else:
+                    #if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        #df[col] = df[col].astype(np.float16)
+                    #Sometimes causes and error and had to remove
+                    if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        print('Error '+col+' Value would be a float64. Disregarding.')
+            else:
+                df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
