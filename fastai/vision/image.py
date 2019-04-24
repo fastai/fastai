c26b06b704f08e1750f1dfc4540243126a1eb4cb
Dominik Engel <dominik.engel@mail.de>
2019-04-24 17:06:57 +0200

    add skip_y to Transform.__call__

--- fastai/vision/image.py
+++ fastai/vision/image.py
@@ -459,10 +459,10 @@ class Transform():
         setattr(Image, func.__name__,
                 lambda x, *args, **kwargs: self.calc(x, *args, **kwargs))
 
-    def __call__(self, *args:Any, p:float=1., is_random:bool=True, **kwargs:Any)->Image:
+    def __call__(self, *args:Any, p:float=1., is_random:bool=True, skip_y=False, **kwargs:Any)->Image:
         "Calc now if `args` passed; else create a transform called prob `p` if `random`."
         if args: return self.calc(*args, **kwargs)
-        else: return RandTransform(self, kwargs=kwargs, is_random=is_random, p=p)
+        else: return RandTransform(self, kwargs=kwargs, is_random=is_random, skip_y=skip_y, p=p)
 
     def calc(self, x:Image, *args:Any, **kwargs:Any)->Image:
         "Apply to image `x`, wrapping it if necessary."
@@ -483,7 +483,7 @@ class RandTransform():
     resolved:dict = field(default_factory=dict)
     do_run:bool = True
     is_random:bool = True
-    skip_y=False
+    skip_y:bool = False
     def __post_init__(self): functools.update_wrapper(self, self.tfm)
 
     def resolve(self)->None:
