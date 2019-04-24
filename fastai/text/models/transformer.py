5bf24a8503494e124a1d6cf7c814bb1bde49fcea
Kushajveer Singh <kushajreal@gmail.com>
2019-04-24 21:26:53 +0530

    Removing duplicate code for x.size(1)

--- fastai/text/models/transformer.py
+++ fastai/text/models/transformer.py
@@ -105,7 +105,7 @@ class MultiHeadRelativeAttention(MultiHeadAttention):
         context = x if mem is None else torch.cat([mem, x], dim=1)
         wq,wk,wv = torch.chunk(self.attention(context), 3, dim=-1)
         wq = wq[:,-x_len:]
-        wq,wk,wv = map(lambda x:x.view(bs, x.size(1), self.n_heads, self.d_head), (wq,wk,wv))
+        wq,wk,wv = map(lambda x:x.view(bs, x_len, self.n_heads, self.d_head), (wq,wk,wv))
         wq,wk,wv = wq.permute(0, 2, 1, 3),wk.permute(0, 2, 3, 1),wv.permute(0, 2, 1, 3)
         wkr = self.r_attn(r)
         wkr = wkr.view(seq_len, self.n_heads, self.d_head)
@@ -127,7 +127,7 @@ class MultiHeadRelativeAttention(MultiHeadAttention):
         wq,wk,wv = torch.chunk(self.attention(context), 3, dim=-1)
         wq = wq[:,-x_len:]
         wkr = self.r_attn(r)
-        wq,wk,wv = map(lambda x:x.view(bs, x.size(1), self.n_heads, self.d_head), (wq,wk,wv))
+        wq,wk,wv = map(lambda x:x.view(bs, x_len, self.n_heads, self.d_head), (wq,wk,wv))
         wkr = wkr.view(seq_len, self.n_heads, self.d_head)     
         #### compute attention score (AC is (a) + (c) and BS is (b) + (d) in the paper)
         AC = torch.einsum('bind,bjnd->bijn', (wq+u, wk))
