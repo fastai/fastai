f8d6fdf80d8c3faaf881663ec49564f90bd10617
Kerem Turgutlu <keremturgutlu@gmail.com>
2019-05-25 01:45:22 -0700

    interpret module init

--- fastai/interpret.py
+++ fastai/interpret.py
@@ -14,33 +14,88 @@ class Interpretation():
         preds_res = learn.get_preds(ds_type=ds_type, with_loss=True)
         return cls(learn, *preds_res)
 
+    def top_losses(self, k:int=None, largest=True):
+        "`k` largest(/smallest) losses and indexes, defaulting to all losses (sorted by `largest`)."
+        return self.losses.topk(ifnone(k, len(self.losses)), largest=largest)
+
+    # def top_scores(self, metric:Callable=None, k:int=None, largest=True):
+    #     "`k` largest(/smallest) metric scores and indexes, defaulting to all scores (sorted by `largest`)."
+    #     self.scores = metric(self.preds, self.y_true) 
+    #     return self.scores.topk(ifnone(k, len(self.scores)), largest=largest)
+
+
+
+class SegmentationInterpretation(Interpretation):
+    "Interpretation methods for classification models."
+    def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor,
+                 ds_type:DatasetType=DatasetType.Valid):
+        super(SegmentationInterpretation, self).__init__(learn,preds,y_true,losses,ds_type)
+        self.pred_class = self.preds.argmax(dim=1)
+        self.c2i = {c:i for i,c in enumerate(self.data.classes)}
+        self.i2c = {i:c for c,i in self.c2i.items()}
+    
+    def top_losses(self, sizes:Tuple, k:int=None, largest=True):
+        "Reduce flatten loss to give a single loss value for each image"
+        losses = self.losses.view(-1, np.prod(sizes)).mean(-1)
+        return losses.topk(ifnone(k, len(losses)), largest=largest)
+    
+    def _interp_show(self, ims:ImageSegment, classes:Collection, sz:int=20, cmap='tab20',
+                    title_suffix:str=None):
+        '''
+        ims: ImageSegment to plot
+        classes: List of classes to plot
+        sz: figsize
+        cmap: colormap
+        c2i: class to pixel value map
+
+        '''
+        fig,axes=plt.subplots(1,2,figsize=(sz,sz))
+        class_idxs = [self.c2i[c] for c in classes]
+        
+        #image
+        mask = torch.cat([ims.data==i for i in class_idxs]).max(dim=0)[0][None,:].long()
+        masked_im = image2np(ims.data*mask)
+        im=axes[0].imshow(masked_im, cmap=cmap)
+
+        #labels
+        masked_im_labels = list(np.unique(masked_im))
+        c = len(masked_im_labels); n = math.ceil(np.sqrt(c))
+        label_im = np.array(masked_im_labels + [np.nan]*(n**2-c)).reshape(n,n)
+        axes[1].imshow(label_im, cmap=cmap)
+        for i,l in enumerate([self.i2c[l] for l in masked_im_labels]):
+            div,mod=divmod(i,n)
+            axes[1].text(mod, div, f"{l}", ha='center', color='white', fontdict={'size':sz})
+
+        if title_suffix:
+            axes[0].set_title(f"{title_suffix}_imsegment");
+            axes[1].set_title(f"{title_suffix}_labels");
+
+    def show_xyz(self, i, classes=None, sz=10):
+        'show image, true and pred'
+        classes = ifnone(classes, self.data.classes)
+        x,y = interp.data.valid_ds[i]
+        self.data.valid_ds.x.show_xys([x],[y], figsize=(sz/2,sz/2))
+        self._interp_show(ImageSegment(self.y_true[i]), classes, sz=sz, title_suffix='true')
+        self._interp_show(ImageSegment(self.pred_class[i][None,:]), classes, sz=sz, title_suffix='pred')
+
 
 
 class MultiLabelClassificationInterpretation(Interpretation):
     "Interpretation methods for classification models."
     def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor, ds_type:DatasetType=DatasetType.Valid,
                      sigmoid:bool=True, thresh:float=0.3):
+        raise NotImplementedError
         super(MultiLabelClassificationInterpretation, self).__init__(learn,preds,y_true,losses,ds_type)
         self.pred_class = self.preds.sigmoid(dim=1)>thresh if sigmoid else self.preds>thresh
-
+        
 
 
 class ObjectDetectionInterpretation(Interpretation):
     "Interpretation methods for classification models."
-    def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor, ds_type:DatasetType=DatasetType.Valid,
-                     sigmoid:bool=True, thresh:float=0.3):
-        super(ObjectDetectionInterpretation, self).__init__(learn,preds,y_true,losses,ds_type)
+    def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor, ds_type:DatasetType=DatasetType.Valid):
         raise NotImplementedError
-
-
-
-class SegmentationInterpretation(Interpretation):
-    "Interpretation methods for classification models."
-    def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor, ds_type:DatasetType=DatasetType.Valid,
-                     sigmoid:bool=True, thresh:float=0.3):
-        super(SegmentationInterpretation, self).__init__(learn,preds,y_true,losses,ds_type)
-        self.pred_class = self.preds.argmax(dim=1)
-
+        super(ObjectDetectionInterpretation, self).__init__(learn,preds,y_true,losses,ds_type)
+        
 
 
 class ClassificationInterpretation(Interpretation):
@@ -93,10 +148,6 @@ class ClassificationInterpretation(Interpretation):
         res = [(self.data.classes[i],self.data.classes[j],cm[i,j])
                 for i,j in zip(*np.where(cm>=min_val))]
         return sorted(res, key=itemgetter(2), reverse=True)
-    
-    def top_losses(self, k:int=None, largest=True):
-        "`k` largest(/smallest) losses and indexes, defaulting to all losses (sorted by `largest`)."
-        return self.losses.topk(ifnone(k, len(self.losses)), largest=largest)
 
 
 
