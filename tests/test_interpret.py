a8d6a441087aa05523b3df95885e05fb0d122482
Kerem Turgutlu <keremturgutlu@gmail.com>
2019-05-25 22:12:20 -0700

    add test file for interpret.py

---
+++ tests/test_interpret.py
@@ -0,0 +1,6 @@
+import pytest
+from fastai.gen_doc.doctest import this_tests
+from fastai.basics import *
+from fastai.interpret import *
+
+#I'm all empty, please fill me with tests about the functions in layers.py
