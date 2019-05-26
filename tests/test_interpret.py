11af0393b3e62b17e71984dc3e92426ff3d55cb4
Kerem Turgutlu <keremturgutlu@gmail.com>
2019-05-26 13:37:02 -0700

    delete test interpet.py

--- tests/test_interpret.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from fastai.gen_doc.doctest import this_tests
-from fastai.basics import *
-from fastai.interpret import *
-
-#I'm all empty, please fill me with tests about the functions in layers.py
