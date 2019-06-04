Removed normalization from COCO_load, for now. Fixed imports for yolo part.

fastai/vision/models/yolov3.py
@@ -53,7 +53,7 @@ def parse_cfg(model):
     """
 
     blocks = []
-    with open('./data/cfg/{}.cfg'.format(model), 'r') as file:
+    with open('d:/Studia/IVrok/ADPB/pycharm_fastai/data/cfg/{}.cfg'.format(model), 'r') as file:
         block = {}
         for line in file:
             line = line.strip()
@@ -327,7 +327,7 @@ class YOLOv3(nn.Module):
         return detections
 
     def load_weights(self, model_name):
-        file = open('./data/weights/{}.weights'.format(model_name), 'rb')
+        file = open('d:/Studia/IVrok/ADPB/weights/{}.weights'.format(model_name), 'rb')
         header = torch.from_numpy(np.fromfile(file, dtype=np.int32, count=5))
         for i, module in enumerate(self.modules_list):
 

tests/test_vision_learner.py
@@ -1,14 +1,14 @@
 import pytest
 import torch
 import torch.nn as nn
-from fastai.gen_doc.doctest import this_tests
-from fastai.vision.learner import *
-from fastai.callbacks.hooks import *
+from ..fastai.gen_doc.doctest import this_tests
+from ..fastai.vision.learner import *
+from ..fastai.callbacks.hooks import *
 from torchvision.models import resnet18
 from torchvision.models.resnet import BasicBlock
-from fastai.datasets import *
-from fastai.vision.data import *
-from fastai.vision import *
+from ..fastai.datasets import *
+from ..fastai.vision.data import *
+from ..fastai.vision import *
 
 @pytest.fixture
 def image():
@@ -47,10 +47,8 @@ def test_create_head(image):
 
 def test_yolo_learner():
     tfms = get_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.)
-    data = COCO_load('/home/marni/COCO/', train_annot='/home/marni/COCO/annotations/instances_train2017_clipped.json',
-                     valid_annot='/home/marni/COCO/annotations/instances_val2017_clipped.json', tfms=tfms)
+    data = COCO_load('d:/Studia/IVrok/ADPB/Coco', tfms=tfms, batch_size=2)
     learner = yolo_learner(data)
     learner.predict()
     learner.show_results()
-
 