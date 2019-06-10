097a942622a4fe172919a7126bacc207fdda0eb6
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-06-10 14:52:17 +0000

    Add data creation in script

--- examples/train_wt103.py
+++ examples/train_wt103.py
@@ -1,10 +1,43 @@
 ##To train a language model on Wikitext-103
 ##`python train_wt103.py fwd` for the forward pretrained model in fastai
 ##`python train_wt103.py bwd --backwards True` for the backward pretrained model in fastai
+## Takes 6 hours on a Titan RTX (24Gb RAM), adjust batch size and lr if less GPU RAM
 
 from fastai.text import *
 from fastai.script import *
 from fastprogress import fastprogress
+
+#Functions to parse WT103 in separate articles
+def istitle(line):
+    return len(re.findall(r'^ = [^=]* = $', line)) != 0
+
+def read_file(filename):
+    articles = []
+    with open(filename, encoding='utf8') as f:
+        lines = f.readlines()
+    current_article = ''
+    for i,line in enumerate(lines):
+        current_article += line
+        if i < len(lines)-2 and lines[i+1] == ' \n' and istitle(lines[i+2]):
+            current_article = current_article.replace('<unk>', UNK)
+            articles.append(current_article)
+            current_article = ''
+    current_article = current_article.replace('<unk>', UNK)
+    articles.append(current_article)
+    return np.array(articles)
+
+def create_data(path):
+    train = read_file(path/'train.txt')
+    valid = read_file(path/'valid.txt')
+    test =  read_file(path/'test.txt')
+    all_texts = np.concatenate([valid, train, test])
+    df = pd.DataFrame({'texts':all_texts})
+    del train ; del valid ; del text #Free RQM before tokenizing
+    data = (TextList.from_df(df, path, cols='texts')
+                    .split_by_idx(range(0,60))
+                    .label_for_lm()
+                    .databunch())
+    data.save()
 
 @call_parse
 def main(
@@ -22,6 +55,7 @@ def main(
     path = Config().data_path()/'wikitext-103'
     fastprogress.SAVE_PATH = f'{name}.txt' #Save the output of the progress bar in {name}.txt
     torch.cuda.set_device(gpu)
+    if not (path/'data_save.pkl').is_file(): create_data(path)
     data = load_data(path, bs=bs, bptt=bptt, backwards=backwards)
     learn = language_model_learner(data, AWD_LSTM, drop_mult=drop_mult, pretrained=False,
                                    metrics=[accuracy, Perplexity()])
