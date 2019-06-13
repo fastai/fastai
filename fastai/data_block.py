f948245547b8201226a7720a1d469616ed5b841e
Lewis Fishgold <lewfish@gmail.com>
2019-06-13 10:44:41 -0400

    Add support for training sampler
    This adds support for a sampler to use for the training set DataLoader.
    This was motivated by wanting to use a WeightedRandomSampler to support
    oversampling of rare classes. The sampler can't simply be passed in as a
    kwarg to databunch() because we only want to pass it to the train_dl.
    Also, when specifying a sampler, we need to turn off shuffle for the
    train_dl because that option is not compatible with setting a sampler --
    PyTorch won't allow it.

--- fastai/data_block.py
+++ fastai/data_block.py
@@ -540,11 +540,12 @@ class LabelLists(ItemLists):
 
     def databunch(self, path:PathOrStr=None, bs:int=64, val_bs:int=None, num_workers:int=defaults.cpus,
                   dl_tfms:Optional[Collection[Callable]]=None, device:torch.device=None, collate_fn:Callable=data_collate,
-                  no_check:bool=False, **kwargs)->'DataBunch':
+                  no_check:bool=False, train_sampler:torch.utils.data.Sampler=None, **kwargs)->'DataBunch':
         "Create an `DataBunch` from self, `path` will override `self.path`, `kwargs` are passed to `DataBunch.create`."
         path = Path(ifnone(path, self.path))
         data = self.x._bunch.create(self.train, self.valid, test_ds=self.test, path=path, bs=bs, val_bs=val_bs,
-                                    num_workers=num_workers, dl_tfms=dl_tfms, device=device, collate_fn=collate_fn, no_check=no_check, **kwargs)
+                                    num_workers=num_workers, dl_tfms=dl_tfms, device=device, collate_fn=collate_fn, no_check=no_check,
+                                    train_sampler=train_sampler, **kwargs)
         if getattr(self, 'normalize', False):#In case a normalization was serialized
             norm = self.normalize
             data.normalize((norm['mean'], norm['std']), do_x=norm['do_x'], do_y=norm['do_y'])
