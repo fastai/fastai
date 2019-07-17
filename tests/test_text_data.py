214cd66732c0d1cc8156fbe4c305a546c70a06aa
chui <chui@freelancer.com>
2019-07-17 16:23:34 +1000

    Fix export from ids Add test cases

--- tests/test_text_data.py
+++ tests/test_text_data.py
@@ -186,6 +186,17 @@ def test_from_ids_works_for_variable_length_sentences():
                                       train_ids=ids, train_lbls=lbl,
                                       valid_ids=ids, valid_lbls=lbl, classes={0:0}, bs=8)
 
+def test_from_ids_exports_classes():
+    this_tests(TextClasDataBunch.from_ids)
+    ids = [np.array([0]),np.array([0,1])]*5 # notice diffrent number of elements in arrays
+    lbl = [0]*10
+    data = TextClasDataBunch.from_ids('/tmp', vocab=Vocab({0: BOS, 1:PAD}),
+                                      train_ids=ids, train_lbls=lbl,
+                                      valid_ids=ids, valid_lbls=lbl, classes={0:0}, bs=8)
+    data.export('/tmp')
+    empty_data = TextClasDataBunch.load_empty('/tmp')
+    assert empty_data.classes is not None
+
 def test_regression():
     this_tests('na')
     path = untar_data(URLs.IMDB_SAMPLE)
