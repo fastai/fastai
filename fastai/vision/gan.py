5d7ee580d84d1441092229d65914a0ee792c8269
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-08-29 11:29:26 +0200

    Fix #2301

--- fastai/vision/gan.py
+++ fastai/vision/gan.py
@@ -301,4 +301,4 @@ class AdaptiveLoss(Module):
 def accuracy_thresh_expand(y_pred:Tensor, y_true:Tensor, thresh:float=0.5, sigmoid:bool=True)->Rank0Tensor:
     "Compute accuracy after expanding `y_true` to the size of `y_pred`."
     if sigmoid: y_pred = y_pred.sigmoid()
-    return ((y_pred>thresh)==y_true[:,None].expand_as(y_pred).byte()).float().mean()
+    return ((y_pred>thresh).byte()==y_true[:,None].expand_as(y_pred).byte()).float().mean()
