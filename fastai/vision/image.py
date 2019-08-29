c7427413623079162dd0b3d388aaa7a600837848
vainaijr <43755000+vainaijr@users.noreply.github.com>
2019-08-29 18:38:03 +0900

    show_image support skimage data images (#2300)
    * show_image supports skimage data images
    
    * regarding cmap, alpha
    
    I modified it, but I am not familiar with cmap, alpha, it gives different image, using cmap='binary' when I do
    `
    from skimage import data
    show_image(data.coins())
    `
    as compared to when not using cmap

--- fastai/vision/image.py
+++ fastai/vision/image.py
@@ -432,7 +432,8 @@ def show_image(img:Image, ax:plt.Axes=None, figsize:tuple=(3,3), hide_axis:bool=
                 alpha:float=None, **kwargs)->plt.Axes:
     "Display `Image` in notebook."
     if ax is None: fig,ax = plt.subplots(figsize=figsize)
-    ax.imshow(image2np(img.data), cmap=cmap, alpha=alpha, **kwargs)
+    xtr = dict(cmap=cmap, alpha=alpha, **kwargs)
+    ax.imshow(image2np(img.data), **xtr) if (hasattr(img, 'cpu')) else ax.imshow(img, **xtr)
     if hide_axis: ax.axis('off')
     return ax
 
