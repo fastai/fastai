e621d438ba0f3aa5a9bfa513b521bafc97f6e5fe
Zachary Mueller <muellerzr@gmail.com>
2019-08-31 01:26:41 -0400

    simplified scheduler call

--- fastai/callbacks/flat_cos_anneal.py
+++ fastai/callbacks/flat_cos_anneal.py
@@ -15,12 +15,11 @@ class FlatCosAnnealScheduler(LearnerCallback):
   https://forums.fast.ai/t/how-we-beat-the-5-epoch-imagewoof-leaderboard-score-some-new-techniques-to-consider/53453
   Based on idea by Mikhail Grankin
   """
-  def __init__(self, learn:Learner, lr:float=4e-3, moms:Floats=(0.95,0.999),
-             start_pct:float=0.72, start_epoch:int=1, tot_epochs:int=None,
-              curve='cosine'):
+  def __init__(self, learn:Learner, lr:float=4e-3, tot_epochs:int=1, moms:Floats=(0.95,0.999),
+             start_pct:float=0.72, curve='cosine'):
       super().__init__(learn)
       n = len(learn.data.train_dl)
-      self.start_epoch, self.tot_epochs = start_epoch, tot_epochs
+      self.tot_epochs = tot_epochs
       self.anneal_start = int(n * self.tot_epochs * start_pct)
       self.batch_finish = (n * self.tot_epochs - self.anneal_start)
       if curve=="cosine":
