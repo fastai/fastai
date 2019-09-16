960f98b3742133604aa990a8cb18ce38bef8e8b4
ItsByteMe <39222138+ItsByteMe@users.noreply.github.com>
2019-09-15 20:06:55 -0400

    Inconsistency in comment spacing on line 6 fixed
    There was a lack of space between the comment tag the actual comment, which is inconsistent with the rest of the file, so I fixed it.

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -3,7 +3,7 @@ $('#mysidebar').height($(".nav").height());
 
 $( document ).ready(function() {
 
-    //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
+    // this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
     // position as your scroll. if you have a lot of nav items, this height may not work for you.
     var h = $(window).height();
     //console.log (h);
