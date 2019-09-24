a91265c1b6bacaa0ec5cc61d50dc7ef00e754b2c
Josh Varty <JoshVarty@users.noreply.github.com>
2019-09-24 17:21:26 -0400

    Expose tfm_y argument in load_learner (#2351)
    When loading an exported learner, we want callers to be able to choose whether or not to apply transforms against y.

--- fastai/basic_train.py
+++ fastai/basic_train.py
@@ -608,13 +608,13 @@ def load_callback(class_func, state, learn:Learner):
     for k,v in others.items(): setattr(res, k, v)
     return res
 
-def load_learner(path:PathOrStr, file:PathLikeOrBinaryStream='export.pkl', test:ItemList=None, **db_kwargs):
+def load_learner(path:PathOrStr, file:PathLikeOrBinaryStream='export.pkl', test:ItemList=None, tfm_y=None, **db_kwargs):
     "Load a `Learner` object saved with `export_state` in `path/file` with empty data, optionally add `test` and load on `cpu`. `file` can be file-like (file or buffer)"
     source = Path(path)/file if is_pathlike(file) else file
     state = torch.load(source, map_location='cpu') if defaults.device == torch.device('cpu') else torch.load(source)
     model = state.pop('model')
     src = LabelLists.load_state(path, state.pop('data'))
-    if test is not None: src.add_test(test)
+    if test is not None: src.add_test(test, tfm_y=tfm_y)
     data = src.databunch(**db_kwargs)
     cb_state = state.pop('cb_state')
     clas_func = state.pop('cls')
