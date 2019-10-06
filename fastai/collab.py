af395a701d6660543c49804b4f2cb53a6e67f261
Abhi Mahule <55562157+abhi-mahule-dtl@users.noreply.github.com>
2019-10-06 06:18:57 -0700

    Update collab.py (#2362)
    "Weight" instead of "Bias" in the documentation

--- fastai/collab.py
+++ fastai/collab.py
@@ -87,7 +87,7 @@ class CollabLearner(Learner):
         return layer(idx).squeeze()
 
     def weight(self, arr:Collection, is_item:bool=True):
-        "Bias for item or user (based on `is_item`) for all in `arr`. (Set model to `cpu` and no grad.)"
+        "Weight for item or user (based on `is_item`) for all in `arr`. (Set model to `cpu` and no grad.)"
         idx = self.get_idx(arr, is_item)
         m = self.model
         layer = m.i_weight if is_item else m.u_weight
