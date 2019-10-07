1ad51d4fdbea4e5917f97d3eced143421a77099d
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-10-07 09:32:41 -0700

    Remove unnecessary test

--- tests/test_basic_train.py
+++ tests/test_basic_train.py
@@ -87,20 +87,6 @@ def test_purge():
     learn.purge()
     learn.model_dir = model_dir_orig
 
-    # should fail to purge with a non-existent path
-    learn.model_dir = "read_only"
-    os.makedirs(learn.path/"read_only")
-    os.chmod(learn.path/"read_only", mode=444)
-    
-    try: learn.purge()
-    except Exception as e:
-        assert "Can't write to" in str(e) # should fail
-    else: assert False, "should have failed with non-writable path"
-
-    finally: # restore the learner fixture
-        learn.model_dir = model_dir_orig
-        shutil.rmtree(learn.path/"read_only")
-
 def test_save_load(learn):
     this_tests(learn.save, learn.load, learn.purge)
     name = 'mnist-tiny-test-save-load'
