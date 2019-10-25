885512cc4cb7b483e9ad449c7359ab63998dc627
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-10-25 12:14:57 -0700

    Ignore warning

--- fastai/torch_core.py
+++ fastai/torch_core.py
@@ -413,7 +413,11 @@ def add_metrics(last_metrics:Collection[Rank0Tensor], mets:Union[Rank0Tensor, Co
 
 def try_save(state:Dict, path:Path=None, file:PathLikeOrBinaryStream=None):
     target = open(path/file, 'wb') if is_pathlike(file) else file
-    try: torch.save(state, target)
+    try: 
+        with warnings.catch_warnings():
+            #To avoid the warning that come from PyTorch about model not being checked
+            warnings.simplefilter("ignore")
+            torch.save(state, target)
     except OSError as e:
         raise Exception(f"{e}\n Can't write {path/file}. Pass an absolute writable pathlib obj `fname`.")
 
