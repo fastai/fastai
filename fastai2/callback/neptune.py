ae49e5d3c928b7e82d5fd96ada7b485fd5ba163e
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-02-14 15:51:49 +0100

    Added flag to leave exp open, minor corrections

--- fastai2/callback/neptune.py
+++ fastai2/callback/neptune.py
@@ -13,9 +13,10 @@ import neptune
 # Cell
 class NeptuneCallback(Callback):
     "Log losses, metrics, model weights, model architecture summary to neptune"
-    def __init__(self, log_model_weights=True):
+    def __init__(self, log_model_weights=True, keep_experiment_running=False):
         self.log_model_weights = log_model_weights
-        self.neptune_exp = None
+        self.keep_experiment_running = keep_experiment_running
+        self.experiment = None
 
         if neptune.project is None:
             raise ValueError('You did not initialize project in neptune.\n',
@@ -23,13 +24,13 @@ class NeptuneCallback(Callback):
 
     def begin_fit(self):
         try:
-            self.neptune_exp = neptune.get_experiment()
+            self.experiment = neptune.get_experiment()
         except ValueError:
             print('No active experiment. Please invoke `neptune.create_experiment()` before this callback.')
 
         try:
-            self.neptune_exp.set_property('n_epoch', str(self.learn.n_epoch))
-            self.neptune_exp.set_property('model_class', str(type(self.learn.model)))
+            self.experiment.set_property('n_epoch', str(self.learn.n_epoch))
+            self.experiment.set_property('model_class', str(type(self.learn.model)))
         except:
             print(f'Did not log all properties. Check properties in the {neptune.get_experiment()}.')
 
@@ -37,7 +38,7 @@ class NeptuneCallback(Callback):
             with tempfile.NamedTemporaryFile(mode='w') as f:
                 with open(f.name, 'w') as g:
                     g.write(repr(self.learn.model))
-                self.neptune_exp.log_artifact(f.name, 'model_summary.txt')
+                self.experiment.log_artifact(f.name, 'model_summary.txt')
         except:
             print('Did not log model summary. Check if your model is PyTorch model.')
 
@@ -47,22 +48,22 @@ class NeptuneCallback(Callback):
 
     def after_batch(self):
         # log loss and opt.hypers
-        self.neptune_exp.set_property('n_iter', str(self.learn.n_iter))
+        self.experiment.set_property('n_iter', str(self.learn.n_iter))
         if self.learn.training:
-            self.neptune_exp.log_metric('batch__smooth_loss', self.learn.smooth_loss)
-            self.neptune_exp.log_metric('batch__loss', self.learn.loss)
-            self.neptune_exp.log_metric('batch__train_iter', self.learn.train_iter)
+            self.experiment.log_metric('batch__smooth_loss', self.learn.smooth_loss)
+            self.experiment.log_metric('batch__loss', self.learn.loss)
+            self.experiment.log_metric('batch__train_iter', self.learn.train_iter)
             for i, h in enumerate(self.learn.opt.hypers):
                 for k, v in h.items():
-                    self.neptune_exp.log_metric(f'batch__opt.hypers.{k}', v)
+                    self.experiment.log_metric(f'batch__opt.hypers.{k}', v)
 
     def after_epoch(self):
         # log metrics
         for n, v in zip(self.learn.recorder.metric_names, self.learn.recorder.log):
             if n not in ['epoch', 'time']:
-                self.neptune_exp.log_metric(f'epoch__{n}', v)
+                self.experiment.log_metric(f'epoch__{n}', v)
             if n == 'time':
-                self.neptune_exp.log_text(f'epoch__{n}', str(v))
+                self.experiment.log_text(f'epoch__{n}', str(v))
 
         # log model weights
         if self.log_model_weights and hasattr(self.learn, 'save_model'):
@@ -74,10 +75,14 @@ class NeptuneCallback(Callback):
                 _file = join_path_file(self.learn.save_model.fname,
                                        self.learn.path / self.learn.model_dir,
                                        ext='.pth')
-            self.neptune_exp.log_artifact(_file)
+            self.experiment.log_artifact(_file)
 
     def after_fit(self):
-        try:
-            self.neptune_exp.stop()
-        except:
-            print('No neptune experiment to stop.')
+        if not self.keep_experiment_running:
+            try:
+                self.experiment.stop()
+            except:
+                print('No neptune experiment to stop.')
+        else:
+            print(f'Your experiment (id: {self.experiment.id}, name: {self.experiment.name}) is left in the running state.\n',
+                  'You can log more data to it, like this: `neptune.log_metric()`')
