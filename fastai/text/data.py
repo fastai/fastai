94619b01442ee4f0d31de4554304a52cfbcdc831
Angertdev Singh <angert99@gmail.com>
2020-03-01 03:25:48 +0800

    Update train_sentencepiece to accept paths with spaces

--- fastai/text/data.py
+++ fastai/text/data.py
@@ -427,11 +427,11 @@ def train_sentencepiece(texts:Collection[str], path:PathOrStr, pre_rules: ListRu
     with open(raw_text_path, 'w', encoding=enc) as f: f.write("\n".join(texts))
     spec_tokens = ['\u2581'+s for s in defaults.text_spec_tok]
     SentencePieceTrainer.Train(" ".join([
-        f"--input={raw_text_path} --max_sentence_length={max_sentence_len}",
+        f"--input={\" + str(raw_text_path) + \"} --max_sentence_length={max_sentence_len}",
         f"--character_coverage={ifnone(char_coverage, 0.99999 if lang in full_char_coverage_langs else 0.9998)}",
         f"--unk_id={len(defaults.text_spec_tok)} --pad_id=-1 --bos_id=-1 --eos_id=-1",
         f"--user_defined_symbols={','.join(spec_tokens)}",
-        f"--model_prefix={cache_dir/'spm'} --vocab_size={vocab_sz} --model_type={model_type}"]))
+        f"--model_prefix={\" + str(cache_dir/'spm') + \"} --vocab_size={vocab_sz} --model_type={model_type}"]))
     raw_text_path.unlink()
     return cache_dir
 
