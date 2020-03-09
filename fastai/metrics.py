fixing CM computation with numpy type conversion

fastai/metrics.py
@@ -155,8 +155,10 @@ class ConfusionMatrix(Callback):
         targs = last_target.cpu()
         if self.n_classes == 0:
             self.n_classes = last_output.shape[-1]
-        if self.cm is None: self.cm = torch.zeros((n_classes, n_classes))
-        self.cm[targs, preds] += 1
+        if self.cm is None: self.cm = torch.zeros((n_classes, n_classes), device=torch.device('cpu'), dtype=torch.int32)
+        cm_temp_numpy = self.cm.numpy()
+        np.add.at(cm_temp_numpy, (targs ,preds), 1)
+        self.cm = torch.from_numpy(cm_temp_numpy)
 
     def on_epoch_end(self, **kwargs):
         self.metric = self.cm