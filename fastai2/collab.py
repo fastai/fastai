e047e407001afebaa5156f01e5a276a412b860c7
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-03-23 14:54:11 -0700

    Clean up and add doc

--- fastai2/collab.py
+++ fastai2/collab.py
@@ -7,6 +7,7 @@ from .tabular.all import *
 
 # Cell
 class TabularCollab(TabularPandas):
+    "Instance of `TabularPandas` suitable for collaborative filtering (with no continuous variable)"
     with_cont=False
 
 # Cell
@@ -16,19 +17,20 @@ class CollabDataLoaders(DataLoaders):
     @classmethod
     def from_df(cls, ratings, valid_pct=0.2, user_name=None, item_name=None, rating_name=None, seed=None, path='.', **kwargs):
         "Create a `DataLoaders` suitable for collaborative filtering from `ratings`."
-        user_name   = ifnone(user_name,  ratings.columns[0])
-        item_name   = ifnone(item_name,  ratings.columns[1])
-        rating_name = ifnone(rating_name,ratings.columns[2])
+        user_name   = ifnone(user_name,   ratings.columns[0])
+        item_name   = ifnone(item_name,   ratings.columns[1])
+        rating_name = ifnone(rating_name, ratings.columns[2])
         cat_names = [user_name,item_name]
         splits = RandomSplitter(valid_pct=valid_pct, seed=seed)(range_of(ratings))
         to = TabularCollab(ratings, [Categorify], cat_names, y_names=[rating_name], y_block=TransformBlock(), splits=splits)
         return to.dataloaders(path=path, **kwargs)
 
-    @delegates(DataLoaders.from_dblock)
     @classmethod
-    def from_csv(cls, csv, valid_pct=0.2, user_name=None, item_name=None, rating_name=None, seed=None, path='.', **kwargs):
+    def from_csv(cls, csv, **kwargs):
         "Create a `DataLoaders` suitable for collaborative filtering from `csv`."
-        return cls.from_df(pd.read_csv(csv), valid_pct, user_name, item_name, rating_name, seed=None, path='.', **kwargs)
+        return cls.from_df(pd.read_csv(csv), **kwargs)
+
+CollabDataLoaders.from_csv = delegates(to=CollabDataLoaders.from_df)(CollabDataLoaders.from_csv)
 
 # Cell
 class EmbeddingDotBias(Module):
@@ -48,13 +50,14 @@ class EmbeddingDotBias(Module):
 
     @classmethod
     def from_classes(cls, n_factors, classes, user=None, item=None, y_range=None):
+        "Build a model with `n_factors` by inferring `n_users` and  `n_items` from `classes`"
         if user is None: user = list(classes.keys())[0]
         if item is None: item = list(classes.keys())[1]
         res = cls(n_factors, len(classes[user]), len(classes[item]), y_range=y_range)
         res.classes,res.user,res.item = classes,user,item
         return res
 
-    def get_idx(self, arr, is_item=True):
+    def _get_idx(self, arr, is_item=True):
         "Fetch item or user (based on `is_item`) for all in `arr`"
         assert hasattr(self, 'classes'), "Build your model with `EmbeddingDotBias.from_classes` to use this functionality."
         classes = self.classes[self.item] if is_item else self.classes[self.user]
@@ -66,27 +69,27 @@ class EmbeddingDotBias(Module):
 
     def bias(self, arr, is_item=True):
         "Bias for item or user (based on `is_item`) for all in `arr`"
-        idx = self.get_idx(arr, is_item)
+        idx = self._get_idx(arr, is_item)
         layer = (self.i_bias if is_item else self.u_bias).eval().cpu()
         return to_detach(layer(idx).squeeze())
 
     def weight(self, arr, is_item=True):
         "Weight for item or user (based on `is_item`) for all in `arr`"
-        idx = self.get_idx(arr, is_item)
+        idx = self._get_idx(arr, is_item)
         layer = (self.i_weight if is_item else self.u_weight).eval().cpu()
         return to_detach(layer(idx))
 
 # Cell
-@delegates()
 class EmbeddingNN(TabularModel):
     "Subclass `TabularModel` to create a NN suitable for collaborative filtering."
+    @delegates(TabularModel.__init__)
     def __init__(self, emb_szs, layers, **kwargs):
         super().__init__(emb_szs=emb_szs, n_cont=0, out_sz=1, layers=layers, **kwargs)
 
 # Cell
 @delegates(Learner.__init__)
 def collab_learner(dls, n_factors=50, use_nn=False, emb_szs=None, layers=None, config=None, y_range=None, loss_func=None, **kwargs):
-    "Create a Learner for collaborative filtering on `data`."
+    "Create a Learner for collaborative filtering on `dls`."
     emb_szs = get_emb_sz(dls, ifnone(emb_szs, {}))
     if loss_func is None: loss_func = MSELossFlat()
     if config is None: config = tabular_config()
