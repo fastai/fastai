ceb24c7065949fda824bb687772243f6924ad96f
ketan-b <54092325+ketan-b@users.noreply.github.com>
2020-04-15 04:21:47 +0530

    Update __init__.py

--- fastai/vision/__init__.py
+++ fastai/vision/__init__.py
@@ -1,5 +1,5 @@
 from .. import basics
-from . import learner, image, data, transfrom, tta, models
+from . import learner, image, data, transform, tta, models
 from .. import vision
 
 __all__ = [*basics.__all__, *learner.__all__, *data.__all__, *image.__all__, *transform.__all__, *tta.__all__, 'models', 'vision']
