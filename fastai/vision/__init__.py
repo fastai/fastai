44a3b04144cba7c7c099913bea99ef321ee422c8
ketan-b <54092325+ketan-b@users.noreply.github.com>
2020-04-15 04:06:36 +0530

    Update __init__.py
    fastai.vision import * -> name ‘learner’ is not defined issue resolved

--- fastai/vision/__init__.py
+++ fastai/vision/__init__.py
@@ -1,13 +1,5 @@
 from .. import basics
-from ..basics import *
-from .learner import *
-from .image import *
-from .data import *
-from .transform import *
-from .tta import *
-from . import models
-
+from . import learner, image, data, transfrom, tta, models
 from .. import vision
 
 __all__ = [*basics.__all__, *learner.__all__, *data.__all__, *image.__all__, *transform.__all__, *tta.__all__, 'models', 'vision']
-
