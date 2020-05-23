708076afdc39744e892d04271dc407becef3d9f4
Boris Dayma <boris.dayma@gmail.com>
2020-05-23 08:15:46 -0500

    feat(wandb): custom logging of semantic segmentation (#372)

--- fastai2/callback/wandb.py
+++ fastai2/callback/wandb.py
@@ -157,6 +157,20 @@ def wandb_process(x:TensorImage, y:(TensorCategory,TensorMultiCategory), samples
 
 # Cell
 @typedispatch
+def wandb_process(x:TensorImage, y:TensorMask, samples, outs):
+    res = []
+    class_labels = {i:f'{c}' for i,c in enumerate(y.get_meta('codes'))} if y.get_meta('codes') is not None else None
+    for s,o in zip(samples, outs):
+        img = s[0].permute(1,2,0)
+        masks = {}
+        for t, capt in ((o[0], "Prediction"), (s[1], "Ground Truth")):
+            masks[capt] = {'mask_data':t.numpy().astype(np.uint8)}
+            if class_labels: masks[capt]['class_labels'] = class_labels
+        res.append(wandb.Image(img, masks=masks))
+    return {"Prediction Samples":res}
+
+# Cell
+@typedispatch
 def wandb_process(x:TensorText, y:(TensorCategory,TensorMultiCategory), samples, outs):
     data = [[s[0], s[1], o[0]] for s,o in zip(samples,outs)]
     return {"Prediction Samples": wandb.Table(data=data, columns=["Text", "Target", "Prediction"])}
