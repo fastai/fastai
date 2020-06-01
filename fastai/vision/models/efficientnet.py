5379ba581d35eacd0e95a357f6ab39e0c5835527
jiahuawu <j.wu@l2f.ch>
2020-06-01 17:52:42 +0200

    Improve Sequential object creation

--- fastai/vision/models/efficientnet.py
+++ fastai/vision/models/efficientnet.py
@@ -1,3 +1,4 @@
+from fastai.layers import Flatten
 from torch import nn
 
 from ...core import *
@@ -12,7 +13,7 @@ from efficientnet_pytorch.utils import Conv2dStaticSamePadding
 
 def create_efficientnet(model_name, pretrained=False):
     model = EfficientNet.from_pretrained(model_name) if pretrained else EfficientNet.from_name(model_name)
-    return EfficientNetWrapper(model)
+    return SequentialEfficientNet(model)
 
 def EfficientNetB0(pretrained=False): return create_efficientnet("efficientnet-b0", pretrained)
 def EfficientNetB1(pretrained=False): return create_efficientnet("efficientnet-b1", pretrained)
@@ -25,7 +26,7 @@ def EfficientNetB7(pretrained=False): return create_efficientnet("efficientnet-b
 
 class MBConvBlockWrapper(nn.Module):
 
-    def __init__(self, model: MBConvBlock, drop_connect_rate: float):
+    def __init__(self, model: MBConvBlock, drop_connect_rate: float=None):
         super().__init__()
         self.model = model
         self.drop_connect_rate = drop_connect_rate
@@ -33,40 +34,37 @@ class MBConvBlockWrapper(nn.Module):
     def forward(self, inputs):
         return self.model.forward(inputs, self.drop_connect_rate)
 
-class EfficientNetWrapper(nn.Module):
+class SequentialEfficientNet(nn.Module):
     """Convert EfficientNet instance to an object that can be processed by existing cnn_learner scheme."""
 
     def __init__(self, model: EfficientNet):
         super().__init__()
-        self.model = model
-        _transform_conv_2d_static_same_padding_to_seq(self.model)
+        _transform_conv_2d_static_same_padding_to_sequential(model)
+        for block in model._blocks:
+            _transform_conv_2d_static_same_padding_to_sequential(block)
+        self._conv_stem = model._conv_stem
+        self._bn0 = model._bn0
+        self._swish0 = deepcopy(model._swish)
         blocks = []
         for idx, block in enumerate(model._blocks):
-            _transform_conv_2d_static_same_padding_to_seq(block)
+            _transform_conv_2d_static_same_padding_to_sequential(block)
             drop_connect_rate = model._global_params.drop_connect_rate
             if drop_connect_rate:
                 drop_connect_rate *= float(idx) / len(model._blocks)
             blocks.append(MBConvBlockWrapper(block, drop_connect_rate))
-        self.model._blocks = nn.Sequential(*blocks)
+        self._blocks = nn.Sequential(*blocks)
+        self._conv_head = model._conv_head
+        self._bn1 = model._bn1
+        self._swish1 = deepcopy(model._swish)
+        self._avg_pooling = model._avg_pooling
+        self._flatten = Flatten()
+        self._dropout = model._dropout
+        self._fc = model._fc
 
-    def children(self):
-        swish = self.model._swish
-        modules = list(self.model.children())
-        modules.remove(swish)
-        modules.remove(self.model._blocks)
-        bn0_index = modules.index(self.model._bn0)
-        modules = modules[:bn0_index + 1] + list(self.model._blocks) + modules[bn0_index + 1:]
-        modules.insert(bn0_index + 1, swish)
-        modules.insert(modules.index(self.model._bn1) + 1, swish)
-        return (module for module in modules)
+    def forward(self, inputs):
+        return nn.Sequential(*self.children()).forward(inputs)
 
-    def __getitem__(self, item):
-        return nn.Sequential(*self.children())[item]
-
-    def __len__(self):
-        return len(list(self.children()))
-
-def _transform_conv_2d_static_same_padding_to_seq(model: nn.Module):
+def _transform_conv_2d_static_same_padding_to_sequential(model: nn.Module):
     """Split Conv2dStaticSamePadding instance into Sequential(padding, Conv2d)."""
     for name, module in model._modules.items():
         if isinstance(module, Conv2dStaticSamePadding):
