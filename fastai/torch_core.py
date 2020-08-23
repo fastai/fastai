d852cdcac97ae4f7c1054af7593ce3cab1512b52
Jeremy Howard <j@fast.ai>
2020-08-22 20:58:33 -0700

    fixes #2655

--- fastai/torch_core.py
+++ fastai/torch_core.py
@@ -134,6 +134,8 @@ def set_seed(s, reproducible=False):
     "Set random seed for `random`, `torch`, and `numpy` (where available)"
     try: torch.manual_seed(s)
     except NameError: pass
+    try: torch.cuda.manual_seed_all(s)
+    except NameError: pass
     try: np.random.seed(s%(2**32-1))
     except NameError: pass
     random.seed(s)
