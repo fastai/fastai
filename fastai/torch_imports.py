69a48da49e8afb398e945943d10452c38e73d052
Jeremy Howard <j@fast.ai>
2020-08-27 11:08:56 -0700

    use fork multiprocessing on mac h/t aseifert

--- fastai/torch_imports.py
+++ fastai/torch_imports.py
@@ -7,3 +7,7 @@ from torch.utils.data import SequentialSampler,RandomSampler,Sampler,BatchSample
 from torch.utils.data import IterableDataset,get_worker_info
 from torch.utils.data._utils.collate import default_collate,default_convert
 
+# Python 3.8 changed to 'spawn' but that doesn't work with PyTorch DataLoader w n_workers>0
+import platform
+if platform.system()=='Darwin': multiprocessing.set_start_method('fork', force=True)
+
