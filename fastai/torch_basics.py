e3f746625ca21440c8404bb456621d1f85001203
Jeremy Howard <j@fast.ai>
2020-08-28 11:28:07 -0700

    KMP_DUPLICATE_LIB_OK

--- fastai/torch_basics.py
+++ fastai/torch_basics.py
@@ -1,7 +1,10 @@
 from torch import multiprocessing
-# Python 3.8 changed to 'spawn' but that doesn't work with PyTorch DataLoader w n_workers>0
-import platform
-if platform.system()=='Darwin': multiprocessing.set_start_method('fork', force=True)
+import platform,os
+if platform.system()=='Darwin':
+    # Python 3.8 changed to 'spawn' but that doesn't work with PyTorch DataLoader w n_workers>0
+    multiprocessing.set_start_method('fork', force=True)
+    # workaround "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized"
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 from .imports import *
 from .torch_imports import *
