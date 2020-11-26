46d3cfc4397d607e0666c11f1f11938b366d7a57
Zachary Mueller <muellerzr@gmail.com>
2020-11-25 21:44:15 -0500

    adjust callback.all

--- fastai/callback/all.py
+++ fastai/callback/all.py
@@ -2,7 +2,7 @@ from .core import *
 from .data import *
 from .fp16 import *
 from .hook import *
-from .mixup import *
+from .mixups import *
 from .progress import *
 from .schedule import *
 from .tracker import *
