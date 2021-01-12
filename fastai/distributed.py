9072c48a3d2bcb1f0fa7d6732a2ca84d36bc818c
Andres Torrubia <antorsae@gmail.com>
2021-01-12 16:42:38 +0100

    fixes distributed training when using 0 workers

--- fastai/distributed.py
+++ fastai/distributed.py
@@ -76,7 +76,8 @@ class DistributedDL(TfmdDL):
         if rank is None: rank=rank_distrib()
         if world_size is None: world_size=num_distrib()
         store_attr()
-        self.bs,self.device,self.drop_last,self.dataset,fake = attrgetter('bs','device','drop_last','dataset','fake_l')(dl)
+        self.bs,self.device,self.drop_last,self.dataset,fake,self.num_workers,self.offs = \
+            attrgetter('bs','device','drop_last','dataset','fake_l','num_workers','offs')(dl)
         self.fake_l = _FakeLoader(self, fake.pin_memory, fake.num_workers, fake.timeout, persistent_workers=fake.persistent_workers)
 
     def _broadcast(self,t,rank):
