eba52ee17f99a974fc4aa9b049852ef1338a1e13
Yi Zhang <zhanyi@microsoft.com>
2021-02-04 15:16:24 +0000

    test optimization

--- fastai/vision/data.py
+++ fastai/vision/data.py
@@ -7,6 +7,7 @@ __all__ = ['get_grid', 'clip_remove_empty', 'bb_pad', 'ImageBlock', 'MaskBlock',
 from ..torch_basics import *
 from ..data.all import *
 from .core import *
+import types
 
 # Cell
 @delegates(subplots)
@@ -108,6 +109,10 @@ class ImageDataLoaders(DataLoaders):
     @classmethod
     def from_name_func(cls, path, fnames, label_func, **kwargs):
         "Create from the name attrs of `fnames` in `path`s with `label_func`"
+        if sys.platform == 'win32':
+            if type(label_func) is types.LambdaType:
+                # refer https://medium.com/@jwnx/multiprocessing-serialization-in-python-with-pickle-9844f6fa1812
+                raise ValueError("label_func couldn't be lambda function in Windows")
         f = using_attr(label_func, 'name')
         return cls.from_path_func(path, fnames, f, **kwargs)
 
