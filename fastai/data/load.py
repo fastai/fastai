a3cfdea2dee99065fdd7814437a85a93574c2723
Zachary Mueller <muellerzr@gmail.com>
2021-04-21 22:36:03 -0500

    Keep if self.device is not None

--- fastai/data/load.py
+++ fastai/data/load.py
@@ -107,7 +107,7 @@ class DataLoader(GetAttr):
         self.before_iter()
         self.__idxs=self.get_idxs() # called in context of main process (not workers/subprocesses)
         for b in _loaders[self.fake_l.num_workers==0](self.fake_l):
-            b = to_device(b, self.device)
+            if self.device is not None: b = to_device(b, self.device)
             yield self.after_batch(b)
         self.after_iter()
         if hasattr(self, 'it'): del(self.it)
