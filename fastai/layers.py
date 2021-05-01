b82e34dadf54dd3bcc07513c0dcad017e98f6b14
Thomas Capelle <thomascapelle@gmail.com>
2021-05-01 02:42:06 +0200

    Timedistributed layer and Image Sequence Tutorial (#3124)
    * add Timedistributed layer
    
    * use store_attr
    
    * clean up
    
    * re run CI
    
    * rephrased some doc
    
    * Image sequences tutorial notebook
    
    * add all_slow flag
    
    * typos and formating
    
    * more rephrasing
    
    * show_res
    
    * add timesformer training
    
    Co-authored-by: Thomas Capelle <thomas.capelle@steady-sun.com>
    Co-authored-by: Jeremy Howard <j@fast.ai>

--- fastai/layers.py
+++ fastai/layers.py
@@ -6,9 +6,9 @@ __all__ = ['module', 'Identity', 'Lambda', 'PartialLambda', 'Flatten', 'View', '
            'sigmoid_', 'vleaky_relu', 'init_default', 'init_linear', 'ConvLayer', 'AdaptiveAvgPool', 'MaxPool',
            'AvgPool', 'trunc_normal_', 'Embedding', 'SelfAttention', 'PooledSelfAttention2d', 'SimpleSelfAttention',
            'icnr_init', 'PixelShuffle_ICNR', 'sequential', 'SequentialEx', 'MergeLayer', 'Cat', 'SimpleCNN',
-           'ProdLayer', 'inplace_relu', 'SEModule', 'ResBlock', 'SEBlock', 'SEResNeXtBlock', 'SeparableBlock', 'swish',
-           'Swish', 'MishJitAutoFn', 'mish', 'Mish', 'ParameterModule', 'children_and_parameters', 'has_children',
-           'flatten_model', 'NoneReduce', 'in_channels']
+           'ProdLayer', 'inplace_relu', 'SEModule', 'ResBlock', 'SEBlock', 'SEResNeXtBlock', 'SeparableBlock',
+           'TimeDistributed', 'swish', 'Swish', 'MishJitAutoFn', 'mish', 'Mish', 'ParameterModule',
+           'children_and_parameters', 'has_children', 'flatten_model', 'NoneReduce', 'in_channels']
 
 # Cell
 from .imports import *
@@ -494,6 +494,48 @@ def SEResNeXtBlock(expansion, ni, nf, groups=32, reduction=16, stride=1, base_wi
 # Cell
 def SeparableBlock(expansion, ni, nf, reduction=16, stride=1, base_width=4, **kwargs):
     return ResBlock(expansion, ni, nf, stride=stride, reduction=reduction, nh2=nf*2, dw=True, **kwargs)
+
+# Cell
+def _stack_tups(tuples, stack_dim=1):
+    "Stack tuple of tensors along `stack_dim`"
+    return tuple(torch.stack([t[i] for t in tuples], dim=stack_dim) for i in range_of(tuples[0]))
+
+# Cell
+class TimeDistributed(Module):
+    "Applies `module` over `tdim` identically for each step, use `low_mem` to compute one at a time."
+    def __init__(self, module, low_mem=False, tdim=1):
+        store_attr()
+
+    def forward(self, *tensors, **kwargs):
+        "input x with shape:(bs,seq_len,channels,width,height)"
+        if self.low_mem or self.tdim!=1:
+            return self.low_mem_forward(*tensors, **kwargs)
+        else:
+            #only support tdim=1
+            inp_shape = tensors[0].shape
+            bs, seq_len = inp_shape[0], inp_shape[1]
+            out = self.module(*[x.view(bs*seq_len, *x.shape[2:]) for x in tensors], **kwargs)
+        return self.format_output(out, bs, seq_len)
+
+    def low_mem_forward(self, *tensors, **kwargs):
+        "input x with shape:(bs,seq_len,channels,width,height)"
+        seq_len = tensors[0].shape[self.tdim]
+        args_split = [torch.unbind(x, dim=self.tdim) for x in tensors]
+        out = []
+        for i in range(seq_len):
+            out.append(self.module(*[args[i] for args in args_split]), **kwargs)
+        if isinstance(out[0], tuple):
+            return _stack_tups(out, stack_dim=self.tdim)
+        return torch.stack(out, dim=self.tdim)
+
+    def format_output(self, out, bs, seq_len):
+        "unstack from batchsize outputs"
+        if isinstance(out, tuple):
+            return tuple(out_i.view(bs, seq_len, *out_i.shape[1:]) for out_i in out)
+        return out.view(bs, seq_len,*out.shape[1:])
+
+    def __repr__(self):
+        return f'TimeDistributed({self.module})'
 
 # Cell
 from torch.jit import script
