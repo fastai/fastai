24879974eb02e6dd20186f7875e9a770253d29a6
ohmeow <wgilliam@ohmeow.com>
2021-06-14 15:24:46 -0700

    Updated layer_info to work with seq2seq huggingface transformer models (#3399)
    * updated
    
    * updated layer_info to work with seq2seq huggingface transformer models; tested with ohmeow-blurr

--- fastai/callback/hook.py
+++ fastai/callback/hook.py
@@ -146,7 +146,7 @@ def layer_info(learn, *xb):
     "Return layer infos of `model` on `xb` (only support batch first inputs)"
     def _track(m, i, o):
         params, trainable, shape = '', '', ''
-        same = any((x[0].shape[1:] == x[1].shape for x in zip(i, o)))
+        same = any((isinstance(x[0], torch.Tensor) and x[0].shape[1:] == x[1].shape for x in zip(i, o)))
         if hasattr(m, 'weight'): # non activation layer
             params, trainable = total_params(m)
             shape = apply(lambda x: x.shape, o)
