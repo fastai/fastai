896da46f3992aad8297f3b2531bf2352280ad666
Michael J. Booth <mjboothaus@gmail.com>
2021-06-22 09:27:01 +1000

    removal of trailing query string following image file in URL (#3400)
    * removal of trailing query string following image file in URL
    
    * Delete lesson2-download.ipynb
    
    PR changes do not relate to this file so it has been removed from PR
    
    * Removed lesson2-download.ipynb from pull request
    
    Co-authored-by: Michael Booth <mjboothaus@Michaels-MacBook.local>

--- fastai/vision/utils.py
+++ fastai/vision/utils.py
@@ -22,6 +22,7 @@ def _get_downloaded_image_filename(dest, name, suffix):
 # Cell
 def _download_image_inner(dest, inp, timeout=4, preserve_filename=False):
     i,url = inp
+    url = url.split("?")[0]
     url_path = Path(url)
     suffix = url_path.suffix if url_path.suffix else '.jpg'
     name = _get_downloaded_image_filename(dest, url_path.stem, suffix) if preserve_filename else f"{i:08d}"
