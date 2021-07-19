995d2345bb197793e28e06cd95f4a00bc70fe8e6
rbrtwlz <abitdevelopment@gmail.com>
2021-07-20 01:00:50 +0200

    Fix tabular_learner to accept custom embedding sizes

--- fastai/tabular/learner.py
+++ fastai/tabular/learner.py
@@ -26,7 +26,7 @@ def tabular_learner(dls, layers=None, emb_szs=None, config=None, n_out=None, y_r
     if config is None: config = tabular_config()
     if layers is None: layers = [200,100]
     to = dls.train_ds
-    emb_szs = get_emb_sz(dls.train_ds, {} if emb_szs is None else emb_szs)
+    emb_szs = get_emb_sz(dls.train_ds, {}) if emb_szs is None else emb_szs
     if n_out is None: n_out = get_c(dls)
     assert n_out, "`n_out` is not defined, and could not be inferred from data, set `dls.c` or pass `n_out`"
     if y_range is None and 'y_range' in config: y_range = config.pop('y_range')
