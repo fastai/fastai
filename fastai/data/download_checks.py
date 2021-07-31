84da60b686ff3c6d3d97ad9c2cb88348a204b276
Jeremy Howard <j@fast.ai>
2021-08-01 05:51:43 +1000

    fastdownload

---
+++ fastai/data/download_checks.py
@@ -0,0 +1,4 @@
+{'https://s3.amazonaws.com/fast-ai-nlp/ag_news_csv.tgz': (11784419,
+                                                          'b86f328f4dbd072486591cb7a5644dcd'),
+ 'https://s3.amazonaws.com/fast-ai-sample/adult_sample.tgz': (968212,
+                                                              '64eb9d7e23732de0b138f7372d15492f')}
