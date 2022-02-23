c8a3260ea0d734ecb08a6242909502cfddff5aad
Your Name <you@example.com>
2022-02-22 21:27:40 -0800

    removed typing library

--- fastai/vision/data.py
+++ fastai/vision/data.py
@@ -22,7 +22,7 @@ def get_grid(
     return_fig:bool=False, # Whether to return the figure created by `subplots`
     flatten:bool=True, # Whether to flatten the matplot axes such that they can be iterated over with a single loop
     **kwargs,
-) -> (typing.Tuple[plt.Figure, plt.Axes], plt.Axes):
+) -> (plt.Figure, plt.Axes): # Returns just `axs` by default, and (`fig`, `axs`) if `return_fig` is set to True
     "Return a grid of `n` axes, `rows` by `cols`"
     if nrows:
         ncols = ncols or int(np.ceil(n/nrows))
