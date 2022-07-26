a6b831cb3c2f8e8114f679231a71a79249f1d3e5
Jeremy Howard <j@fast.ai>
2022-07-27 09:47:35 +1000

    entry-point

--- setup.py
+++ setup.py
@@ -55,5 +55,9 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     zip_safe = False,
-    entry_points = { 'console_scripts': cfg.get('console_scripts','').split() },
+    entry_points = {
+        'console_scripts': cfg.get('console_scripts','').split(),
+        'nbdev': [f'{cfg.get("lib_path")}={cfg.get("lib_path")}._modidx:d']
+    },
     **setup_cfg)
+
