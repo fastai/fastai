83831a3434cdaf0a5ca214dfc4bd7fec65d4ffac
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2022-09-12 23:33:18 +0100

    Add latest TorchVision models on fastai

--- fastai/vision/models/tvm.py
+++ fastai/vision/models/tvm.py
@@ -1,4 +1,11 @@
-from torchvision.models import ResNet,resnet18,resnet34,resnet50,resnet101,resnet152
-from torchvision.models import SqueezeNet,squeezenet1_0,squeezenet1_1
-from torchvision.models import densenet121,densenet169,densenet201,densenet161
-from torchvision.models import vgg11_bn,vgg13_bn,vgg16_bn,vgg19_bn,alexnet
+from torchvision.models import *
+import types as _t
+
+_g = globals()
+for _k, _v in list(_g.items()):
+    if (
+        isinstance(_v, _t.ModuleType) and _v.__name__.startswith("torchvision.models")
+    ) or (callable(_v) and _v.__module__ == "torchvision.models._api"):
+        del _g[_k]
+
+del _k, _v, _g, _t
