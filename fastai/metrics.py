785ec908c7f75a272c718dd0df53822bdf5e9e0e
Jeremy Howard <github@jhoward.fastmail.fm>
2024-05-25 15:16:59 +1000

    export

--- fastai/metrics.py
+++ fastai/metrics.py
@@ -76,7 +76,7 @@ class AccumMetric(Metric):
     @name.setter
     def name(self, value): self._name = value
 
-# %% ../nbs/13b_metrics.ipynb 15
+# %% ../nbs/13b_metrics.ipynb 16
 def skm_to_fastai(func, is_class=True, thresh=None, axis=-1, activation=None, **kwargs):
     "Convert `func` from sklearn.metrics to a fastai metric"
     dim_argmax = axis if is_class and thresh is None else None
@@ -85,7 +85,7 @@ def skm_to_fastai(func, is_class=True, thresh=None, axis=-1, activation=None, **
     return AccumMetric(func, dim_argmax=dim_argmax, activation=activation, thresh=thresh,
                        to_np=True, invert_arg=True, **kwargs)
 
-# %% ../nbs/13b_metrics.ipynb 21
+# %% ../nbs/13b_metrics.ipynb 22
 def optim_metric(f, argname, bounds, tol=0.01, do_neg=True, get_x=False):
     "Replace metric `f` with a version that optimizes argument `argname`"
     def _f(preds, targs):
@@ -100,225 +100,225 @@ def optim_metric(f, argname, bounds, tol=0.01, do_neg=True, get_x=False):
     _f.__name__ = f'opt_{f.__name__}'
     return _f
 
-# %% ../nbs/13b_metrics.ipynb 25
+# %% ../nbs/13b_metrics.ipynb 26
 def accuracy(inp, targ, axis=-1):
     "Compute accuracy with `targ` when `pred` is bs * n_classes"
     pred,targ = flatten_check(inp.argmax(dim=axis), targ)
     return (pred == targ).float().mean()
 
-# %% ../nbs/13b_metrics.ipynb 28
+# %% ../nbs/13b_metrics.ipynb 29
 def error_rate(inp, targ, axis=-1):
     "1 - `accuracy`"
     return 1 - accuracy(inp, targ, axis=axis)
 
-# %% ../nbs/13b_metrics.ipynb 30
+# %% ../nbs/13b_metrics.ipynb 31
 def top_k_accuracy(inp, targ, k=5, axis=-1):
     "Computes the Top-k accuracy (`targ` is in the top `k` predictions of `inp`)"
     inp = inp.topk(k=k, dim=axis)[1]
     targ = targ.unsqueeze(dim=axis).expand_as(inp)
     return (inp == targ).sum(dim=-1).float().mean()
 
-# %% ../nbs/13b_metrics.ipynb 32
+# %% ../nbs/13b_metrics.ipynb 33
 def APScoreBinary(axis=-1, average='macro', pos_label=1, sample_weight=None):
     "Average Precision for single-label binary classification problems"
     return skm_to_fastai(skm.average_precision_score, axis=axis, activation=ActivationType.BinarySoftmax,
                          average=average, pos_label=pos_label, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 34
+# %% ../nbs/13b_metrics.ipynb 35
 def BalancedAccuracy(axis=-1, sample_weight=None, adjusted=False):
     "Balanced Accuracy for single-label binary classification problems"
     return skm_to_fastai(skm.balanced_accuracy_score, axis=axis,
                          sample_weight=sample_weight, adjusted=adjusted)
 
-# %% ../nbs/13b_metrics.ipynb 36
+# %% ../nbs/13b_metrics.ipynb 37
 def BrierScore(axis=-1, sample_weight=None, pos_label=None):
     "Brier score for single-label classification problems"
     return skm_to_fastai(skm.brier_score_loss, axis=axis,
                          sample_weight=sample_weight, pos_label=pos_label)
 
-# %% ../nbs/13b_metrics.ipynb 38
+# %% ../nbs/13b_metrics.ipynb 39
 def CohenKappa(axis=-1, labels=None, weights=None, sample_weight=None):
     "Cohen kappa for single-label classification problems"
     return skm_to_fastai(skm.cohen_kappa_score, axis=axis, labels=labels, weights=weights,
                          sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 40
+# %% ../nbs/13b_metrics.ipynb 41
 def F1Score(axis=-1, labels=None, pos_label=1, average='binary', sample_weight=None):
     "F1 score for single-label classification problems"
     return skm_to_fastai(skm.f1_score, axis=axis,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 42
+# %% ../nbs/13b_metrics.ipynb 43
 def FBeta(beta, axis=-1, labels=None, pos_label=1, average='binary', sample_weight=None):
     "FBeta score with `beta` for single-label classification problems"
     return skm_to_fastai(skm.fbeta_score, axis=axis,
                 beta=beta, labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 44
+# %% ../nbs/13b_metrics.ipynb 45
 def HammingLoss(axis=-1, sample_weight=None):
     "Hamming loss for single-label classification problems"
     return skm_to_fastai(skm.hamming_loss, axis=axis,
                          sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 46
+# %% ../nbs/13b_metrics.ipynb 47
 def Jaccard(axis=-1, labels=None, pos_label=1, average='binary', sample_weight=None):
     "Jaccard score for single-label classification problems"
     return skm_to_fastai(skm.jaccard_score, axis=axis,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 48
+# %% ../nbs/13b_metrics.ipynb 49
 def Precision(axis=-1, labels=None, pos_label=1, average='binary', sample_weight=None):
     "Precision for single-label classification problems"
     return skm_to_fastai(skm.precision_score, axis=axis,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 50
+# %% ../nbs/13b_metrics.ipynb 51
 def Recall(axis=-1, labels=None, pos_label=1, average='binary', sample_weight=None):
     "Recall for single-label classification problems"
     return skm_to_fastai(skm.recall_score, axis=axis,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 52
+# %% ../nbs/13b_metrics.ipynb 53
 def RocAuc(axis=-1, average='macro', sample_weight=None, max_fpr=None, multi_class='ovr'):
     "Area Under the Receiver Operating Characteristic Curve for single-label multiclass classification problems"
     assert multi_class in ['ovr', 'ovo']
     return skm_to_fastai(skm.roc_auc_score, axis=axis, activation=ActivationType.Softmax, flatten=False,
                          average=average, sample_weight=sample_weight, max_fpr=max_fpr, multi_class=multi_class)
 
-# %% ../nbs/13b_metrics.ipynb 54
+# %% ../nbs/13b_metrics.ipynb 55
 def RocAucBinary(axis=-1, average='macro', sample_weight=None, max_fpr=None, multi_class='raise'):
     "Area Under the Receiver Operating Characteristic Curve for single-label binary classification problems"
     return skm_to_fastai(skm.roc_auc_score, axis=axis, activation=ActivationType.BinarySoftmax,
                          average=average, sample_weight=sample_weight, max_fpr=max_fpr, multi_class=multi_class)
 
-# %% ../nbs/13b_metrics.ipynb 56
+# %% ../nbs/13b_metrics.ipynb 57
 def MatthewsCorrCoef(sample_weight=None, **kwargs):
     "Matthews correlation coefficient for single-label classification problems"
     return skm_to_fastai(skm.matthews_corrcoef, sample_weight=sample_weight, **kwargs)
 
-# %% ../nbs/13b_metrics.ipynb 59
+# %% ../nbs/13b_metrics.ipynb 60
 def accuracy_multi(inp, targ, thresh=0.5, sigmoid=True):
     "Compute accuracy when `inp` and `targ` are the same size."
     inp,targ = flatten_check(inp,targ)
     if sigmoid: inp = inp.sigmoid()
     return ((inp>thresh)==targ.bool()).float().mean()
 
-# %% ../nbs/13b_metrics.ipynb 62
+# %% ../nbs/13b_metrics.ipynb 63
 def APScoreMulti(sigmoid=True, average='macro', pos_label=1, sample_weight=None):
     "Average Precision for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.average_precision_score, activation=activation, flatten=False,
                          average=average, pos_label=pos_label, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 64
+# %% ../nbs/13b_metrics.ipynb 65
 def BrierScoreMulti(thresh=0.5, sigmoid=True, sample_weight=None, pos_label=None):
     "Brier score for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.brier_score_loss, thresh=thresh, activation=activation, flatten=False,
                          sample_weight=sample_weight, pos_label=pos_label)
 
-# %% ../nbs/13b_metrics.ipynb 66
+# %% ../nbs/13b_metrics.ipynb 67
 def F1ScoreMulti(thresh=0.5, sigmoid=True, labels=None, pos_label=1, average='macro', sample_weight=None):
     "F1 score for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.f1_score, thresh=thresh, activation=activation, flatten=False,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 68
+# %% ../nbs/13b_metrics.ipynb 69
 def FBetaMulti(beta, thresh=0.5, sigmoid=True, labels=None, pos_label=1, average='macro', sample_weight=None):
     "FBeta score with `beta` for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.fbeta_score, thresh=thresh, activation=activation, flatten=False,
                 beta=beta, labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 70
+# %% ../nbs/13b_metrics.ipynb 71
 def HammingLossMulti(thresh=0.5, sigmoid=True, labels=None, sample_weight=None):
     "Hamming loss for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.hamming_loss, thresh=thresh, activation=activation, flatten=False,
                          sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 72
+# %% ../nbs/13b_metrics.ipynb 73
 def JaccardMulti(thresh=0.5, sigmoid=True, labels=None, pos_label=1, average='macro', sample_weight=None):
     "Jaccard score for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.jaccard_score, thresh=thresh, activation=activation, flatten=False,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 74
+# %% ../nbs/13b_metrics.ipynb 75
 def MatthewsCorrCoefMulti(thresh=0.5, sigmoid=True, sample_weight=None):
     "Matthews correlation coefficient for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.matthews_corrcoef, thresh=thresh, activation=activation, flatten=False, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 76
+# %% ../nbs/13b_metrics.ipynb 77
 def PrecisionMulti(thresh=0.5, sigmoid=True, labels=None, pos_label=1, average='macro', sample_weight=None):
     "Precision for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.precision_score, thresh=thresh, activation=activation, flatten=False,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 78
+# %% ../nbs/13b_metrics.ipynb 79
 def RecallMulti(thresh=0.5, sigmoid=True, labels=None, pos_label=1, average='macro', sample_weight=None):
     "Recall for multi-label classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.recall_score, thresh=thresh, activation=activation, flatten=False,
                          labels=labels, pos_label=pos_label, average=average, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 80
+# %% ../nbs/13b_metrics.ipynb 81
 def RocAucMulti(sigmoid=True, average='macro', sample_weight=None, max_fpr=None):
     "Area Under the Receiver Operating Characteristic Curve for multi-label binary classification problems"
     activation = ActivationType.Sigmoid if sigmoid else ActivationType.No
     return skm_to_fastai(skm.roc_auc_score, activation=activation, flatten=False,
                          average=average, sample_weight=sample_weight, max_fpr=max_fpr)
 
-# %% ../nbs/13b_metrics.ipynb 84
+# %% ../nbs/13b_metrics.ipynb 85
 def mse(inp,targ):
     "Mean squared error between `inp` and `targ`."
     return F.mse_loss(*flatten_check(inp,targ))
 
-# %% ../nbs/13b_metrics.ipynb 86
+# %% ../nbs/13b_metrics.ipynb 87
 def _rmse(inp, targ): return torch.sqrt(F.mse_loss(inp, targ))
 rmse = AccumMetric(_rmse)
 rmse.__doc__ = "Root mean squared error"
 
-# %% ../nbs/13b_metrics.ipynb 89
+# %% ../nbs/13b_metrics.ipynb 90
 def mae(inp,targ):
     "Mean absolute error between `inp` and `targ`."
     inp,targ = flatten_check(inp,targ)
     return torch.abs(inp - targ).mean()
 
-# %% ../nbs/13b_metrics.ipynb 91
+# %% ../nbs/13b_metrics.ipynb 92
 def msle(inp, targ):
     "Mean squared logarithmic error between `inp` and `targ`."
     inp,targ = flatten_check(inp,targ)
     return F.mse_loss(torch.log(1 + inp), torch.log(1 + targ))
 
-# %% ../nbs/13b_metrics.ipynb 93
+# %% ../nbs/13b_metrics.ipynb 94
 def _exp_rmspe(inp,targ):
     inp,targ = torch.exp(inp),torch.exp(targ)
     return torch.sqrt(((targ - inp)/targ).pow(2).mean())
 exp_rmspe = AccumMetric(_exp_rmspe)
 exp_rmspe.__doc__ = "Root mean square percentage error of the exponential of  predictions and targets"
 
-# %% ../nbs/13b_metrics.ipynb 96
+# %% ../nbs/13b_metrics.ipynb 97
 def ExplainedVariance(sample_weight=None):
     "Explained variance between predictions and targets"
     return skm_to_fastai(skm.explained_variance_score, is_class=False, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 98
+# %% ../nbs/13b_metrics.ipynb 99
 def R2Score(sample_weight=None):
     "R2 score between predictions and targets"
     return skm_to_fastai(skm.r2_score, is_class=False, sample_weight=sample_weight)
 
-# %% ../nbs/13b_metrics.ipynb 100
+# %% ../nbs/13b_metrics.ipynb 101
 @delegates(AccumMetric)
 def PearsonCorrCoef(dim_argmax=None, **kwargs):
     "Pearson correlation coefficient for regression problem"
     def pearsonr(x,y): return scs.pearsonr(x,y)[0]
     return AccumMetric(pearsonr, invert_arg=False, dim_argmax=dim_argmax, **kwargs)
 
-# %% ../nbs/13b_metrics.ipynb 103
+# %% ../nbs/13b_metrics.ipynb 104
 @delegates(AccumMetric)
 def SpearmanCorrCoef(dim_argmax=None, axis=0, nan_policy='propagate', **kwargs):
     "Spearman correlation coefficient for regression problem"
@@ -326,14 +326,14 @@ def SpearmanCorrCoef(dim_argmax=None, axis=0, nan_policy='propagate', **kwargs):
     return AccumMetric(partial(spearmanr, axis=axis, nan_policy=nan_policy),
                        invert_arg=False, dim_argmax=dim_argmax, **kwargs)
 
-# %% ../nbs/13b_metrics.ipynb 111
+# %% ../nbs/13b_metrics.ipynb 112
 def foreground_acc(inp, targ, bkg_idx=0, axis=1):
     "Computes non-background accuracy for multiclass segmentation"
     targ = cast(targ.squeeze(1), TensorBase)
     mask = targ != bkg_idx
     return (inp.argmax(dim=axis)[mask]==targ[mask]).float().mean()
 
-# %% ../nbs/13b_metrics.ipynb 113
+# %% ../nbs/13b_metrics.ipynb 114
 class Dice(Metric):
     "Dice coefficient metric for binary target in segmentation"
     def __init__(self, axis=1): self.axis = axis
@@ -346,7 +346,7 @@ class Dice(Metric):
     @property
     def value(self): return 2. * self.inter/self.union if self.union > 0 else None
 
-# %% ../nbs/13b_metrics.ipynb 115
+# %% ../nbs/13b_metrics.ipynb 116
 class DiceMulti(Metric):
     "Averaged Dice metric (Macro F1) for multiclass target in segmentation"
     def __init__(self, axis=1): self.axis = axis
@@ -372,14 +372,14 @@ class DiceMulti(Metric):
             binary_dice_scores = np.append(binary_dice_scores, 2.*self.inter[c]/self.union[c] if self.union[c] > 0 else np.nan)
         return np.nanmean(binary_dice_scores)
 
-# %% ../nbs/13b_metrics.ipynb 118
+# %% ../nbs/13b_metrics.ipynb 119
 class JaccardCoeff(Dice):
     "Implementation of the Jaccard coefficient that is lighter in RAM"
     @property
     def value(self): return self.inter/(self.union-self.inter) if self.union > 0 else None
 
 
-# %% ../nbs/13b_metrics.ipynb 120
+# %% ../nbs/13b_metrics.ipynb 121
 class JaccardCoeffMulti(DiceMulti):
     "Averaged Jaccard coefficient metric (mIoU) for multiclass target in segmentation"
     @property
@@ -389,7 +389,7 @@ class JaccardCoeffMulti(DiceMulti):
             binary_jaccard_scores = np.append(binary_jaccard_scores, self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan)
         return np.nanmean(binary_jaccard_scores)
 
-# %% ../nbs/13b_metrics.ipynb 123
+# %% ../nbs/13b_metrics.ipynb 124
 class CorpusBLEUMetric(Metric):
     def __init__(self, vocab_sz=5000, axis=-1):
         "BLEU Metric calculated over the validation corpus"
@@ -441,7 +441,7 @@ class CorpusBLEUMetric(Metric):
             len_penalty = math.exp(1 - self.targ_len/self.pred_len) if self.pred_len < self.targ_len else 1
             return len_penalty * ((precs[0]*precs[1]*precs[2]*precs[3]) ** 0.25)
 
-# %% ../nbs/13b_metrics.ipynb 126
+# %% ../nbs/13b_metrics.ipynb 127
 class Perplexity(AvgLoss):
     "Perplexity (exponential of cross-entropy loss) for Language Models"
     @property
@@ -451,7 +451,7 @@ class Perplexity(AvgLoss):
 
 perplexity = Perplexity()
 
-# %% ../nbs/13b_metrics.ipynb 129
+# %% ../nbs/13b_metrics.ipynb 130
 class LossMetric(AvgMetric):
     "Create a metric from `loss_func.attr` named `nm`"
     def __init__(self, attr, nm=None): store_attr('attr,nm')
@@ -463,7 +463,7 @@ class LossMetric(AvgMetric):
     @property
     def name(self): return self.attr if self.nm is None else self.nm
 
-# %% ../nbs/13b_metrics.ipynb 130
+# %% ../nbs/13b_metrics.ipynb 131
 def LossMetrics(attrs, nms=None):
     "List of `LossMetric` for each of `attrs` and `nms`"
     if isinstance(attrs, str): attrs = attrs.split(',')
